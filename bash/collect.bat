0f97b4735c1eb6e80af8d3fe8a147618ab56252d
Ice2Faith <Ugex_Savelar@163.com>
2024-04-26 11:55:14 +0800

    feat(bash): collect.bat

--- bash/collect.bat
+++ bash/collect.bat
@@ -3,6 +3,7 @@ echo collect begin ...
 
 set src_path=..
 set dst_path=.\deploy
+set target_dir=target
 
 :: 脚本功能
 :: 将打包的结果收集到 deploy 目录中，并将原来的 deploy 目录移动到 backup 目录
@@ -17,43 +18,24 @@ move /Y %dst_path% .\backup
 rd /q /s %dst_path%
 mkdir %dst_path%
 
-for /F %%i in ('dir /B %src_path%\*') do (
-    if "%%i" == "bash" (
-        echo jump
-    ) else (
-      for /F %%j in ('dir /S /B %src_path%\%%i\*.jar') do (
-          echo %%j | findstr target\archive-tmp > nul && (
-                echo ignore: %%j
-          ) || (
-                echo copy /B /Y %%j %dst_path%\
-                copy /B /Y %%j %dst_path%\
-          )
+for /F %%i in ('dir /S /B %src_path%\%target_dir%') do (
+
+      for /F %%j in ('dir  /B %%i\*.jar') do (
+          echo copy /B /Y %%i\%%j %dst_path%\
+          copy /B /Y %%i\%%j %dst_path%\
       )
-      for /F %%j in ('dir /S /B %src_path%\%%i\*.war') do (
-          echo %%j | findstr target\archive-tmp > nul && (
-                echo ignore: %%j
-          ) || (
-                echo copy /B /Y %%j %dst_path%\
-                copy /B /Y %%j %dst_path%\
-          )
+      for /F %%j in ('dir  /B %%i\*.war') do (
+          echo copy /B /Y %%i\%%j %dst_path%\
+          copy /B /Y %%i\%%j %dst_path%\
       )
-      for /F %%j in ('dir /S /B %src_path%\%%i\*.tar') do (
-          echo %%j | findstr target\archive-tmp > nul && (
-                echo ignore: %%j
-            ) || (
-                echo copy /B /Y %%j %dst_path%\
-                copy /B /Y %%j %dst_path%\
-            )
+      for /F %%j in ('dir  /B %%i\*.tar') do (
+          echo copy /B /Y %%i\%%j %dst_path%\
+          copy /B /Y %%i\%%j %dst_path%\
       )
-      for /F %%j in ('dir /S /B %src_path%\%%i\*.gz') do (
-          echo %%j | findstr target\archive-tmp > nul && (
-                echo ignore: %%j
-            ) || (
-                echo copy /B /Y %%j %dst_path%\
-                copy /B /Y %%j %dst_path%\
-            )
+      for /F %%j in ('dir  /B %%i\*.gz') do (
+          echo copy /B /Y %%i\%%j %dst_path%\
+          copy /B /Y %%i\%%j %dst_path%\
       )
-    )
 )
 
 
