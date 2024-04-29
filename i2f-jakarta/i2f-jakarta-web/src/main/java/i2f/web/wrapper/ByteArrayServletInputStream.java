765a0c2293981881c56acc375212b1ddc98ca732
Ice2Faith <Ugex_Savelar@163.com>
2024-04-29 21:09:47 +0800

    feat(j2ee): java ext for web

---
+++ i2f-jakarta/i2f-jakarta-web/src/main/java/i2f/web/wrapper/ByteArrayServletInputStream.java
@@ -0,0 +1,39 @@
+package i2f.web.wrapper;
+
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+/**
+ * @author Ice2Faith
+ * @date 2022/6/29 14:16
+ * @desc
+ */
+public class ByteArrayServletInputStream extends ServletInputStream {
+    protected ByteArrayInputStream bis ;
+    public ByteArrayServletInputStream(ByteArrayInputStream bis) {
+        this.bis = bis;
+    }
+
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public void setReadListener(ReadListener readListener) {
+
+    }
+
+    @Override
+    public int read() throws IOException {
+        return bis.read();
+    }
+}
