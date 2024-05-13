a70aabf1f955610dd5b259345abe4024b2e8bca1
Ice2Faith <Ugex_Savelar@163.com>
2024-05-13 16:20:57 +0800

    feat(jni): easyx api finish

--- i2f-jdk/i2f-native-windows-easyx/src/main/cpp/NativesEasyX/NativesEasyX/NativesEasyX.cpp
+++ i2f-jdk/i2f-native-windows-easyx/src/main/cpp/NativesEasyX/NativesEasyX/NativesEasyX.cpp
@@ -111,7 +111,7 @@ jint height,
 jint flag
 ){
 	HWND hwnd = initgraph(width, height, flag);
-	return toPtr(hwnd); 
+	return toPtr(hwnd);
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -173,7 +173,7 @@ jint height,
 jboolean resize
 ){
 	IMAGE * img = new IMAGE();
-	wchar_t* filePath_ptr=jstring2wchar(env, filePath);
+	wchar_t* filePath_ptr = jstring2wchar(env, filePath);
 	loadimage(img, filePath_ptr, width, height, resize ? true : false);
 	freeWchar(filePath_ptr);
 	return toPtr(img);
@@ -223,7 +223,7 @@ jlong pImage,
 jlong dwRop
 ){
 	IMAGE * img = ptrOf<IMAGE*>(pImage);
-	putimage(dstX, dstY, img, dwRop); 
+	putimage(dstX, dstY, img, dwRop);
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -279,7 +279,7 @@ jint x,
 jint y,
 jint radius
 ){
-	circle(x,y,radius);
+	circle(x, y, radius);
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -324,7 +324,7 @@ jint top,
 jint right,
 jint bottom
 ){
-	rectangle(left,top,right,bottom);
+	rectangle(left, top, right, bottom);
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -392,7 +392,7 @@ jint y,
 jstring str
 ){
 	wchar_t* str_ptr = jstring2wchar(env, str);
-	outtextxy(x,y,str_ptr);
+	outtextxy(x, y, str_ptr);
 	freeWchar(str_ptr);
 }
 
@@ -404,7 +404,7 @@ jfloat h,
 jfloat s,
 jfloat l
 ){
-	COLORREF ret=HSLtoRGB(h, s, l);
+	COLORREF ret = HSLtoRGB(h, s, l);
 	return (jint)ret;
 }
 
@@ -477,9 +477,9 @@ jobject obj,
 jint color
 ){
 	float h = 0, s = 0, l = 0;
-	RGBtoHSL(color,&h,&s,&l);
+	RGBtoHSL(color, &h, &s, &l);
 	jfloat arr[] = { h, s, l };
-	jfloatArray ret=env->NewFloatArray(3);
+	jfloatArray ret = env->NewFloatArray(3);
 	env->SetFloatArrayRegion(ret, 0, 3, arr);
 	return ret;
 }
@@ -503,7 +503,7 @@ JNI_METHOD(getBkColor)(
 JNIEnv* env,
 jobject obj
 ){
-	COLORREF ret=getbkcolor();
+	COLORREF ret = getbkcolor();
 	return (jint)ret;
 }
 
@@ -557,7 +557,7 @@ JNI_METHOD(getRop2)(
 JNIEnv* env,
 jobject obj
 ){
-	int ret=getrop2();
+	int ret = getrop2();
 	return ret;
 }
 
@@ -567,7 +567,7 @@ JNIEnv* env,
 jobject obj,
 jint mode
 ){
-	setrop2(mode); 
+	setrop2(mode);
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -586,7 +586,7 @@ jobject obj
 	float x = 0, y = 0;
 	getaspectratio(&x, &y);
 	jfloat arr[] = { x, y };
-	jfloatArray ret=env->NewFloatArray(2);
+	jfloatArray ret = env->NewFloatArray(2);
 	env->SetFloatArrayRegion(ret, 0, 2, arr);
 	return ret;
 }
@@ -598,7 +598,7 @@ jobject obj,
 jfloat xasp,
 jfloat yasp
 ){
-	setaspectratio(xasp,yasp);
+	setaspectratio(xasp, yasp);
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -616,7 +616,7 @@ jobject obj,
 jint x,
 jint y
 ){
-	setorigin(x,y);
+	setorigin(x, y);
 }
 
 extern "C" JNIEXPORT jint JNICALL
@@ -636,7 +636,7 @@ jint uFormat
 	r.right = right;
 	r.bottom = bottom;
 	wchar_t* text_ptr = jstring2wchar(env, text);
-	int ret=drawtext(text_ptr,&r,uFormat);
+	int ret = drawtext(text_ptr, &r, uFormat);
 	freeWchar(text_ptr);
 	return ret;
 }
@@ -646,7 +646,7 @@ JNI_METHOD(getTextColor)(
 JNIEnv* env,
 jobject obj
 ){
-	COLORREF ret=gettextcolor();
+	COLORREF ret = gettextcolor();
 	return (jint)ret;
 }
 
@@ -657,7 +657,7 @@ jobject obj,
 jstring text
 ){
 	wchar_t* text_ptr = jstring2wchar(env, text);
-	int ret=textheight(text_ptr);
+	int ret = textheight(text_ptr);
 	freeWchar(text_ptr);
 	return ret;
 }
@@ -681,7 +681,7 @@ jobject obj,
 jlong pImage
 ){
 	IMAGE* ptr = ptrOf<IMAGE*>(pImage);
-	DWORD* ret=GetImageBuffer(ptr);
+	DWORD* ret = GetImageBuffer(ptr);
 	return toPtr(ret);
 }
 
@@ -714,7 +714,7 @@ JNIEnv* env,
 jobject obj,
 jlong pImage
 ){
-	HDC ret=GetImageHDC(ptrOf<IMAGE*>(pImage));
+	HDC ret = GetImageHDC(ptrOf<IMAGE*>(pImage));
 	return toPtr(ret);
 }
 
@@ -788,7 +788,7 @@ JNI_METHOD(mouseHit)(
 JNIEnv* env,
 jobject obj
 ){
-	return MouseHit()==true;
+	return MouseHit() == true;
 }
 
 extern "C" JNIEXPORT jstring JNICALL
@@ -796,7 +796,7 @@ JNI_METHOD(getMouseMsg)(
 JNIEnv* env,
 jobject obj
 ){
-	MOUSEMSG msg=GetMouseMsg();
+	MOUSEMSG msg = GetMouseMsg();
 	wchar_t buff[1024] = { 0 };
 	swprintf(buff, L"uMsg:%d;#;mkCtrl:%d;#;mkShift:%d;#;mkLButton:%d;#;mkMButton:^%d;#;mkRButton:%d;#;x:%d;#;y:%d;#;wheel:%d",
 		msg.uMsg,
@@ -804,7 +804,7 @@ jobject obj
 		msg.mkShift ? 1 : 0,
 		msg.mkLButton ? 1 : 0,
 		msg.mkMButton ? 1 : 0,
-		msg.mkRButton ? 1 : 0, 
+		msg.mkRButton ? 1 : 0,
 		msg.x,
 		msg.y,
 		msg.wheel);
@@ -816,8 +816,8 @@ JNI_METHOD(getEasyXVer)(
 JNIEnv* env,
 jobject obj
 ){
-	wchar_t* ptr=GetEasyXVer();
-	return wchar2jstring(env,ptr);
+	wchar_t* ptr = GetEasyXVer();
+	return wchar2jstring(env, ptr);
 }
 
 extern "C" JNIEXPORT jlong JNICALL
@@ -841,14 +841,14 @@ jint height,
 jboolean onlyOk
 ){
 	wchar_t buff[4096] = { 0 };
-	wchar_t* prompt_ptr = jstring2wchar(env,prompt);
+	wchar_t* prompt_ptr = jstring2wchar(env, prompt);
 	wchar_t* title_ptr = jstring2wchar(env, title);
 	wchar_t* defaultText_ptr = jstring2wchar(env, defaultText);
-	bool ok=InputBox(buff, 4096, prompt_ptr, title_ptr, defaultText_ptr, width, height, (onlyOk==true?true:false));
+	bool ok = InputBox(buff, 4096, prompt_ptr, title_ptr, defaultText_ptr, width, height, (onlyOk == true ? true : false));
 	freeWchar(prompt_ptr);
 	freeWchar(title_ptr);
 	freeWchar(defaultText_ptr);
-	if (ok==false){
+	if (ok == false){
 		return nullptr;
 	}
 	return wchar2jstring(env, buff);
@@ -897,7 +897,7 @@ JNIEnv* env,
 jobject obj
 ){
 	int ret = getmaxy();
-	return (jint)ret; 
+	return (jint)ret;
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -919,14 +919,14 @@ jint      lfQuality,
 jint      lfPitchAndFamily,
 jstring     lfFaceName
 ){
-	LOGFONTW ft = { 0 }; 
+	LOGFONTW ft = { 0 };
 	ft.lfHeight = lfHeight;
 	ft.lfWidth = lfWidth;
 	ft.lfEscapement = lfEscapement;
 	ft.lfOrientation = lfOrientation;
 	ft.lfWeight = lfWeight;
-	ft.lfItalic = lfItalic?TRUE:FALSE;
-	ft.lfUnderline = lfUnderline?TRUE:FALSE;
+	ft.lfItalic = lfItalic ? TRUE : FALSE;
+	ft.lfUnderline = lfUnderline ? TRUE : FALSE;
 	ft.lfStrikeOut = lfStrikeOut ? TRUE : FALSE;
 	ft.lfCharSet = lfCharSet;
 	ft.lfOutPrecision = lfOutPrecision;
@@ -935,7 +935,7 @@ jstring     lfFaceName
 	ft.lfPitchAndFamily = lfPitchAndFamily;
 	wchar_t* faceName_ptr = jstring2wchar(env, lfFaceName);
 	if (faceName_ptr != NULL){
-		lstrcpyW(ft.lfFaceName,faceName_ptr);
+		lstrcpyW(ft.lfFaceName, faceName_ptr);
 	}
 	freeWchar(faceName_ptr);
 	settextstyle(&ft);
@@ -962,7 +962,7 @@ jint      lfPitchAndFamily
 ){
 	wchar_t* faceName_ptr = jstring2wchar(env, lfFaceName);
 	settextstyle(lfHeight, lfWidth, faceName_ptr, lfEscapement, lfOrientation,
-		lfWeight, lfItalic?true:false, lfUnderline?true:false, lfStrikeOut?true:false, lfCharSet, lfOutPrecision,
+		lfWeight, lfItalic ? true : false, lfUnderline ? true : false, lfStrikeOut ? true : false, lfCharSet, lfOutPrecision,
 		lfClipPrecision, lfQuality, lfPitchAndFamily);
 	freeWchar(faceName_ptr);
 }
@@ -1002,6 +1002,27 @@ jstring     lfFaceName
 	freeWchar(faceName_ptr);
 }
 
+static jstring stringify_LOGFONT(JNIEnv* env, LOGFONTW* ft){
+	wchar_t buff[2048] = { 0 };
+	swprintf(buff, L"lfHeight:%d;#;lfWidth:%d;#;lfEscapement:%d;#;lfOrientation:%d;#;lfWeight:%d;#;lfItalic:%d;#;lfUnderline:%d;#;lfStrikeOut:%d;#;lfCharSet:%d;#;lfOutPrecision:%d;#;lfClipPrecision:%d;#;lfQuality:%d;#;lfPitchAndFamily:%d;#;lfFaceName:%s",
+		ft->lfHeight,
+		ft->lfWidth,
+		ft->lfEscapement,
+		ft->lfOrientation,
+		ft->lfWeight,
+		ft->lfItalic == TRUE ? 1 : 0,
+		ft->lfUnderline == TRUE ? 1 : 0,
+		ft->lfStrikeOut == TRUE ? 1 : 0,
+		ft->lfCharSet,
+		ft->lfOutPrecision,
+		ft->lfClipPrecision,
+		ft->lfQuality,
+		ft->lfPitchAndFamily,
+		ft->lfFaceName
+		);
+	return wchar2jstring(env, buff);
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 JNI_METHOD(getTxtStyle)(
 JNIEnv* env,
@@ -1009,22 +1030,542 @@ jobject obj
 ){
 	LOGFONTW ft = { 0 };
 	gettextstyle(&ft);
-	wchar_t buff[2048] = { 0 };
-		swprintf(buff, L"lfHeight:%d;#;lfWidth:%d;#;lfEscapement:%d;#;lfOrientation:%d;#;lfWeight:%d;#;lfItalic:%d;#;lfUnderline:%d;#;lfStrikeOut:%d;#;lfCharSet:%d;#;lfOutPrecision:%d;#;lfClipPrecision:%d;#;lfQuality:%d;#;lfPitchAndFamily:%d;#;lfFaceName:%s",
-		ft.lfHeight,
-		ft.lfWidth,
-		ft.lfEscapement,
-		ft.lfOrientation,
-		ft.lfWeight,
-		ft.lfItalic==TRUE?1:0,
-		ft.lfUnderline==TRUE?1:0,
-		ft.lfStrikeOut==TRUE?1:0,
-		ft.lfCharSet,
-		ft.lfOutPrecision,
-		ft.lfClipPrecision,
-		ft.lfQuality,
-		ft.lfPitchAndFamily,
-		ft.lfFaceName
-		);
+	return stringify_LOGFONT(env, &ft);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(bar3d)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jint depth,
+jboolean topFlag
+){
+	bar3d(left, top, right, bottom, depth, (topFlag == true ? true : false));
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(drawPoly)(
+JNIEnv* env,
+jobject obj,
+jintArray points
+){
+	int len = env->GetArrayLength(points);
+	jint* arr = env->GetIntArrayElements(points, NULL);
+	int* args = (int*)malloc(sizeof(int)*len);
+	for (int i = 0; i < len; i++){
+		args[i] = arr[i];
+	}
+	drawpoly(len / 2, args);
+	env->ReleaseIntArrayElements(points, arr, 0);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(fillPoly)(
+JNIEnv* env,
+jobject obj,
+jintArray points
+){
+	int len = env->GetArrayLength(points);
+	jint* arr = env->GetIntArrayElements(points, NULL);
+	int* args = (int*)malloc(sizeof(int)*len);
+	for (int i = 0; i < len; i++){
+		args[i] = arr[i];
+	}
+	fillpoly(len / 2, args);
+	env->ReleaseIntArrayElements(points, arr, 0);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(setWriteMode)(
+JNIEnv* env,
+jobject obj,
+jint mode
+){
+	setwritemode(mode);
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+JNI_METHOD(getFillStyle)(
+JNIEnv* env,
+jobject obj
+){
+	FILLSTYLE fs;
+	getfillstyle(&fs);
+	wchar_t buff[256] = { 0 };
+
+	swprintf(buff, L"style:%d;#;hatch:%d;#;ppattern:%l64d", fs.style, fs.hatch, fs.ppattern);
+
 	return wchar2jstring(env, buff);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(setFillStyle)(
+JNIEnv* env,
+jobject obj,
+jint style,
+jint hatch,
+jlong pImage
+){
+	IMAGE* ppattern = ptrOf<IMAGE*>(pImage);
+	setfillstyle(style, hatch, ppattern);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(setFillStylePattern8x8)(
+JNIEnv* env,
+jobject obj,
+jbyteArray ppattern8x8
+){
+	byte arr[8] = { 0 };
+	jbyte* bts = env->GetByteArrayElements(ppattern8x8, NULL);
+	for (int i = 0; i < 8; i++){
+		arr[i] = bts[i];
+	}
+	setfillstyle(arr);
+	env->ReleaseByteArrayElements(ppattern8x8, bts, 0);
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+JNI_METHOD(getLineStyle)(
+JNIEnv* env,
+jobject obj
+){
+	LINESTYLE ls;
+	getlinestyle(&ls);
+	wchar_t buff[1024] = { 0 };
+	wchar_t tmp[50] = { 0 };
+
+	swprintf(buff, L"style:%d;#;thickness:%d;#;userStyle:", ls.style, ls.thickness);
+	DWORD* arr = ls.puserstyle;
+	for (int i = 0; i < ls.userstylecount; i++){
+		if (i>0){
+			lstrcatW(buff, L",");
+		}
+		ZeroMemory(tmp, sizeof(tmp));
+		swprintf(tmp, L"%d", arr[i]);
+		lstrcatW(buff, tmp);
+	}
+	return wchar2jstring(env, buff);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(setLineStyle)(
+JNIEnv* env,
+jobject obj,
+jint style,
+jint thickness,
+jintArray userType
+){
+	DWORD* puserStyle = NULL;
+	int len = 0;
+	if (userType != nullptr){
+		len = env->GetArrayLength(userType);
+		jint* elems = env->GetIntArrayElements(userType, NULL);
+		puserStyle = (DWORD*)malloc(sizeof(DWORD)*len);
+		for (int i = 0; i < len; i++){
+			puserStyle[i] = elems[i];
+		}
+		env->ReleaseIntArrayElements(userType, elems, 0);
+	}
+	setlinestyle(style, thickness, puserStyle, len);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(arc)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jdouble startRadian,
+jdouble endRadian
+){
+	arc(left, top, right, bottom, startRadian, endRadian);
+}
+
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(ellipse)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom
+){
+	ellipse(left, top, right, bottom);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(fillEllipse)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom
+){
+	fillellipse(left, top, right, bottom);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(clearEllipse)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom
+){
+	clearellipse(left, top, right, bottom);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(solidEllipse)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom
+){
+	solidellipse(left, top, right, bottom);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(pie)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jdouble startRadian,
+jdouble endRadian
+){
+	pie(left, top, right, bottom, startRadian, endRadian);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(fillPie)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jdouble startRadian,
+jdouble endRadian
+){
+	fillpie(left, top, right, bottom, startRadian, endRadian);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(clearPie)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jdouble startRadian,
+jdouble endRadian
+){
+	clearpie(left, top, right, bottom, startRadian, endRadian);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(solidPie)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jdouble startRadian,
+jdouble endRadian
+){
+	solidpie(left, top, right, bottom, startRadian, endRadian);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(polygon)(
+JNIEnv* env,
+jobject obj,
+jintArray points
+){
+	int len = env->GetArrayLength(points);
+	jint* arr = env->GetIntArrayElements(points, NULL);
+	POINT* args = (POINT*)malloc(sizeof(POINT)*(len / 2));
+	for (int i = 0; i < len / 2; i++){
+		args[i].x = arr[i * 2];
+		args[i].y = arr[i * 2 + 1];
+	}
+	polygon(args, len / 2);
+	free(args);
+	env->ReleaseIntArrayElements(points, arr, 0);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(fillPolygon)(
+JNIEnv* env,
+jobject obj,
+jintArray points
+){
+	int len = env->GetArrayLength(points);
+	jint* arr = env->GetIntArrayElements(points, NULL);
+	POINT* args = (POINT*)malloc(sizeof(POINT)*(len / 2));
+	for (int i = 0; i < len / 2; i++){
+		args[i].x = arr[i * 2];
+		args[i].y = arr[i * 2 + 1];
+	}
+	fillpolygon(args, len / 2);
+	free(args);
+	env->ReleaseIntArrayElements(points, arr, 0);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(clearPolygon)(
+JNIEnv* env,
+jobject obj,
+jintArray points
+){
+	int len = env->GetArrayLength(points);
+	jint* arr = env->GetIntArrayElements(points, NULL);
+	POINT* args = (POINT*)malloc(sizeof(POINT)*(len / 2));
+	for (int i = 0; i < len / 2; i++){
+		args[i].x = arr[i * 2];
+		args[i].y = arr[i * 2 + 1];
+	}
+	clearpolygon(args, len / 2);
+	free(args);
+	env->ReleaseIntArrayElements(points, arr, 0);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(solidPolygon)(
+JNIEnv* env,
+jobject obj,
+jintArray points
+){
+	int len = env->GetArrayLength(points);
+	jint* arr = env->GetIntArrayElements(points, NULL);
+	POINT* args = (POINT*)malloc(sizeof(POINT)*(len / 2));
+	for (int i = 0; i < len / 2; i++){
+		args[i].x = arr[i * 2];
+		args[i].y = arr[i * 2 + 1];
+	}
+	solidpolygon(args, len / 2);
+	free(args);
+	env->ReleaseIntArrayElements(points, arr, 0);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(roundRect)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jint ellipseWidth,
+jint ellipseHeight
+){
+	roundrect(left, top, right, bottom, ellipseWidth, ellipseHeight);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(fillRoundRect)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jint ellipseWidth,
+jint ellipseHeight
+){
+	fillroundrect(left, top, right, bottom, ellipseWidth, ellipseHeight);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(clearRoundRect)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jint ellipseWidth,
+jint ellipseHeight
+){
+	clearroundrect(left, top, right, bottom, ellipseWidth, ellipseHeight);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(solidRoundRect)(
+JNIEnv* env,
+jobject obj,
+jint left,
+jint top,
+jint right,
+jint bottom,
+jint ellipseWidth,
+jint ellipseHeight
+){
+	solidroundrect(left, top, right, bottom, ellipseWidth, ellipseHeight);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(floodFill)(
+JNIEnv* env,
+jobject obj,
+jint x,
+jint y,
+jint color,
+jint fillType
+){
+	floodfill(x, y, (COLORREF)color, fillType);
+}
+
+extern "C" JNIEXPORT jint JNICALL
+JNI_METHOD(getPixel)(
+JNIEnv* env,
+jobject obj,
+jint x,
+jint y
+){
+	COLORREF ret=getpixel(x, y);
+	return (jint)ret;
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(putPixel)(
+JNIEnv* env,
+jobject obj,
+jint x,
+jint y,
+jint color
+){
+	putpixel(x, y, color);
+}
+
+
+extern "C" JNIEXPORT jint JNICALL
+JNI_METHOD(getX)(
+JNIEnv* env,
+jobject obj
+){
+	return getx();
+}
+
+extern "C" JNIEXPORT jint JNICALL
+JNI_METHOD(getY)(
+JNIEnv* env,
+jobject obj
+){
+	return gety();
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(line)(
+JNIEnv* env,
+jobject obj,
+jint x1,
+jint y1,
+jint x2,
+jint y2
+){
+	line(x1, y1, x2, y2);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(lineRel)(
+JNIEnv* env,
+jobject obj,
+jint dx,
+jint dy
+){
+	linerel(dx,dy);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(lineTo)(
+JNIEnv* env,
+jobject obj,
+jint x,
+jint y
+){
+	lineto(x, y);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(moveRel)(
+JNIEnv* env,
+jobject obj,
+jint dx,
+jint dy
+){
+	moverel(dx, dy);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(moveTo)(
+JNIEnv* env,
+jobject obj,
+jint x,
+jint y
+){
+	moveto(x, y);
+}
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(polyBezier)(
+JNIEnv* env,
+jobject obj,
+jintArray points
+){
+	int len = env->GetArrayLength(points);
+	jint* arr = env->GetIntArrayElements(points, NULL);
+	POINT* args = (POINT*)malloc(sizeof(POINT)*(len / 2));
+	for (int i = 0; i < len / 2; i++){
+		args[i].x = arr[i * 2];
+		args[i].y = arr[i * 2 + 1];
+	}
+	polybezier(args, len / 2);
+	free(args);
+	env->ReleaseIntArrayElements(points, arr, 0);
+}
+
+
+extern "C" JNIEXPORT void JNICALL
+JNI_METHOD(polyLine)(
+JNIEnv* env,
+jobject obj,
+jintArray points
+){
+	int len = env->GetArrayLength(points);
+	jint* arr = env->GetIntArrayElements(points, NULL);
+	POINT* args = (POINT*)malloc(sizeof(POINT)*(len / 2));
+	for (int i = 0; i < len / 2; i++){
+		args[i].x = arr[i * 2];
+		args[i].y = arr[i * 2 + 1];
+	}
+	polyline(args, len / 2);
+	free(args);
+	env->ReleaseIntArrayElements(points, arr, 0);
 }
---
+++ i2f-jdk/i2f-native-windows/src/main/java/i2f/natives/windows/consts/WinGdiBrushStyle.java
@@ -0,0 +1,21 @@
+package i2f.natives.windows.consts;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/5/13 14:55
+ * @desc
+ */
+public interface WinGdiBrushStyle {
+    /* Brush Styles */
+    int BS_SOLID = 0;
+    int BS_NULL = 1;
+    int BS_HOLLOW = BS_NULL;
+    int BS_HATCHED = 2;
+    int BS_PATTERN = 3;
+    int BS_INDEXED = 4;
+    int BS_DIBPATTERN = 5;
+    int BS_DIBPATTERNPT = 6;
+    int BS_PATTERN8X8 = 7;
+    int BS_DIBPATTERN8X8 = 8;
+    int BS_MONOPATTERN = 9;
+}
