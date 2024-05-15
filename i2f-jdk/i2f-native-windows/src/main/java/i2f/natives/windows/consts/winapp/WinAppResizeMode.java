cc091f184be7edd5e030e208cc615a8865a1f12b
Ice2Faith <Ugex_Savelar@163.com>
2024-05-15 23:27:30 +0800

    refactor(jni): windows win32 app jni

---
+++ i2f-jdk/i2f-native-windows/src/main/java/i2f/natives/windows/consts/winapp/WinAppResizeMode.java
@@ -0,0 +1,12 @@
+package i2f.natives.windows.consts.winapp;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/5/15 21:09
+ * @desc
+ */
+public interface WinAppResizeMode {
+    int RESIZE_MODE_ADAPT = 0;
+    int RESIZE_MODE_CUT = 1;
+    int RESIZE_MODE_FIXED = 2;
+}
---
+++ i2f-jdk/i2f-native-windows/src/main/java/i2f/natives/windows/types/gdi/HRgn.java
@@ -0,0 +1,18 @@
+package i2f.natives.windows.types.gdi;
+
+import i2f.natives.core.Ptr;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/5/10 8:32
+ * @desc
+ */
+public class HRgn extends HGdiObj {
+    public HRgn(long ptr) {
+        super(ptr);
+    }
+
+    public HRgn(Ptr ptr) {
+        super(ptr);
+    }
+}
