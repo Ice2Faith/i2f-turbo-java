df6cb8aa44e20446da0b15821ed318b3f5dc082d
Ice2Faith <Ugex_Savelar@163.com>
2024-05-23 21:13:36 +0800

    feat(bc): jce on bc

---
+++ i2f-extension/i2f-jce-bc/src/main/java/i2f/jce/bc/supports/MessageDigestAlgorithm.java
@@ -0,0 +1,50 @@
+package i2f.jce.bc.supports;
+
+import i2f.jce.bc.BcProvider;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/3/27 14:31
+ * @desc https://www.bouncycastle.org/documentation/specification_interoperability/#algorithms-and-key-types
+ */
+public enum MessageDigestAlgorithm {
+
+
+    MD5("MD5"),
+
+    SHA_1("SHA-1"),
+    SHA224("SHA224"),
+    SHA256("SHA256"),
+    SHA384("SHA384"),
+    SHA512("SHA512"),
+
+    SHA512_224("SHA512/224"),
+    SHA512_256("SHA512/256"),
+
+    SHAKE_128("SHAKE-128"),
+    SHAKE_256("SHAKE-256"),
+
+    SHA3_224("SHA3-224"),
+    SHA3_256("SHA3-256"),
+    SHA3_384("SHA3-384"),
+    SHA3_512("SHA3-512"),
+
+    SM3("SM3"),
+    Tiger("Tiger"),
+    Whirlpool("Whirlpool"),
+    ;
+
+    static {
+        BcProvider.registryProvider();
+    }
+
+    private String text;
+
+    MessageDigestAlgorithm(String text) {
+        this.text = text;
+    }
+
+    public String text() {
+        return this.text;
+    }
+}
