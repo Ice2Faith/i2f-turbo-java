cb9c4e5890044cbc584e7ed1b9875bbd164a7507
Ice2Faith <Ugex_Savelar@163.com>
2024-06-26 11:47:49 +0800

    feat(cache): cache

---
+++ i2f-jdk/i2f-cache/src/main/java/i2f/cache/expire/IStringObjectExpireCache.java
@@ -0,0 +1,10 @@
+package i2f.cache.expire;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/6/25 13:58
+ * @desc
+ */
+public interface IStringObjectExpireCache extends IExpireCache<String, Object> {
+
+}
