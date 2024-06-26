1a0017e9baf01ebdbbc4b03cf3cd6a7675aaf7d7
Ice2Faith <Ugex_Savelar@163.com>
2024-06-26 20:51:59 +0800

    feat(swagger2): swagger2 starter

---
+++ i2f-springboot/i2f-springboot-swagger2-starter/src/main/java/i2f/springboot/swagger2/Swagger2AutoConfiguration.java
@@ -0,0 +1,67 @@
+package i2f.springboot.swagger2;
+
+import com.google.common.base.Predicates;
+import i2f.springboot.swagger2.properties.Swagger2ApiInfoProperties;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/6/26 20:25
+ * @desc
+ */
+@Slf4j
+@Data
+@EnableConfigurationProperties({
+        Swagger2ApiInfoProperties.class
+})
+@Import({
+        Swagger2RestfulConfiguration.class
+})
+@ConfigurationProperties(prefix = "i2f.swagger2.apis")
+public class Swagger2AutoConfiguration {
+
+    @ConditionalOnMissingBean(ApiInfo.class)
+    @Bean
+    public ApiInfo apiInfo(Swagger2ApiInfoProperties properties) {
+        log.info("SwaggerConfig api info config.");
+        return new ApiInfoBuilder().title(properties.getTitle())
+                .description(properties.getDescription())
+                .contact(new Contact(null, null, null))
+                .license(properties.getLicense())
+                .licenseUrl(properties.getLicenseUrl())
+                .version(properties.getVersion())
+                .build();
+    }
+
+    @ConditionalOnExpression("${i2f.swagger2.apis.all.enable:true}")
+    @Bean
+    public Docket allApi(ApiInfo apiInfo) {
+        log.info("SwaggerConfig all api config.");
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("10-all")
+                .select()
+                .apis(RequestHandlerSelectors
+                        .any())
+                .paths(PathSelectors.ant("/**"))
+                .build()
+                .apiInfo(apiInfo);
+    }
+
+
+}
---
+++ i2f-springboot/i2f-springboot-swagger2-starter/src/main/java/i2f/springboot/swagger2/properties/Swagger2ApiInfoProperties.java
@@ -0,0 +1,19 @@
+package i2f.springboot.swagger2.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/6/26 20:25
+ * @desc
+ */
+@Data
+@ConfigurationProperties(prefix = "i2f.swagger2.api-info")
+public class Swagger2ApiInfoProperties {
+    private String title="Micro-Service Project Api";
+    private String description="Micro-Service project.";
+    private String license="Apache 2.0";
+    private String licenseUrl="http://www.apache.org/licenses/LICENSE-2.0.html";
+    private String version="1.0.0";
+}
