38ca221985007f3f8f1b0382c594a30845aae47c
Ice2Faith <Ugex_Savelar@163.com>
2024-06-26 17:58:40 +0800

    feat(xxljob): xxl-job starter

---
+++ i2f-springboot/i2f-springboot-xxl-job-starter/src/main/java/i2f/springboot/xxljob/XxlJobAutoConfiguration.java
@@ -0,0 +1,57 @@
+package i2f.springboot.xxljob;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import i2f.springboot.xxljob.properties.XxlJobProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * xxl-job config
+ *
+ * @author xuxueli 2017-04-28
+ */
+@EnableConfigurationProperties({
+        XxlJobProperties.class
+})
+@Data
+@Slf4j
+@NoArgsConstructor
+public class XxlJobAutoConfiguration {
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor(XxlJobProperties properties) {
+        log.info(">>>>>>>>>>> xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(properties.getAdmin().getAddresses());
+        xxlJobSpringExecutor.setAppname(properties.getExecutor().getAppname());
+        xxlJobSpringExecutor.setAddress(properties.getExecutor().getAddress());
+        xxlJobSpringExecutor.setIp(properties.getExecutor().getIp());
+        xxlJobSpringExecutor.setPort(properties.getExecutor().getPort());
+        xxlJobSpringExecutor.setAccessToken(properties.getAccessToken());
+        xxlJobSpringExecutor.setLogPath(properties.getExecutor().getLogPath());
+        xxlJobSpringExecutor.setLogRetentionDays(properties.getExecutor().getLogRetentionDays());
+
+        return xxlJobSpringExecutor;
+    }
+
+    /**
+     * 针对多网卡、容器内部署等情况，可借助 "spring-cloud-commons" 提供的 "InetUtils" 组件灵活定制注册IP；
+     *
+     *      1、引入依赖：
+     *          <dependency>
+     *             <groupId>org.springframework.cloud</groupId>
+     *             <artifactId>spring-cloud-commons</artifactId>
+     *             <version>${version}</version>
+     *         </dependency>
+     *
+     *      2、配置文件，或者容器启动变量
+     *          spring.cloud.inetutils.preferred-networks: 'xxx.xxx.xxx.'
+     *
+     *      3、获取IP
+     *          String ip_ = inetUtils.findFirstNonLoopbackHostInfo().getIpAddress();
+     */
+
+}
