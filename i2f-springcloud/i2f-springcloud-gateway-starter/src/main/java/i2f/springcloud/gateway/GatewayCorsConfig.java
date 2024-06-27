d7d0b9afbfd7481246ab07fc564e0418e8e6e2ec
Ice2Faith <Ugex_Savelar@163.com>
2024-06-27 17:54:47 +0800

    feat(gateway): gateway starter

---
+++ i2f-springcloud/i2f-springcloud-gateway-starter/src/main/java/i2f/springcloud/gateway/GatewayCorsConfig.java
@@ -0,0 +1,50 @@
+package i2f.springcloud.gateway;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.Arrays;
+
+/**
+ * @author ltb
+ * @date 2022/6/12 18:45
+ * @desc
+ */
+@Slf4j
+@Data
+@ConditionalOnExpression("${i2f.springcloud.gateway.cors.enable:true}")
+@ConfigurationProperties(prefix = "i2f.springcloud.gateway.cors")
+public class GatewayCorsConfig {
+    private String urlPatten = "/**";
+
+    private String allowOrigins = "*";
+    private String allowMethods = "*";
+    private String allowHeaders = "*";
+
+    private boolean allowCredentials = true;
+    private long maxAge = 6000L;
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedMethods(Arrays.asList(allowMethods.split(",")));
+        config.setAllowedOrigins(Arrays.asList(allowOrigins.split(",")));
+        config.setAllowedHeaders(Arrays.asList(allowHeaders.split(",")));
+
+        config.setAllowCredentials(allowCredentials);
+
+        config.setMaxAge(maxAge);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration(urlPatten, config);
+
+        log.info("GatewayCorsConfig CorsWebFilter config done.");
+        return new CorsWebFilter(source);
+    }
+}
---
+++ i2f-springcloud/i2f-springcloud-gateway-starter/src/main/java/i2f/springcloud/gateway/filters/global/TimeStatistic.java
@@ -0,0 +1,85 @@
+package i2f.springcloud.gateway.filters.global;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/4/1 17:28
+ * @desc
+ */
+public class TimeStatistic {
+    protected volatile AtomicLong curr = new AtomicLong(0);
+    protected volatile AtomicLong count = new AtomicLong(0);
+    protected volatile AtomicLong sum = new AtomicLong(0);
+    protected volatile AtomicLong max = new AtomicLong(Long.MIN_VALUE);
+    protected volatile AtomicLong min = new AtomicLong(Long.MAX_VALUE);
+
+    public TimeStatistic reset() {
+        curr.set(0);
+        count.set(0);
+        sum.set(0);
+        max.set(Long.MIN_VALUE);
+        min.set(Long.MAX_VALUE);
+        return this;
+    }
+
+    public TimeStatistic add(long diff) {
+        curr.set(diff);
+        count.incrementAndGet();
+        sum.addAndGet(diff);
+        if (diff > max.get()) {
+            max.set(diff);
+        }
+        if (diff < min.get()) {
+            min.set(diff);
+        }
+        return this;
+    }
+
+    public AtomicLong getCount() {
+        return count;
+    }
+
+    public void setCount(AtomicLong count) {
+        this.count = count;
+    }
+
+    public AtomicLong getSum() {
+        return sum;
+    }
+
+    public void setSum(AtomicLong sum) {
+        this.sum = sum;
+    }
+
+    public AtomicLong getMax() {
+        return max;
+    }
+
+    public void setMax(AtomicLong max) {
+        this.max = max;
+    }
+
+    public AtomicLong getMin() {
+        return min;
+    }
+
+    public void setMin(AtomicLong min) {
+        this.min = min;
+    }
+
+    public String formatString() {
+        return String.format("curr: %5d, avg: %8.2f, min: %5d, max: %5d, count: %5d",
+                curr.get(), (sum.get() * 1.0 / count.get()), min.get(), max.get(), count.get());
+    }
+
+    @Override
+    public String toString() {
+        return "TimeStatistic{" +
+                "count=" + count +
+                ", sum=" + sum +
+                ", max=" + max +
+                ", min=" + min +
+                '}';
+    }
+}
