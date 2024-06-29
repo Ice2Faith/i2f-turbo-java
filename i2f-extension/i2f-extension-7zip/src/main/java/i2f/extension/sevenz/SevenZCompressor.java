47fdae5d1f73c84f9fe6173ac3bdf2e35ce1b62f
Ice2Faith <Ugex_Savelar@163.com>
2024-06-29 18:21:29 +0800

    feat(compress): compress zip/7z with password

---
+++ i2f-extension/i2f-extension-7zip/src/main/java/i2f/extension/sevenz/SevenZCompressor.java
@@ -0,0 +1,67 @@
+package i2f.extension.sevenz;
+
+import i2f.compress.data.CompressBindData;
+import i2f.compress.impl.AbsCompressor;
+import i2f.extension.sevenz.callbak.CompressSevenZCallback;
+import i2f.extension.sevenz.callbak.ExtractSevenZCallback;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.sf.sevenzipjbinding.*;
+import net.sf.sevenzipjbinding.impl.RandomAccessFileInStream;
+import net.sf.sevenzipjbinding.impl.RandomAccessFileOutStream;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Collection;
+import java.util.function.BiConsumer;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/6/29 17:16
+ * @desc
+ */
+@Data
+@NoArgsConstructor
+public class SevenZCompressor extends AbsCompressor {
+    private String password;
+
+    public SevenZCompressor(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public void compressBindData(File output, Collection<CompressBindData> inputs) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile(output, "rw");
+        IOutCreateArchive7z outArchive = SevenZip.openOutArchive7z();
+
+        if (password != null && !password.isEmpty()) {
+            outArchive.setHeaderEncryption(true);
+        }
+
+        // Create archive
+        outArchive.createArchive(new RandomAccessFileOutStream(raf),
+                inputs.size(), new CompressSevenZCallback(password, inputs));
+
+        outArchive.close();
+        raf.close();
+    }
+
+    @Override
+    public void release(File input, File output, BiConsumer<CompressBindData, File> consumer) throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(input, "r");
+        IInArchive inArchive = SevenZip.openInArchive(null, // autodetect archive type
+                new RandomAccessFileInStream(randomAccessFile),password);
+
+        int[] in = new int[inArchive.getNumberOfItems()];
+        for (int i = 0; i < in.length; i++) {
+            in[i] = i;
+        }
+        inArchive.extract(in, false, // Non-test mode
+                new ExtractSevenZCallback(password,inArchive,output));
+
+        inArchive.close();
+
+        randomAccessFile.close();
+    }
+}
---
+++ i2f-extension/i2f-extension-7zip/src/main/java/i2f/extension/sevenz/callbak/ExtractSevenZCallback.java
@@ -0,0 +1,86 @@
+package i2f.extension.sevenz.callbak;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.sf.sevenzipjbinding.*;
+import net.sf.sevenzipjbinding.impl.RandomAccessFileOutStream;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/6/29 17:39
+ * @desc
+ */
+@Data
+@NoArgsConstructor
+public class ExtractSevenZCallback implements IArchiveExtractCallback, ICryptoGetTextPassword {
+    private String password;
+    private IInArchive inArchive;
+    private File output;
+    private ExtractOperationResult operationResult;
+    private long total;
+    private long completed;
+
+    public ExtractSevenZCallback(String password, IInArchive inArchive, File output) {
+        this.password = password;
+        this.inArchive = inArchive;
+        this.output = output;
+    }
+
+    public ExtractSevenZCallback(IInArchive inArchive, File output) {
+        this.inArchive = inArchive;
+        this.output = output;
+    }
+
+    @Override
+    public ISequentialOutStream getStream(int i, ExtractAskMode extractAskMode) throws SevenZipException {
+        if(extractAskMode != ExtractAskMode.EXTRACT){
+            return null;
+        }
+        boolean isFolder = (Boolean) inArchive
+                .getProperty(i, PropID.IS_FOLDER);
+        String name = (String)inArchive.getProperty(i, PropID.PATH);
+        File file=new File(output,name);
+        if (isFolder) {
+            file.mkdirs();
+            return null;
+        }
+
+        try{
+            if(!file.getParentFile().exists()){
+                file.getParentFile().mkdirs();
+            }
+            RandomAccessFile accessFile=new RandomAccessFile(file,"rw");
+            return new RandomAccessFileOutStream(accessFile);
+        }catch(Exception e){
+            throw new SevenZipException(e.getMessage(),e);
+        }
+    }
+
+    @Override
+    public void prepareOperation(ExtractAskMode extractAskMode) throws SevenZipException {
+
+    }
+
+    @Override
+    public void setOperationResult(ExtractOperationResult extractOperationResult) throws SevenZipException {
+        this.operationResult=extractOperationResult;
+    }
+
+    @Override
+    public String cryptoGetTextPassword() throws SevenZipException {
+        return password;
+    }
+
+    @Override
+    public void setTotal(long l) throws SevenZipException {
+        this.total=l;
+    }
+
+    @Override
+    public void setCompleted(long l) throws SevenZipException {
+        this.completed=l;
+    }
+}
