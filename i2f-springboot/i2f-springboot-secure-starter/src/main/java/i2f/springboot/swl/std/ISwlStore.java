31cca9de463e2f1df40acd5f8052cf1700c8c47b
Ice2Faith <Ugex_Savelar@163.com>
2024-07-09 21:15:46 +0800

    feat(secure): secure

---
+++ i2f-springboot/i2f-springboot-secure-starter/src/main/java/i2f/springboot/swl/std/ISwlStore.java
@@ -0,0 +1,23 @@
+package i2f.springboot.swl.std;
+
+import i2f.jce.std.encrypt.asymmetric.key.AsymKeyPair;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/7/9 20:35
+ * @desc
+ */
+public interface ISwlStore {
+    AsymKeyPair getSelfKeyPair();
+    void setSelfKeyPair(AsymKeyPair selfKeyPair);
+    String getSelfPrivateKey(String selfAsymSign);
+
+    String getOtherPublicKey(String otherAsymSign);
+    void setOtherPublicKey(String otherAsymSign, String publicKey);
+
+    String getTargetPublicKey(String targetName);
+    void setTargetPublicKey(String targetName, String publicKey);
+
+    boolean containsNonce(String nonce);
+    void setNonce(String nonce,long timeoutSeconds);
+}
