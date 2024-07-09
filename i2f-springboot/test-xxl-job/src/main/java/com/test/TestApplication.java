919a4c0e38c525b2fa0955b9f1b320355462d7f4
Ice2Faith <Ugex_Savelar@163.com>
2024-07-09 18:31:05 +0800

    feat(properties): properties

---
+++ i2f-springboot/test-xxl-job/src/main/java/com/test/TestApplication.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/7/4 19:03
+ * @desc
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args){
+        SpringApplication.run(TestApplication.class,args);
+    }
+}
