020386b149558db9f977712684dc9021de2fb4a6
Ice2Faith <Ugex_Savelar@163.com>
2024-07-10 15:20:48 +0800

    feat(secure): secure starter

--- i2f-springboot/i2f-springboot-secure-starter/src/main/java/i2f/springboot/swl/core/SwlTransfer.java
+++ i2f-springboot/i2f-springboot-secure-starter/src/main/java/i2f/springboot/swl/core/SwlTransfer.java
@@ -6,6 +6,7 @@ import i2f.cache.impl.expire.ObjectExpireCacheWrapper;
 import i2f.clock.SystemClock;
 import i2f.jce.std.encrypt.asymmetric.key.AsymKeyPair;
 import i2f.springboot.swl.consts.SwlCode;
+import i2f.springboot.swl.data.SwlContext;
 import i2f.springboot.swl.data.SwlData;
 import i2f.springboot.swl.data.SwlHeader;
 import i2f.springboot.swl.exception.SwlException;
@@ -33,7 +34,8 @@ import java.util.function.Supplier;
 public class SwlTransfer {
     public static final String SELF_KEY_PAIR_CURRENT_KEY = "swl:key:self:current";
     public static final String SELF_KEY_PAIR_HISTORY_KEY_PREFIX = "swl:key:self:history:";
-    public static final String OTHER_KEY_PUBLIC_KEY_PREFIX = "swl:key:other:";
+    public static final String OTHER_KEY_PUBLIC_KEY_PREFIX = "swl:key:other:keys:";
+    public static final String OTHER_KEY_PUBLIC_DEFAULT = "swl:key:other:default";
     public static final String NONCE_PREFIX = "swl:nonce:";
 
     private Supplier<ISwlAsymmetricEncryptor> asymmetricEncryptorSupplier;
@@ -41,8 +43,8 @@ public class SwlTransfer {
     private ISwlMessageDigester messageDigester;
     private IExpireCache<String, Object> cache = new ObjectExpireCacheWrapper<>(new MapCache<>(new ConcurrentHashMap<>()));
     private ISwlObfuscator obfuscator;
-    private long nonceWindowSeconds =30;
-    private long nonceTimeoutSeconds = 30;
+    private long nonceWindowSeconds = TimeUnit.MINUTES.toSeconds(30);
+    private long nonceTimeoutSeconds = TimeUnit.MINUTES.toSeconds(30);
     private long selfKeyExpireSeconds = TimeUnit.HOURS.toSeconds(24);
     private int selfKeyMaxCount = 3;
     private long otherKeyExpireSeconds = TimeUnit.HOURS.toSeconds(24);
@@ -59,9 +61,10 @@ public class SwlTransfer {
             return resetSelfKeyPair();
         }
 
-        AsymKeyPair ret = (AsymKeyPair) obj;
-        ret.setPublicKey(obfuscateDecode(ret.getPublicKey()));
-        ret.setPrivateKey(obfuscateDecode(ret.getPrivateKey()));
+        AsymKeyPair pair = (AsymKeyPair) obj;
+        AsymKeyPair ret = new AsymKeyPair();
+        ret.setPublicKey(obfuscateDecode(pair.getPublicKey()));
+        ret.setPrivateKey(obfuscateDecode(pair.getPrivateKey()));
         return ret;
     }
 
@@ -94,9 +97,19 @@ public class SwlTransfer {
         return obfuscateDecode((String) obj);
     }
 
+    public String getOtherPublicKeyDefault() {
+        Object obj = cache.get(OTHER_KEY_PUBLIC_DEFAULT);
+        if (obj == null) {
+            return null;
+        }
+        String otherAsymSign = (String) obj;
+        return getOtherPublicKey(otherAsymSign);
+    }
+
     public void setOtherPublicKey(String otherAsymSign, String publicKey) {
         String key = OTHER_KEY_PUBLIC_KEY_PREFIX + otherAsymSign;
         cache.set(key, obfuscateEncode(publicKey), otherKeyExpireSeconds, TimeUnit.SECONDS);
+        cache.set(OTHER_KEY_PUBLIC_DEFAULT, otherAsymSign);
     }
 
     public void refreshOtherPublicKeyExpire(String otherAsymSign) {
@@ -147,32 +160,43 @@ public class SwlTransfer {
         return obfuscator.decode(data);
     }
 
-    public SwlData clientSend(String serverPublicKey, List<String> parts) {
+    public SwlData send(String remotePublicKey, List<String> parts) {
         SwlData ret=new SwlData();
         ret.setParts(new ArrayList<>());
         ret.setHeader(new SwlHeader());
+        ret.setContext(new SwlContext());
+
+        ret.getContext().setRemotePublicKey(remotePublicKey);
 
         ISwlAsymmetricEncryptor asymmetricEncryptor = asymmetricEncryptorSupplier.get();
         ISwlSymmetricEncryptor symmetricEncryptor = symmetricEncryptorSupplier.get();
 
         AsymKeyPair selfKeyPair = getSelfKeyPair();
+        ret.getContext().setSelfKeyPair(selfKeyPair);
 
-        String clientAsymSign=messageDigester.digest(selfKeyPair.getPublicKey());
-        ret.getHeader().setClientAsymSign(clientAsymSign);
+        String localAsymSign = messageDigester.digest(selfKeyPair.getPublicKey());
+        ret.getHeader().setLocalAsymSign(localAsymSign);
+        ret.getContext().setLocalAsymSign(localAsymSign);
 
-        String serverAsymSign=messageDigester.digest(serverPublicKey);
-        ret.getHeader().setServerAsymSign(serverAsymSign);
+        String remoteAsymSign = messageDigester.digest(remotePublicKey);
+        ret.getHeader().setRemoteAsymSign(remoteAsymSign);
+        ret.getContext().setRemoteAsymSign(remoteAsymSign);
 
         long timestamp = SystemClock.currentTimeMillis() / 1000;
         long seq=random.nextInt(0x7fff);
         String nonce=Long.toString(timestamp,16)+"-"+Long.toString(seq,16);
         ret.getHeader().setNonce(nonce);
+        ret.getContext().setTimestamp(String.valueOf(timestamp));
+        ret.getContext().setSeq(String.valueOf(seq));
+        ret.getContext().setNonce(nonce);
 
         String key=symmetricEncryptor.generateKey();
+        ret.getContext().setKey(key);
 
-        asymmetricEncryptor.setPublicKey(serverPublicKey);
+        asymmetricEncryptor.setPublicKey(remotePublicKey);
         String randomKey=asymmetricEncryptor.encrypt(key);
         ret.getHeader().setRandomKey(randomKey);
+        ret.getContext().setRandomKey(randomKey);
 
         symmetricEncryptor.setKey(key);
         StringBuilder builder=new StringBuilder();
@@ -188,16 +212,19 @@ public class SwlTransfer {
         }
 
         String data=builder.toString();
+        ret.getContext().setData(data);
 
-        String sign=messageDigester.digest(data+randomKey+nonce+clientAsymSign+serverAsymSign);
+        String sign = messageDigester.digest(data + randomKey + nonce + localAsymSign + remoteAsymSign);
         ret.getHeader().setSign(sign);
+        ret.getContext().setSign(sign);
 
         asymmetricEncryptor.setPrivateKey(selfKeyPair.getPrivateKey());
         String digital=asymmetricEncryptor.sign(sign);
         ret.getHeader().setDigital(digital);
+        ret.getContext().setDigital(digital);
 
-        ret.getHeader().setClientAsymSign(obfuscateEncode(ret.getHeader().getClientAsymSign()));
-        ret.getHeader().setServerAsymSign(obfuscateEncode(ret.getHeader().getServerAsymSign()));
+        ret.getHeader().setLocalAsymSign(obfuscateEncode(ret.getHeader().getLocalAsymSign()));
+        ret.getHeader().setRemoteAsymSign(obfuscateEncode(ret.getHeader().getRemoteAsymSign()));
         ret.getHeader().setRandomKey(obfuscateEncode(ret.getHeader().getRandomKey()));
         ret.getHeader().setNonce(obfuscateEncode(ret.getHeader().getNonce()));
         ret.getHeader().setSign(obfuscateEncode(ret.getHeader().getSign()));
@@ -205,21 +232,30 @@ public class SwlTransfer {
         return ret;
     }
 
-    public SwlData serverReceive(String clientId,SwlData request){
+    public SwlData receive(String clientId, SwlData request) {
         SwlData ret=new SwlData();
         ret.setParts(new ArrayList<>());
         ret.setHeader(SwlHeader.copy(request.getHeader()));
+        ret.setContext(new SwlContext());
 
-        ret.getHeader().setClientAsymSign(obfuscateDecode(ret.getHeader().getClientAsymSign()));
-        ret.getHeader().setServerAsymSign(obfuscateDecode(ret.getHeader().getServerAsymSign()));
+        ret.getHeader().setLocalAsymSign(obfuscateDecode(ret.getHeader().getLocalAsymSign()));
+        ret.getHeader().setRemoteAsymSign(obfuscateDecode(ret.getHeader().getRemoteAsymSign()));
         ret.getHeader().setRandomKey(obfuscateDecode(ret.getHeader().getRandomKey()));
         ret.getHeader().setNonce(obfuscateDecode(ret.getHeader().getNonce()));
         ret.getHeader().setSign(obfuscateDecode(ret.getHeader().getSign()));
         ret.getHeader().setDigital(obfuscateDecode(ret.getHeader().getDigital()));
 
+        String str = ret.getHeader().getLocalAsymSign();
+        ret.getHeader().setLocalAsymSign(ret.getHeader().getRemoteAsymSign());
+        ret.getHeader().setRemoteAsymSign(str);
+
+        ret.getContext().setClientId(clientId);
+
         long currentTimestamp=SystemClock.currentTimeMillis()/1000;
+        ret.getContext().setCurrentTimestamp(String.valueOf(currentTimestamp));
 
         String nonce = ret.getHeader().getNonce();
+        ret.getContext().setNonce(nonce);
         if(nonce==null || nonce.isEmpty()){
             throw new SwlException(SwlCode.NONCE_MISSING_EXCEPTION.code(), "nonce cannot is empty!");
         }
@@ -231,14 +267,21 @@ public class SwlTransfer {
 
         long timestamp=Long.parseLong(nonceArr[0],16);
         String seq=nonceArr[1];
+        ret.getContext().setTimestamp(String.valueOf(timestamp));
+        ret.getContext().setSeq(seq);
 
         long window= nonceWindowSeconds;
+        ret.getContext().setWindow(String.valueOf(window));
 
         if(Math.abs(currentTimestamp-timestamp)>window){
             throw new SwlException(SwlCode.NONCE_TIMESTAMP_EXCEED_EXCEPTION.code(), "timestamp is exceed allow window seconds!");
         }
 
-        String nonceKey=clientId+nonce;
+        String nonceKey = nonce;
+        if (clientId != null && !clientId.isEmpty()) {
+            nonceKey = clientId + "-" + nonce;
+        }
+        ret.getContext().setNonceKey(nonceKey);
         if (containsNonce(nonceKey)) {
             throw new SwlException(SwlCode.NONCE_ALREADY_EXISTS_EXCEPTION.code(), "nonce key already exists!");
         }
@@ -246,6 +289,7 @@ public class SwlTransfer {
         setNonce(nonceKey, nonceTimeoutSeconds);
 
         String sign = ret.getHeader().getSign();
+        ret.getContext().setSign(sign);
         if(sign==null || sign.isEmpty()){
             throw new SwlException(SwlCode.SIGN_MISSING_EXCEPTION.code(), "sign cannot be empty!");
         }
@@ -261,54 +305,64 @@ public class SwlTransfer {
         }
 
         String data = builder.toString();
+        ret.getContext().setData(data);
 
         String randomKey = ret.getHeader().getRandomKey();
+        ret.getContext().setRandomKey(randomKey);
         if(randomKey==null || randomKey.isEmpty()){
             throw new SwlException(SwlCode.RANDOM_KEY_MISSING_EXCEPTION.code(), "random key cannot be empty!");
         }
 
-        String clientAsymSign = ret.getHeader().getClientAsymSign();
-        if(clientAsymSign==null || clientAsymSign.isEmpty()){
-            throw new SwlException(SwlCode.CLIENT_ASYM_KEY_SIGN_MISSING_EXCEPTION.code(), "client asym sign cannot be empty!");
+        String remoteAsymSign = ret.getHeader().getRemoteAsymSign();
+        ret.getContext().setRemoteAsymSign(remoteAsymSign);
+        if (remoteAsymSign == null || remoteAsymSign.isEmpty()) {
+            throw new SwlException(SwlCode.CLIENT_ASYM_KEY_SIGN_MISSING_EXCEPTION.code(), "remote asym sign cannot be empty!");
         }
 
-        String serverAsymSign = ret.getHeader().getServerAsymSign();
-        if(serverAsymSign==null || serverAsymSign.isEmpty()){
-            throw new SwlException(SwlCode.SERVER_ASYM_KEY_SIGN_MISSING_EXCEPTION.code(), "server asym sign cannot be empty!");
+        String localAsymSign = ret.getHeader().getLocalAsymSign();
+        ret.getContext().setLocalAsymSign(localAsymSign);
+        if (localAsymSign == null || localAsymSign.isEmpty()) {
+            throw new SwlException(SwlCode.SERVER_ASYM_KEY_SIGN_MISSING_EXCEPTION.code(), "local asym sign cannot be empty!");
         }
 
-        boolean signOk=messageDigester.verify(sign,data+randomKey+nonce+clientAsymSign+serverAsymSign);
+        boolean signOk = messageDigester.verify(sign, data + randomKey + nonce + remoteAsymSign + localAsymSign);
+        ret.getContext().setSignOk(signOk);
         if(!signOk){
             throw new SwlException(SwlCode.SIGN_VERIFY_FAILURE_EXCEPTION.code(), "verify sign failure!");
         }
 
         String digital = ret.getHeader().getDigital();
+        ret.getContext().setDigital(digital);
         if(digital==null || digital.isEmpty()){
             throw new SwlException(SwlCode.DIGITAL_MISSING_EXCEPTION.code(), "digital cannot be empty!");
         }
 
-        String clientPublicKey = getOtherPublicKey(clientAsymSign);
-        if(clientPublicKey==null || clientPublicKey.isEmpty()){
-            throw new SwlException(SwlCode.CLIENT_ASYM_KEY_NOT_FOUND_EXCEPTION.code(), "client key not found!");
+        String remotePublicKey = getOtherPublicKey(remoteAsymSign);
+        ret.getContext().setRemotePublicKey(remotePublicKey);
+        if (remotePublicKey == null || remotePublicKey.isEmpty()) {
+            throw new SwlException(SwlCode.CLIENT_ASYM_KEY_NOT_FOUND_EXCEPTION.code(), "remote key not found!");
         }
 
-        refreshOtherPublicKeyExpire(clientAsymSign);
+        refreshOtherPublicKeyExpire(remoteAsymSign);
 
         ISwlAsymmetricEncryptor asymmetricEncryptor = asymmetricEncryptorSupplier.get();
-        asymmetricEncryptor.setPublicKey(clientPublicKey);
+        asymmetricEncryptor.setPublicKey(remotePublicKey);
 
         boolean digitalOk=asymmetricEncryptor.verify(digital,sign);
+        ret.getContext().setDigitalOk(digitalOk);
         if(!digitalOk){
             throw new SwlException(SwlCode.DIGITAL_VERIFY_FAILURE_EXCEPTION.code(), "verify digital failure!");
         }
 
-        String serverPrivateKey = getSelfPrivateKey(serverAsymSign);
-        if(serverPrivateKey==null || serverPrivateKey.isEmpty()){
+        String localPrivateKey = getSelfPrivateKey(localAsymSign);
+        ret.getContext().setSelfKeyPair(new AsymKeyPair(null, localPrivateKey));
+        if (localPrivateKey == null || localPrivateKey.isEmpty()) {
             throw new SwlException(SwlCode.SERVER_ASYM_KEY_NOT_FOUND_EXCEPTION.code(), "server key not found!");
         }
 
-        asymmetricEncryptor.setPrivateKey(serverPrivateKey);
+        asymmetricEncryptor.setPrivateKey(localPrivateKey);
         String key=asymmetricEncryptor.decrypt(randomKey);
+        ret.getContext().setKey(key);
         if(key==null || key.isEmpty()){
             throw new SwlException(SwlCode.RANDOM_KEY_INVALID_EXCEPTION.code(), "random key is invalid!");
         }
@@ -327,6 +381,16 @@ public class SwlTransfer {
         }
 
         return ret;
+    }
+
+    public SwlData send(List<String> parts) {
+        String otherPublicKey = getOtherPublicKeyDefault();
+        return send(otherPublicKey, parts);
+    }
+
+    public SwlData response(String remoteAsymSign, List<String> parts) {
+        String otherPublicKey = getOtherPublicKey(remoteAsymSign);
+        return send(otherPublicKey, parts);
     }
 
 
