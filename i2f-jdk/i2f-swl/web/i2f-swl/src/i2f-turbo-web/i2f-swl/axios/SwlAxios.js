b7848dfd10c5723f3ba9d1df7a3d8efc56cf0294
Ice2Faith <Ugex_Savelar@163.com>
2024-07-13 19:02:28 +0800

    feat(swl): swl starter & web axios interceptor

--- i2f-jdk/i2f-swl/web/i2f-swl/src/i2f-turbo-web/i2f-swl/axios/SwlAxios.js
+++ i2f-jdk/i2f-swl/web/i2f-swl/src/i2f-turbo-web/i2f-swl/axios/SwlAxios.js
@@ -7,7 +7,7 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
 
 const SwlAxios = axios.create({
     // axios中请求配置有baseURL选项，表示请求URL公共部分
-    baseURL: 'http://localhost:8081/app/',
+    baseURL: 'http://localhost:8082/app/',
     // 超时
     timeout: 30 * 60 * 1000
 })
