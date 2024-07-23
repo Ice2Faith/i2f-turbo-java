07b79af91d160e851fc19db11b86b73d5495860d
Ice2Faith <Ugex_Savelar@163.com>
2024-07-23 17:07:33 +0800

    feat(maven): springboot maven project

---
+++ i2f-springboot/i2f-springboot-maven-project/src/main/java/com/springboot/maven/SpringBootMavenApplication.java
@@ -0,0 +1,20 @@
+package com.springboot.maven;
+
+import com.springboot.maven.application.WarBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/7/23 15:20
+ * @desc
+ */
+@SpringBootApplication
+public class SpringBootMavenApplication extends WarBootApplication {
+
+    public static void main(String[] args){
+        startup(SpringBootMavenApplication.class,args);
+    }
+}
