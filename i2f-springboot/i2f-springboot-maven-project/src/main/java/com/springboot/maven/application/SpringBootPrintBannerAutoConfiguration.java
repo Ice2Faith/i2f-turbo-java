ca412dd7a481a50e58998715a4b0ef06dc97b9db
Ice2Faith <Ugex_Savelar@163.com>
2024-07-23 17:07:33 +0800

    feat(maven): springboot maven project

---
+++ i2f-springboot/i2f-springboot-maven-project/src/main/java/com/springboot/maven/application/SpringBootPrintBannerAutoConfiguration.java
@@ -0,0 +1,48 @@
+package com.springboot.maven.application;
+
+import com.springboot.maven.slf4j.Slf4jPrintStream;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/7/12 22:14
+ * @desc
+ */
+@Configuration
+@Data
+@NoArgsConstructor
+public class SpringBootPrintBannerAutoConfiguration implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Slf4jPrintStream.redirectSysoutSyserr();
+        System.out.println("starting...");
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationStartedEvent> applicationStartedEventApplicationListener() {
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+        StackTraceElement elem = trace[trace.length - 1];
+        String className = elem.getClassName();
+        Class<?> clazz = null;
+        try {
+            clazz = Class.forName(className);
+        } catch (Throwable e) {
+
+        }
+        if (clazz == null) {
+            try {
+                clazz = Thread.currentThread().getContextClassLoader().loadClass(className);
+            } catch (Throwable e) {
+            }
+        }
+        return BaseBootApplication.getStartedListener(null, clazz);
+    }
+}
