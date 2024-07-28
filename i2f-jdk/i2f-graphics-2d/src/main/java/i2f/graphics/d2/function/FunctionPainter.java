0e7f2e0243305a543b72ee3a01b4b0b18cda6cb1
Ice2Faith <Ugex_Savelar@163.com>
2024-07-28 19:34:17 +0800

    feat(painter): math function painter

---
+++ i2f-jdk/i2f-graphics-2d/src/main/java/i2f/graphics/d2/function/FunctionPainter.java
@@ -0,0 +1,341 @@
+package i2f.graphics.d2.function;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.function.Function;
+
+import java.util.List;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/7/28 15:09
+ * @desc
+ */
+public class FunctionPainter {
+    public static void main(String[] args) throws IOException {
+        testCross();
+//        testCenter();
+//        testArguments();
+    }
+
+    public static void testArguments() throws IOException {
+        BufferedImage img = drawFunction(0, Math.PI * 2, 0.02,
+                -1, -1,
+                null,
+                15, 15,
+                FunctionType.ARGUMENTS,
+                (t) -> new double[]{
+                        16*Math.pow(Math.sin(t),3.0),
+                        13*Math.cos(t)-5*Math.cos(2*t)-2*Math.cos(3*t)-Math.cos(4*t)
+                });
+        ImageIO.write(img, "png", new File("./tmp.png"));
+    }
+
+    public static void testCenter() throws IOException {
+        BufferedImage img = drawFunction(0, Math.PI*2, 0.02,
+                -1, -1,
+                null,
+                15, 15,
+                FunctionType.CENTER,
+                (x) -> new double[]{
+                        x
+                });
+        ImageIO.write(img, "png", new File("./tmp.png"));
+    }
+
+    public static void testCross() throws IOException {
+        BufferedImage img = drawFunction(-25, 5, 0.2,
+                -1, -1,
+                null,
+                15, 15,
+                FunctionType.CROSS,
+                (x) -> new double[]{
+                        Math.sin(x),
+                        Math.cos(x),
+                        Math.pow(1.12, x)
+                });
+        ImageIO.write(img, "png", new File("./tmp.png"));
+    }
+
+    public enum FunctionType {
+        CROSS, // 直角坐标
+        CENTER, // 极坐标
+        ARGUMENTS // 参数方程
+    }
+
+    public static BufferedImage drawFunction(double beginX, double endX, double accX,
+                                             int width, int height, BufferedImage img,
+                                             double xAxisCount, double yAxisCount,
+                                             FunctionType type,
+                                             Function<Double, double[]> yValueMapper) {
+        if (width <= 0) {
+            width = 1080;
+        }
+        if (height <= 0) {
+            height = 720;
+        }
+        if (beginX > endX) {
+            double tmp = endX;
+            beginX = endX;
+            endX = tmp;
+        }
+        if (accX < 0) {
+            accX = -accX;
+        }
+        if (img == null) {
+            img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        }
+        width = img.getWidth();
+        height = img.getHeight();
+
+        if (type == null) {
+            type = FunctionType.CROSS;
+        }
+
+        Graphics2D g = img.createGraphics();
+        // clean background
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+
+        Double minY = null;
+        Double maxY = null;
+        Double minX=beginX;
+        Double maxX=endX;
+
+        List<double[]> yList = new ArrayList<>(100);
+        int yLen = -1;
+        for (double x = beginX; x <= endX; x += accX) {
+            double[] yArr = yValueMapper.apply(x);
+            if (yLen <= 0) {
+                yLen = yArr.length;
+                if(type==FunctionType.ARGUMENTS){
+                    yLen= yArr.length/2;
+                }
+                if (yLen < 1) {
+                    yLen = 1;
+                }
+            }
+            if (type == FunctionType.CENTER) {
+                double angle = x;
+                // angle, radius ...
+                for (double radius : yArr) {
+                    double cx = radius * Math.cos(angle);
+                    double cy = radius * Math.sin(angle);
+                    double[] arr = new double[]{cx, cy};
+
+                    if (minY == null || arr[1] < minY) {
+                        minY = arr[1];
+                    }
+                    if (maxY == null || arr[1] > maxY) {
+                        maxY = arr[1];
+                    }
+
+                    if(minX==null || arr[0] <minX){
+                        minX=arr[0];
+                    }
+                    if(maxX==null || arr[0]>maxX){
+                        maxX=arr[0];
+                    }
+
+                    yList.add(arr);
+                }
+
+            } else if (type==FunctionType.CROSS){
+
+                double[] arr = new double[1 + yLen];
+                arr[0] = x;
+                for (int i = 1; i < arr.length; i++) {
+                    arr[i] = 0;
+                    if ((i - 1) < yArr.length) {
+                        arr[i] = yArr[i - 1];
+                    }
+                    if (minY == null || arr[i] < minY) {
+                        minY = arr[i];
+                    }
+                    if (maxY == null || arr[i] > maxY) {
+                        maxY = arr[i];
+                    }
+
+                }
+
+
+                yList.add(arr);
+            }else if(type == FunctionType.ARGUMENTS){
+
+                for (int i = 0; (i+1) < yArr.length; i+=2) {
+                    double cx = yArr[i];
+                    double cy = yArr[i+1];
+                    double[] arr = new double[]{cx, cy};
+
+                    if (minY == null || arr[1] < minY) {
+                        minY = arr[1];
+                    }
+                    if (maxY == null || arr[1] > maxY) {
+                        maxY = arr[1];
+                    }
+
+
+                    if(minX==null || arr[0] <minX){
+                        minX=arr[0];
+                    }
+                    if(maxX==null || arr[0]>maxX){
+                        maxX=arr[0];
+                    }
+
+                    yList.add(arr);
+                }
+            }
+        }
+
+        if (type == FunctionType.CENTER) {
+            yLen = 1;
+        }
+        if (type == FunctionType.ARGUMENTS) {
+            yLen = 1;
+        }
+
+
+
+
+        final double finalWidth = width;
+
+        final double finalDiffX = maxX - minX;
+        final double finalDiffY = maxY - minY;
+
+        final double finalMinX = minX;
+        Function<Double, Double> xTransform = (x) -> {
+            return (x - finalMinX) / finalDiffX * finalWidth;
+        };
+
+        final double finalHeight = height;
+        final double finalMinY = minY;
+        final double finalMaxY = maxY;
+        Function<Double, Double> yTransform = (y) -> {
+            return finalHeight - ((y - finalMinY) / (finalDiffY) * finalHeight);
+        };
+
+
+        // axis line
+        g.setColor(new Color(220, 220, 220));
+        g.setFont(new Font(null, 0, 20));
+        double midX = finalDiffX / 2.0;
+        double stepX = finalDiffX / xAxisCount;
+        double px = midX;
+        while (px > minX) {
+            g.drawLine((int) (double) xTransform.apply(px),
+                    (int) (double) yTransform.apply(minY),
+                    (int) (double) xTransform.apply(px),
+                    (int) (double) yTransform.apply(maxY));
+            g.drawString(String.format("%.2f", px), (int) (double) xTransform.apply(px),
+                    (int) (double) yTransform.apply(minY));
+            px -= stepX;
+        }
+
+        px = midX;
+        while (px < maxX) {
+            g.drawLine((int) (double) xTransform.apply(px),
+                    (int) (double) yTransform.apply(minY),
+                    (int) (double) xTransform.apply(px),
+                    (int) (double) yTransform.apply(maxY));
+            g.drawString(String.format("%.2f", px), (int) (double) xTransform.apply(px),
+                    (int) (double) yTransform.apply(minY));
+            px += stepX;
+        }
+
+
+        double midY = finalDiffY / 2.0;
+        double stepY = finalDiffY / yAxisCount;
+        double py = midY;
+        while (py > minY) {
+            g.drawLine((int) (double) xTransform.apply(minX),
+                    (int) (double) yTransform.apply(py),
+                    (int) (double) xTransform.apply(maxX),
+                    (int) (double) yTransform.apply(py));
+            g.drawString(String.format("%.2f", py), (int) (double) xTransform.apply(minX),
+                    (int) (double) yTransform.apply(py));
+            py -= stepY;
+        }
+
+        px = midX;
+        while (py < maxY) {
+            g.drawLine((int) (double) xTransform.apply(minX),
+                    (int) (double) yTransform.apply(py),
+                    (int) (double) xTransform.apply(maxX),
+                    (int) (double) yTransform.apply(py));
+            g.drawString(String.format("%.2f", py), (int) (double) xTransform.apply(minX),
+                    (int) (double) yTransform.apply(py));
+            py += stepY;
+        }
+
+
+        // draw x,y axis
+        // x axis
+        g.setColor(Color.BLUE);
+        g.drawLine((int) (double) xTransform.apply(-2 * finalDiffX),
+                (int) (double) yTransform.apply(0.0),
+                (int) (double) xTransform.apply(2 * finalDiffX),
+                (int) (double) yTransform.apply(0.0));
+
+        // y axis
+        g.setColor(Color.RED);
+        g.drawLine((int) (double) xTransform.apply(0.0),
+                (int) (double) yTransform.apply(-2 * finalDiffY),
+                (int) (double) xTransform.apply(0.0),
+                (int) (double) yTransform.apply(2 * finalDiffY));
+
+
+        Random random = new SecureRandom();
+
+        Color[] colors = new Color[yLen];
+        for (int i = 0; i < yLen; i++) {
+            colors[i] = new Color(random.nextInt(230), random.nextInt(230), random.nextInt(230));
+        }
+
+        g.setColor(Color.BLACK);
+        double lastX = 0;
+        double[] lastY = new double[yLen];
+        boolean isFirst = true;
+        for (double[] xy : yList) {
+            double x = xy[0];
+            if (Double.isInfinite(x)) {
+                x = width;
+            }
+            if (Double.isNaN(x)) {
+                x = 0;
+            }
+            double vx = xTransform.apply(x);
+
+            for (int i = 0; i < yLen; i++) {
+                double y = xy[1 + i];
+                if (Double.isInfinite(y)) {
+                    y = height;
+                }
+                if (Double.isNaN(y)) {
+                    y = 0;
+                }
+                double vy = yTransform.apply(y);
+
+                if (!isFirst) {
+                    g.setColor(colors[i]);
+                    g.drawLine((int) (lastX), (int) (lastY[i]),
+                            (int) (vx), (int) (vy));
+                }
+
+                lastY[i] = vy;
+            }
+
+
+            lastX = vx;
+            isFirst = false;
+        }
+
+        return img;
+    }
+}
