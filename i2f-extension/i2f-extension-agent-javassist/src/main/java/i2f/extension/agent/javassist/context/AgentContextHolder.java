07a5b0e52a8cee7bac8d7910754d73d18959e8f7
Ice2Faith <Ugex_Savelar@163.com>
2024-08-02 16:20:19 +0800

    feat(agent): java agent for local file expression evaluator

--- i2f-extension/i2f-extension-agent-javassist/src/main/java/i2f/extension/agent/javassist/context/AgentContextHolder.java
+++ i2f-extension/i2f-extension-agent-javassist/src/main/java/i2f/extension/agent/javassist/context/AgentContextHolder.java
@@ -1,10 +1,27 @@
 package i2f.extension.agent.javassist.context;
 
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * @author Ice2Faith
  * @date 2024/8/2 11:33
  * @desc
  */
 public class AgentContextHolder {
+    public static final AgentContextHolder HOLDER = new AgentContextHolder();
+
+    public static volatile Instrumentation instrumentation;
+    public static volatile String agentArg;
+
+    public static volatile CopyOnWriteArrayList<ClassFileTransformer> transformers = new CopyOnWriteArrayList<>();
+
     public static volatile Object springApplicationContext;
+
+    public static volatile CopyOnWriteArrayList<Object> globalList = new CopyOnWriteArrayList<>();
+
+    public static volatile ConcurrentHashMap<String, Object> globalMap = new ConcurrentHashMap<>();
+
 }
