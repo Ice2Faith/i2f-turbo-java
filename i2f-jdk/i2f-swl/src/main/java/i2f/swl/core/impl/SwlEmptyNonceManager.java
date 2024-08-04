a06c2fb3998e214487f944d513cc873ccad5d7ce
Ice2Faith <Ugex_Savelar@163.com>
2024-08-04 22:40:57 +0800

    feat(i2f-swl): simple swl exchanger

---
+++ i2f-jdk/i2f-swl/src/main/java/i2f/swl/core/impl/SwlEmptyNonceManager.java
@@ -0,0 +1,20 @@
+package i2f.swl.core.impl;
+
+import i2f.swl.core.SwlNonceManager;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/8/4 22:25
+ * @desc
+ */
+public class SwlEmptyNonceManager implements SwlNonceManager {
+    @Override
+    public boolean contains(String nonce) {
+        return false;
+    }
+
+    @Override
+    public void set(String nonce, long timeoutSeconds) {
+
+    }
+}
