222f004674c1b76220921d97b8a2148f3a4f73bc
Ice2Faith <Ugex_Savelar@163.com>
2024-08-07 20:02:59 +0800

    feat(nashorn): nashorn check & adapt after jdk15

--- i2f-extension/i2f-extension-jce-sm-antherd/src/main/java/i2f/extension/jce/sm/antherd/encrypt/symmetric/Sm4Encryptor.java
+++ i2f-extension/i2f-extension-jce-sm-antherd/src/main/java/i2f/extension/jce/sm/antherd/encrypt/symmetric/Sm4Encryptor.java
@@ -3,6 +3,8 @@ package i2f.extension.jce.sm.antherd.encrypt.symmetric;
 import com.antherd.smcrypto.sm4.Sm4;
 import i2f.codec.CodecUtil;
 import i2f.codec.bytes.raw.HexStringByteCodec;
+import i2f.extension.jce.sm.antherd.NashornProvider;
+import i2f.extension.jce.sm.antherd.SmAntherdProvider;
 import i2f.jce.jdk.encrypt.Encryptor;
 import i2f.jce.jdk.supports.SecureRandomAlgorithm;
 import i2f.jce.std.encrypt.symmetric.ISymmetricEncryptor;
@@ -17,7 +19,10 @@ import java.util.Objects;
  * @desc
  */
 public class Sm4Encryptor implements ISymmetricEncryptor {
-
+    static {
+        NashornProvider.printNonNashorn();
+        SmAntherdProvider.printNonDependency();
+    }
     private String keyHex;
 
     public Sm4Encryptor() {
