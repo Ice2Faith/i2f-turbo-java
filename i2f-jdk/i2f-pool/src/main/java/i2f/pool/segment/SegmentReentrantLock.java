b44b94092a35158b81ae33cf970a14200796ce09
Ice2Faith <Ugex_Savelar@163.com>
2024-08-12 20:42:42 +0800

    feat(segment): segment synchronized object/ segment share object/segment lock

---
+++ i2f-jdk/i2f-pool/src/main/java/i2f/pool/segment/SegmentReentrantLock.java
@@ -0,0 +1,20 @@
+package i2f.pool.segment;
+
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/8/12 20:30
+ * @desc 分段互斥锁
+ */
+public class SegmentReentrantLock extends SegmentLock<ReentrantLock> {
+    public static final Supplier<ReentrantLock> LOCK_SUPPLIER=ReentrantLock::new;
+    public SegmentReentrantLock() {
+        super(LOCK_SUPPLIER);
+    }
+
+    public SegmentReentrantLock(int segmentSize) {
+        super(segmentSize, LOCK_SUPPLIER);
+    }
+}
