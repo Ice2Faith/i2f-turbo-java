10deca9a941d710e2e74e4dfafc2912025c11886
Ice2Faith <Ugex_Savelar@163.com>
2024-08-21 22:14:33 +0800

    feat(vue): simple vue page loader

---
+++ i2f-jdk/test-features/web/comp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="comp">
+    {{message}}
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "comp",
+  data(){
+    return {
+      message: 'xxx',
+      timer:null
+    }
+  },
+  mounted(){
+    this.timer=setInterval(()=>{
+      this.message=new Date()+''
+    },1000)
+  },
+  destroyed(){
+    clearInterval(this.timer)
+  }
+}
+</script>
+
+<style>
+.comp{
+  color: red;
+}
+</style>
+
---
+++ i2f-jdk/test-features/web/vue2-loader.js
@@ -0,0 +1,224 @@
+/**
+ * @return {Vue2Loader}
+ * @constructor {Vue2Loader}
+ */
+function Vue2Loader(){
+
+}
+
+/**
+ *
+ * @type {DOMParser}
+ */
+Vue2Loader.parser=new DOMParser()
+
+/**
+ *
+ * @param html {string}
+ * @return {Document}
+ */
+Vue2Loader.parseHtmlDom=function(html){
+    return Vue2Loader.parser.parseFromString(html,"text/html")
+}
+
+/**
+ *
+ * @return {string}
+ */
+Vue2Loader.randomUUID=function(){
+    let ret=''
+    let codes='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for (let i = 0; i < 32; i++) {
+        let num=Math.floor(Math.random()*codes.length)
+        ret+=codes.charAt(num)
+    }
+    return ret
+}
+
+/**
+ *
+ * @param html {string}
+ * @return {{template: (*|string), varName: string, style: (*|string), script: string}}
+ */
+Vue2Loader.parseVueTemplate=function(html){
+    const doc=Vue2Loader.parseHtmlDom(html)
+    let template=doc.querySelector('template')?.innerHTML||''
+    let script=doc.querySelector('script')?.innerHTML||''
+    let style=doc.querySelector('style')?.innerHTML||''
+    let varName='vueComponent_'+Vue2Loader.randomUUID()
+    script=script.replace(/export\s+default\s+\{/,'let '+varName+' = {')
+    return {
+        template: template,
+        script: script,
+        style: style,
+        varName: varName
+    }
+}
+
+/**
+ *
+ * @param url {string}
+ * @param appId {string}
+ * @return {Promise<string>}
+ */
+Vue2Loader.createApp=function(url,appId='app'){
+    if(!appId){
+        appId='app'
+    }
+    return fetch(url)
+        .then(res=>res.text())
+        .then(vueHtml=>{
+            const vueTemplate = Vue2Loader.parseVueTemplate(vueHtml);
+
+            let appDom = document.querySelector(`#${appId}`);
+            if(appDom){
+                appDom.innerHTML=''
+            }else{
+                appDom = document.createElement('div');
+                appDom.id=appId
+                document.body.append(appDom)
+            }
+            appDom.innerHTML=vueTemplate.template
+
+
+            let styleDom = document.querySelector(`#${appId}Style`);
+            if(styleDom){
+                styleDom.innerHTML=''
+            }else{
+                styleDom = document.createElement('style');
+                styleDom.id=`${appId}Style`
+                document.body.append(styleDom)
+            }
+            styleDom.innerHTML=vueTemplate.style
+
+            let script=vueTemplate.script
+            script+='\n'
+            script+='   '+vueTemplate.varName+'.el="#'+appId+'"\n' +
+                    '   window.vueApp=new Vue('+vueTemplate.varName+')\n'
+
+            let scriptDom = document.querySelector(`#${appId}Script`);
+            if(scriptDom){
+                scriptDom.innerHTML=''
+            }else{
+                scriptDom = document.createElement('script');
+                scriptDom.id=`${appId}Script`
+                document.body.append(scriptDom)
+            }
+            scriptDom.innerHTML=script
+
+            return appId
+        })
+}
+
+/**
+ *
+ * @param url {string}
+ * @param componentName {string}
+ * @return {Promise<string>}
+ */
+Vue2Loader.createComponent=function(url,componentName=null){
+    if(!componentName){
+        componentName=url
+        let idx=componentName.lastIndexOf('/')
+        if(idx>=0){
+            componentName=componentName.substring(idx+1)
+        }
+        idx=componentName.lastIndexOf('.vue')
+        if(idx>=0){
+            componentName=componentName.substring(0,idx)
+        }
+    }
+    let appId=`component_`+componentName+'_'+Vue2Loader.randomUUID()
+    return fetch(url)
+        .then(res=>res.text())
+        .then(vueHtml=>{
+            const vueTemplate = Vue2Loader.parseVueTemplate(vueHtml);
+
+            let appDom = document.querySelector(`#${appId}`);
+            if(appDom){
+                appDom.innerHTML=''
+            }else{
+                appDom = document.createElement('div');
+                appDom.id=appId
+                document.body.append(appDom)
+            }
+            appDom.innerHTML=vueTemplate.template
+
+            let styleDom = document.querySelector(`#${appId}Style`);
+            if(styleDom){
+                styleDom.innerHTML=''
+            }else{
+                styleDom = document.createElement('style');
+                styleDom.id=`${appId}Style`
+                document.body.append(styleDom)
+            }
+            styleDom.innerHTML=vueTemplate.style
+
+            let script=vueTemplate.script
+            script+='\n'
+            script+='let compDom'+vueTemplate.varName+'=document.querySelector("#'+appId+'")\n' +
+                '            '+vueTemplate.varName+'.name="'+componentName+'"\n' +
+                '            '+vueTemplate.varName+'.template=compDom'+vueTemplate.varName+'.innerHTML\n' +
+                '            Vue.component("'+componentName+'",'+vueTemplate.varName+')\n'
+
+            let scriptDom = document.querySelector(`#${appId}Script`);
+            if(scriptDom){
+                scriptDom.innerHTML=''
+            }else{
+                scriptDom = document.createElement('script');
+                scriptDom.id=`${appId}Script`
+                document.body.append(scriptDom)
+            }
+            scriptDom.innerHTML=script
+
+            setTimeout(()=>{
+                scriptDom.remove()
+                appDom.remove()
+            })
+
+            return appId
+        })
+        .catch(error=>{
+            return Promise.resolve(appId)
+        })
+
+}
+
+/**
+ * config={
+ *     url: './app.vue',
+ *     appId: 'app',
+ *     components:[
+ *         {
+ *             url: './comp.vue',
+ *             name: 'comp'
+ *         },
+ *         {
+ *             url: './test.vue'
+ *         },
+ *         './hello.vue'
+ *     ]
+ * }
+ * @param config {object}
+ * @return {Promise<string>}
+ */
+Vue2Loader.setupVueApp=function(config){
+    let components=[]
+    if(config.components){
+        for (let i = 0; i < config.components.length; i++) {
+            let item=config.components[i]
+            if(!item){
+                continue
+            }
+            if(item.url){
+                components.push(Vue2Loader.createComponent(item.url,item.name))
+            }else{
+                components.push(Vue2Loader.createComponent(item))
+            }
+        }
+    }
+    return Promise.all(components)
+        .then(()=>{
+            return Vue2Loader.createApp(config.url,config.appId)
+        })
+}
