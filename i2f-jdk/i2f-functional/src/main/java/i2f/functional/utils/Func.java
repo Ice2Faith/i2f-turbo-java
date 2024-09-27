2dfde545d10919fa3042066e9adcf80b2cea3a18
Ice2Faith <Ugex_Savelar@163.com>
2024-09-27 18:18:29 +0800

    feat(reflect): reflect resolver enhance

---
+++ i2f-jdk/i2f-functional/src/main/java/i2f/functional/utils/Func.java
@@ -0,0 +1,35 @@
+package i2f.functional.utils;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/9/27 17:12
+ */
+public class Func {
+    public static <T,V> void setIf(T obj, BiConsumer<T,V> setter, V value){
+        if(value==null){
+            return;
+        }
+        if(value instanceof String){
+            String str = (String) value;
+            if(str.isEmpty()){
+                return;
+            }
+        }
+        setter.accept(obj,value);
+    }
+    public static <V> void setIf(Consumer<V> setter,V value){
+        if(value==null){
+            return;
+        }
+        if(value instanceof String){
+            String str = (String) value;
+            if(str.isEmpty()){
+                return;
+            }
+        }
+        setter.accept(value);
+    }
+}
