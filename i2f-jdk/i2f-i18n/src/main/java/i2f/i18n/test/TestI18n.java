6ac9f2251ffb8599105f1cfd1e04048643166d26
Ice2Faith <Ugex_Savelar@163.com>
2024-09-28 21:32:27 +0800

    feat(i18n): i18n implements support

--- i2f-jdk/i2f-i18n/src/main/java/i2f/i18n/test/TestI18n.java
+++ i2f-jdk/i2f-i18n/src/main/java/i2f/i18n/test/TestI18n.java
@@ -1,6 +1,10 @@
 package i2f.i18n.test;
 
 import i2f.i18n.I18n;
+import i2f.i18n.provider.impl.DefaultI18nProvider;
+
+import java.util.Arrays;
+import java.util.Locale;
 
 /**
  * @author Ice2Faith
@@ -8,10 +12,22 @@ import i2f.i18n.I18n;
  */
 public class TestI18n {
     public static void main(String[] args) {
-//        I18n.setThreadLang("zh-CN");
-        System.out.println(I18n.DEFAULT_LANG);
+        if(I18n.DEFAULT_PROVIDER instanceof DefaultI18nProvider){
+            DefaultI18nProvider defaultProvider = (DefaultI18nProvider) I18n.DEFAULT_PROVIDER;
+            defaultProvider.getFileNames()
+                    .addAll(Arrays.asList(
+                            "file:i2f-jdk/i2f-i18n/src/main/java/i2f/i18n/i18n.xml",
+                            "file:i2f-jdk/i2f-i18n/src/main/java/i2f/i18n/i18n-default.xml",
+                            "file:i2f-jdk/i2f-i18n/src/main/java/i2f/i18n/i18n-zh-CN.properties",
+                            "file:i2f-jdk/i2f-i18n/src/main/java/i2f/i18n/i18n-zh-CN.xml"
+                    ));
+            defaultProvider.loadLangMap(I18n.DEFAULT_LANG);
+            defaultProvider.loadLangMap("zh-CN");
+        }
+        System.out.println(Locale.getDefault().getLanguage());
         String version = I18n.get("app.version");
         System.out.println(version);
+        I18n.setThreadLang("zh");
         String banner = I18n.get("app.banner");
         System.out.println("###" + banner + "###");
     }
