a900821122f174355ae3d92fb3e725934ae9d0c5
Ice2Faith <Ugex_Savelar@163.com>
2024-10-24 16:40:11 +0800

    feat(security): security filter for simple web firewall

--- i2f-jakarta/i2f-jakarta-web/src/main/java/i2f/web/filter/SecurityFilter.java
+++ i2f-jakarta/i2f-jakarta-web/src/main/java/i2f/web/filter/SecurityFilter.java
@@ -1,13 +1,13 @@
 package i2f.web.filter;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 import lombok.Data;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLDecoder;
