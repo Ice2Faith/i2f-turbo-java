6de7c51db8891a953132e151ce1a506905c49b76
Ice2Faith <Ugex_Savelar@163.com>
2024-11-26 14:09:45 +0800

    feat(lombok): lombok style

--- i2f-jdk/i2f-database/src/main/java/i2f/database/metadata/data/IndexColumnMeta.java
+++ i2f-jdk/i2f-database/src/main/java/i2f/database/metadata/data/IndexColumnMeta.java
@@ -1,45 +1,17 @@
 package i2f.database.metadata.data;
 
+import lombok.Data;
+
 /**
  * @author Ice2Faith
  * @date 2024/3/14 10:45
  * @desc
  */
+@Data
 public class IndexColumnMeta {
     protected int index;
     protected String name;
     protected boolean isDesc;
     protected String type;
 
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isDesc() {
-        return isDesc;
-    }
-
-    public void setDesc(boolean desc) {
-        isDesc = desc;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 }
