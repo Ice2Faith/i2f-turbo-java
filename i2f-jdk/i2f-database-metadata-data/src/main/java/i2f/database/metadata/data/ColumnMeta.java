ce9d894668b6b1333328886bf5fa67830e6a57d0
Ice2Faith <Ugex_Savelar@163.com>
2024-11-28 23:30:34 +0800

    feat(reverse-engineer): reverse engineer generator

--- i2f-jdk/i2f-database-metadata-data/src/main/java/i2f/database/metadata/data/ColumnMeta.java
+++ i2f-jdk/i2f-database-metadata-data/src/main/java/i2f/database/metadata/data/ColumnMeta.java
@@ -2,6 +2,9 @@ package i2f.database.metadata.data;
 
 import lombok.Data;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Ice2Faith
  * @date 2024/3/14 10:29
@@ -29,6 +32,17 @@ public class ColumnMeta {
     protected String stdType;
     protected String looseJavaType;
     protected String looseJdbcType;
+
+    // 以下字段，需要使用 TableMeta.fillColumnIndexMeta 后才有值，才能正确使用
+    protected boolean isPrimaryKey;
+    protected String primaryKeyName;
+    protected int primaryKeyOrder;
+
+    protected boolean isUniqueKey;
+    protected List<Map.Entry<String, Integer>> uniqueKeyList;
+
+    protected boolean isIndexKey;
+    protected List<Map.Entry<String, Integer>> indexKeyList;
 
     public static int compare(ColumnMeta o1, ColumnMeta o2) {
         return Integer.compare(o1.getIndex(), o2.getIndex());
