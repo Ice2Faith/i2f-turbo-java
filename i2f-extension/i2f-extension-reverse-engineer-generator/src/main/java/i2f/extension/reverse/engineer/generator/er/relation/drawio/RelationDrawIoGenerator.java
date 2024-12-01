ccf6657be2a211789235ea3607c3aaade74b740f
Ice2Faith <Ugex_Savelar@163.com>
2024-12-01 20:25:41 +0800

    feat(generator): table relation design

---
+++ i2f-extension/i2f-extension-reverse-engineer-generator/src/main/java/i2f/extension/reverse/engineer/generator/er/relation/drawio/RelationDrawIoGenerator.java
@@ -0,0 +1,49 @@
+package i2f.extension.reverse.engineer.generator.er.relation.drawio;
+
+import i2f.database.metadata.data.TableMeta;
+import i2f.extension.velocity.VelocityGenerator;
+import i2f.resources.ResourceUtil;
+import i2f.serialize.str.xml.impl.Xml2;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/12/1 19:29
+ */
+public class RelationDrawIoGenerator {
+    public static String render(List<TableMeta> metas) throws IOException {
+        List<RelationTableContext> list = new ArrayList<>();
+        String tpl = ResourceUtil.getClasspathResourceAsString("tpl/design/table-relation-entity.html.vm", "UTF-8");
+        int x = 0;
+        int y = 0;
+        for (TableMeta item : metas) {
+            Map<String, Object> param = new HashMap<>();
+            param.put("table", item);
+            String str = VelocityGenerator.render(tpl, param);
+            str = str.replaceAll("\\s+", " ");
+            str = Xml2.toXmlString(str);
+            RelationTableContext vo = new RelationTableContext();
+            vo.setMeta(item);
+            vo.setValue(str);
+            vo.setId(UUID.randomUUID().toString().replaceAll("-", "").toLowerCase());
+            vo.setX(10 + (x * 320));
+            vo.setY(100 + (y * 320));
+            vo.setWidth(300);
+            vo.setHeight(50 + 25 * item.getColumns().size());
+            list.add(vo);
+
+            x++;
+            if (x == 5) {
+                x = 0;
+                y++;
+            }
+        }
+
+        tpl = ResourceUtil.getClasspathResourceAsString("tpl/design/table-relation-design.xml.drawio.vm", "UTF-8");
+        Map<String, Object> param = new HashMap<>();
+        param.put("tables", list);
+        return VelocityGenerator.render(tpl, param);
+    }
+}
