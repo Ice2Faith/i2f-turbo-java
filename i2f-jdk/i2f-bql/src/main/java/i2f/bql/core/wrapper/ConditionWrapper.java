db397ceb853c7f930b4ae8211cef966f2b0df1f1
Ice2Faith <Ugex_Savelar@163.com>
2025-01-06 09:19:56 +0800

    feat(bql): bql

--- i2f-jdk/i2f-bql/src/main/java/i2f/bql/core/wrapper/ConditionWrapper.java
+++ i2f-jdk/i2f-bql/src/main/java/i2f/bql/core/wrapper/ConditionWrapper.java
@@ -1,8 +1,8 @@
 package i2f.bql.core.wrapper;
 
+import i2f.bql.core.Bql;
 import i2f.bql.core.condition.Condition;
 import i2f.bql.core.condition.ConditionItem;
-import i2f.bql.core.lambda.Bql;
 import i2f.functional.IFunctional;
 import i2f.functional.adapt.IBuilder;
 import i2f.functional.adapt.IExecute;
@@ -11,7 +11,9 @@ import i2f.functional.adapt.ISetter;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author Ice2Faith
@@ -115,5 +117,133 @@ public class ConditionWrapper<H extends ConditionWrapper<H>> implements BqlWrapp
 
     public <T> H cond(IExecute<T> exec, Object value) {
         return cond((Serializable) exec, value);
+    }
+
+    public <R, T> H eq(IGetter<R, T> getter, Object value) {
+        return cond((Serializable) getter, Condition.$eq(value));
+    }
+
+    public <T, V> H eq(ISetter<T, V> setter, Object value) {
+        return cond((Serializable) setter, Condition.$eq(value));
+    }
+
+    public <R, T> H like(IGetter<R, T> getter, Object value) {
+        return cond((Serializable) getter, Condition.$like(value));
+    }
+
+    public <T, V> H like(ISetter<T, V> setter, Object value) {
+        return cond((Serializable) setter, Condition.$like(value));
+    }
+
+    public <R, T> H neq(IGetter<R, T> getter, Object value) {
+        return cond((Serializable) getter, Condition.$neq(value));
+    }
+
+    public <T, V> H neq(ISetter<T, V> setter, Object value) {
+        return cond((Serializable) setter, Condition.$neq(value));
+    }
+
+    public <R, T> H ne(IGetter<R, T> getter, Object value) {
+        return cond((Serializable) getter, Condition.$ne(value));
+    }
+
+    public <T, V> H ne(ISetter<T, V> setter, Object value) {
+        return cond((Serializable) setter, Condition.$ne(value));
+    }
+
+    public <R, T> H isNull(IGetter<R, T> getter) {
+        return cond((Serializable) getter, Condition.$isNull());
+    }
+
+    public <T, V> H isNull(ISetter<T, V> setter) {
+        return cond((Serializable) setter, Condition.$isNull());
+    }
+
+    public <R, T> H isNotNull(IGetter<R, T> getter) {
+        return cond((Serializable) getter, Condition.$isNotNull());
+    }
+
+    public <T, V> H isNotNull(ISetter<T, V> setter) {
+        return cond((Serializable) setter, Condition.$isNotNull());
+    }
+
+    public <R, T> H in(IGetter<R, T> getter, Object... value) {
+        return cond((Serializable) getter, Condition.$in(value));
+    }
+
+    public <T, V> H in(ISetter<T, V> setter, Object... value) {
+        return cond((Serializable) setter, Condition.$in(value));
+    }
+
+    public <R, T> H in(IGetter<R, T> getter, Collection<?> value) {
+        return cond((Serializable) getter, Condition.$in(value));
+    }
+
+    public <T, V> H in(ISetter<T, V> setter, Collection<?> value) {
+        return cond((Serializable) setter, Condition.$in(value));
+    }
+
+    public <R, T> H notIn(IGetter<R, T> getter, Object... value) {
+        return cond((Serializable) getter, Condition.$notIn(value));
+    }
+
+    public <T, V> H notIn(ISetter<T, V> setter, Object... value) {
+        return cond((Serializable) setter, Condition.$notIn(value));
+    }
+
+    public <R, T> H notIn(IGetter<R, T> getter, Collection<?> value) {
+        return cond((Serializable) getter, Condition.$notIn(value));
+    }
+
+    public <T, V> H notIn(ISetter<T, V> setter, Collection<?> value) {
+        return cond((Serializable) setter, Condition.$notIn(value));
+    }
+
+    public <R, T> H exists(IGetter<R, T> getter, Supplier<Bql<?>> caller) {
+        return cond((Serializable) getter, Condition.$exists(caller));
+    }
+
+    public <T, V> H exists(ISetter<T, V> setter, Supplier<Bql<?>> caller) {
+        return cond((Serializable) setter, Condition.$ne(caller));
+    }
+
+    public <R, T> H notExists(IGetter<R, T> getter, Supplier<Bql<?>> caller) {
+        return cond((Serializable) getter, Condition.$notExists(caller));
+    }
+
+    public <T, V> H notExists(ISetter<T, V> setter, Supplier<Bql<?>> caller) {
+        return cond((Serializable) setter, Condition.$notExists(caller));
+    }
+
+    public <R, T> H and(IGetter<R, T> getter, Supplier<Bql<?>> caller) {
+        return cond((Serializable) getter, Condition.$and(caller));
+    }
+
+    public <T, V> H and(ISetter<T, V> setter, Supplier<Bql<?>> caller) {
+        return cond((Serializable) setter, Condition.$and(caller));
+    }
+
+    public <R, T> H or(IGetter<R, T> getter, Supplier<Bql<?>> caller) {
+        return cond((Serializable) getter, Condition.$or(caller));
+    }
+
+    public <T, V> H or(ISetter<T, V> setter, Supplier<Bql<?>> caller) {
+        return cond((Serializable) setter, Condition.$or(caller));
+    }
+
+    public <R, T> H and(IGetter<R, T> getter, ConditionWrapper<?> wrapper) {
+        return cond((Serializable) getter, Condition.$and(wrapper));
+    }
+
+    public <T, V> H and(ISetter<T, V> setter, ConditionWrapper<?> wrapper) {
+        return cond((Serializable) setter, Condition.$and(wrapper));
+    }
+
+    public <R, T> H or(IGetter<R, T> getter, ConditionWrapper<?> wrapper) {
+        return cond((Serializable) getter, Condition.$or(wrapper));
+    }
+
+    public <T, V> H or(ISetter<T, V> setter, ConditionWrapper<?> wrapper) {
+        return cond((Serializable) setter, Condition.$or(wrapper));
     }
 }
