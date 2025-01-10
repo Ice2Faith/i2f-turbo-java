91ce7c7f412e37a58a2a2bfa8bc54c3f27f707de
Ice2Faith <Ugex_Savelar@163.com>
2025-01-10 22:14:45 +0800

    feat(antlr4): antlr4 calculator

--- i2f-extension/i2f-extension-antlr4/src/main/java/i2f/extension/antlr4/calculator/test/TestCalculator.java
+++ i2f-extension/i2f-extension-antlr4/src/main/java/i2f/extension/antlr4/calculator/test/TestCalculator.java
@@ -1,9 +1,6 @@
 package i2f.extension.antlr4.calculator.test;
 
-import i2f.extension.antlr4.calculator.CalculatorLexer;
 import i2f.extension.antlr4.calculator.CalculatorParser;
-import i2f.extension.antlr4.calculator.CalculatorVisitor;
-import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 /**
@@ -12,16 +9,13 @@ import org.antlr.v4.runtime.CommonTokenStream;
  */
 public class TestCalculator {
     public static void main(String[] args) {
-        String sql = "avg(4+3,5,6)";
-        ANTLRInputStream input = new ANTLRInputStream(sql);
-        CalculatorLexer lexer = new CalculatorLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        String formula = "avg(4+3,5,6)";
+        CommonTokenStream tokens = Calculator.parseTokens(formula);
         CalculatorParser parser = new CalculatorParser(tokens);
         CalculatorParser.EvalContext tree = parser.eval();
         System.out.println(tree.toStringTree(parser));
 
-        CalculatorVisitor visitor = new CalculatorVisitorImpl();
-        Object ret = visitor.visit(tree);
+        Object ret = Calculator.eval(tree);
         System.out.println(ret);
     }
 }
