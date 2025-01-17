f570256193c5f4c818892acec3be4234e7fc60c4
Ice2Faith <Ugex_Savelar@163.com>
2025-01-17 21:25:14 +0800

    feat(easyexcel): easyexcel support import/export type of map

--- i2f-extension/i2f-extension-easyexcel/src/main/java/i2f/extension/easyexcel/core/ExcelExportTask.java
+++ i2f-extension/i2f-extension-easyexcel/src/main/java/i2f/extension/easyexcel/core/ExcelExportTask.java
@@ -2,14 +2,20 @@ package i2f.extension.easyexcel.core;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.enums.WriteDirectionEnum;
 import com.alibaba.excel.write.handler.CellWriteHandler;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.fill.FillConfig;
 import i2f.extension.easyexcel.annotation.ExcelTag;
 import i2f.extension.easyexcel.style.AnnotationExcelStyleCellWriteHandler;
+import i2f.io.stream.StreamUtil;
 import i2f.reflect.ReflectResolver;
+import i2f.resources.ResourceUtil;
+import i2f.typeof.TypeOf;
 import lombok.Data;
 
 import java.io.File;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.*;
@@ -108,6 +114,13 @@ public class ExcelExportTask implements Runnable {
 
     @Override
     public void run() {
+        File parentFile = file.getParentFile();
+        if (parentFile == null) {
+            parentFile = new File(file.getAbsolutePath()).getParentFile();
+        }
+        if (!parentFile.exists()) {
+            parentFile.mkdirs();
+        }
         if (tmpFile == null) {
             String tmpSuffix = "";
             String name = file.getName();
@@ -138,6 +151,9 @@ public class ExcelExportTask implements Runnable {
             }
         }
 
+        Class<?> dataClass = provider.getDataClass();
+        boolean createdMapTemplateFile = false;
+
         Map<String, Object> workbookContext = new HashMap<>();
         Map<Integer, Map<String, Object>> sheetContext = new HashMap<>();
 
@@ -151,7 +167,7 @@ public class ExcelExportTask implements Runnable {
         if (excludeColumnTags == null) {
             excludeColumnTags = new HashSet<>();
         }
-        Class<?> dataClass = provider.getDataClass();
+
         if (dataClass != null && !excludeColumnTags.isEmpty()) {
             Map<Field, Class<?>> fields = ReflectResolver.getFields(dataClass, (field) -> {
                 int modifiers = field.getModifiers();
@@ -183,6 +199,13 @@ public class ExcelExportTask implements Runnable {
         if (!isSupportPage) {
             Class<?> clazz = provider.getDataClass();
             List<?> data = provider.getData(null);
+            if (TypeOf.typeOf(clazz, Map.class)) {
+                if (!data.isEmpty()) {
+                    templateFile = createTemplateFileByMap(file, (Map<?, ?>) data.get(0));
+                    useTemplate = true;
+                    createdMapTemplateFile = true;
+                }
+            }
             int size = data.size();
             if (size <= pageSize) {
                 if (useTemplate) {
@@ -207,25 +230,33 @@ public class ExcelExportTask implements Runnable {
                 }
             } else {
                 ExcelWriter excelWriter = null;
-                // 处理使用模板的情况
-                if (useTemplate) {
-                    excelWriter = EasyExcel.write(tmpFile)
-                            .withTemplate(templateFile)
-                            .registerWriteHandler(writeHandler)
-                            .excludeColumnFieldNames(excludeColumnNames)
-                            .build();
-                } else {
-                    excelWriter = EasyExcel.write(tmpFile, clazz)
-                            .registerWriteHandler(writeHandler)
-                            .excludeColumnFieldNames(excludeColumnNames)
-                            .build();
-                }
                 int curPageIndex = 0;
                 int count = 0;
                 List<Object> curData = new ArrayList<>(pageSize);
                 for (Object item : data) {
+                    if (excelWriter == null) {
+                        if (TypeOf.typeOf(clazz, Map.class)) {
+                            if (!data.isEmpty()) {
+                                templateFile = createTemplateFileByMap(file, (Map<?, ?>) item);
+                                useTemplate = true;
+                                createdMapTemplateFile = true;
+                            }
+                        }
+                        // 处理使用模板的情况
+                        if (useTemplate) {
+                            excelWriter = EasyExcel.write(tmpFile)
+                                    .withTemplate(templateFile)
+                                    .registerWriteHandler(writeHandler)
+                                    .excludeColumnFieldNames(excludeColumnNames)
+                                    .build();
+                        } else {
+                            excelWriter = EasyExcel.write(tmpFile, clazz)
+                                    .registerWriteHandler(writeHandler)
+                                    .excludeColumnFieldNames(excludeColumnNames)
+                                    .build();
+                        }
+                    }
                     if (count == pageSize) {
-
                         WriteSheet writeSheet = EasyExcel.writerSheet(curPageIndex, ((curPageIndex == 0) ? sheetName : (sheetName + "-" + curPageIndex)))
                                 .excludeColumnFieldNames(excludeColumnNames)
                                 .build();
@@ -262,24 +293,34 @@ public class ExcelExportTask implements Runnable {
 
             Class<?> clazz = provider.getDataClass();
             ExcelWriter excelWriter = null;
-            if (useTemplate) {
-                excelWriter = EasyExcel.write(tmpFile)
-                        .withTemplate(templateFile)
-                        .registerWriteHandler(writeHandler)
-                        .excludeColumnFieldNames(excludeColumnNames)
-                        .build();
-            } else {
-                excelWriter = EasyExcel.write(tmpFile, clazz)
-                        .registerWriteHandler(writeHandler)
-                        .excludeColumnFieldNames(excludeColumnNames)
-                        .build();
-            }
 
             while (true) {
                 ExcelExportPage page = new ExcelExportPage(curPageIndex, pageSize);
                 List<?> data = provider.getData(page);
                 if (data == null || data.isEmpty()) {
                     break;
+                }
+
+                if (excelWriter == null) {
+                    if (TypeOf.typeOf(clazz, Map.class)) {
+                        if (!data.isEmpty()) {
+                            templateFile = createTemplateFileByMap(file, (Map<?, ?>) data.get(0));
+                            useTemplate = true;
+                            createdMapTemplateFile = true;
+                        }
+                    }
+                    if (useTemplate) {
+                        excelWriter = EasyExcel.write(tmpFile)
+                                .withTemplate(templateFile)
+                                .registerWriteHandler(writeHandler)
+                                .excludeColumnFieldNames(excludeColumnNames)
+                                .build();
+                    } else {
+                        excelWriter = EasyExcel.write(tmpFile, clazz)
+                                .registerWriteHandler(writeHandler)
+                                .excludeColumnFieldNames(excludeColumnNames)
+                                .build();
+                    }
                 }
 
                 int dsize = data.size();
@@ -301,6 +342,12 @@ public class ExcelExportTask implements Runnable {
                 }
             }
 
+            if (excelWriter == null) {
+                excelWriter = EasyExcel.write(tmpFile, clazz)
+                        .registerWriteHandler(writeHandler)
+                        .excludeColumnFieldNames(excludeColumnNames)
+                        .build();
+            }
             excelWriter.finish();
         }
         if (file.exists()) {
@@ -313,8 +360,56 @@ public class ExcelExportTask implements Runnable {
         }
         tmpFile.renameTo(file);
 
+        if (createdMapTemplateFile) {
+            templateFile.delete();
+        }
+
         if (latch != null) {
             latch.countDown();
+        }
+    }
+
+    public static File createTemplateFileByMap(File file, Map<?, ?> map) {
+        String tmpSuffix = "";
+        String name = file.getName();
+        int idx = name.lastIndexOf(".");
+        if (idx >= 0) {
+            tmpSuffix = name.substring(idx);
+        }
+
+        File templateFile = new File(file.getParentFile(), UUID.randomUUID().toString().replaceAll("-", "") + tmpSuffix + ".data");
+        File ret = new File(file.getParentFile(), UUID.randomUUID().toString().replaceAll("-", "") + tmpSuffix + ".data");
+        try {
+            InputStream is = ResourceUtil.getClasspathResourceAsStream("assets/excel/map-export-template.xlsx");
+            StreamUtil.writeBytes(is, templateFile);
+
+            List<Map<String, Object>> data = new ArrayList<>();
+            for (Object item : map.keySet()) {
+                String prop = String.valueOf(item);
+                Map<String, Object> val = new HashMap<>();
+                val.put("label", prop);
+                val.put("prop", "{." + prop + "}");
+                data.add(val);
+            }
+
+            ExcelWriter excelWriter = EasyExcel.write(ret)
+                    .withTemplate(templateFile)
+                    .build();
+            WriteSheet writeSheet = EasyExcel.write()
+                    .sheet(0, "sheet")
+                    .build();
+            FillConfig fillConfig = FillConfig.builder()
+                    .direction(WriteDirectionEnum.HORIZONTAL)
+                    .build();
+            excelWriter.fill(data, fillConfig, writeSheet);
+            excelWriter.finish();
+            excelWriter.close();
+
+            return ret;
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        } finally {
+            templateFile.delete();
         }
     }
 
