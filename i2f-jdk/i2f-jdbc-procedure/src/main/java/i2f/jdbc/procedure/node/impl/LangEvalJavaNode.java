b71a043f265aa58d8e8bd1e85cfcda782d714ff1
Ice2Faith <Ugex_Savelar@163.com>
2025-02-19 09:20:35 +0800

    feat(jdbc): jdbc procedure eval-java default import packages

--- i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/node/impl/LangEvalJavaNode.java
+++ i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/node/impl/LangEvalJavaNode.java
@@ -1,11 +1,21 @@
 package i2f.jdbc.procedure.node.impl;
 
+import i2f.clock.SystemClock;
 import i2f.compiler.MemoryCompiler;
+import i2f.convert.obj.ObjectConvertor;
+import i2f.jdbc.JdbcResolver;
+import i2f.jdbc.procedure.annotations.JdbcProcedure;
+import i2f.jdbc.procedure.caller.JdbcProcedureExecutorCaller;
+import i2f.jdbc.procedure.caller.impl.DefaultJdbcProcedureExecutorCaller;
 import i2f.jdbc.procedure.consts.AttrConsts;
+import i2f.jdbc.procedure.consts.FeatureConsts;
 import i2f.jdbc.procedure.context.ExecuteContext;
 import i2f.jdbc.procedure.executor.JdbcProcedureExecutor;
 import i2f.jdbc.procedure.node.ExecutorNode;
 import i2f.jdbc.procedure.parser.data.XmlNode;
+import i2f.match.regex.RegexUtil;
+import i2f.reflect.ReflectResolver;
+import i2f.reflect.vistor.Visitor;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -78,6 +88,17 @@ public class LangEvalJavaNode implements ExecutorNode {
 
     }
 
+    public static String castAsImportPackageName(String className) {
+        if (className == null || className.isEmpty()) {
+            return null;
+        }
+        int idx = className.lastIndexOf(".");
+        if (idx <= 0) {
+            return className;
+        }
+        return className.substring(0, idx + 1) + "*";
+    }
+
     public static Object evalJava(ExecuteContext context, JdbcProcedureExecutor executor, String importSegment, String memberSegment, String bodySegment) {
         if (importSegment == null) {
             importSegment = "";
@@ -91,12 +112,37 @@ public class LangEvalJavaNode implements ExecutorNode {
         bodySegment = bodySegment.trim();
         Matcher matcher = RETURN_PATTERN.matcher(bodySegment);
         StringBuilder builder = new StringBuilder();
-        builder.append("import ").append(ExecuteContext.class.getName()).append(";").append("\n");
-        builder.append("import ").append(JdbcProcedureExecutor.class.getName()).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(JdbcProcedure.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(ExecuteContext.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(XmlNode.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(JdbcProcedureExecutor.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(JdbcProcedureExecutorCaller.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(DefaultJdbcProcedureExecutorCaller.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(FeatureConsts.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(JdbcResolver.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(RegexUtil.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(ReflectResolver.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(Visitor.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(ObjectConvertor.class.getName())).append(";").append("\n");
+        builder.append("import ").append(castAsImportPackageName(SystemClock.class.getName())).append(";").append("\n");
         builder.append("import ").append("java.util.*").append(";").append("\n");
         builder.append("import ").append("java.math.*").append(";").append("\n");
         builder.append("import ").append("java.time.*").append(";").append("\n");
         builder.append("import ").append("java.text.*").append(";").append("\n");
+        builder.append("import ").append("java.util.regex.*").append(";").append("\n");
+        builder.append("import ").append("java.io.*").append(";").append("\n");
+        builder.append("import ").append("java.lang.reflect.*").append(";").append("\n");
+        builder.append("import ").append("javax.sql.*").append(";").append("\n");
+        builder.append("import ").append("java.concurrent.*").append(";").append("\n");
+        builder.append("import ").append("java.nio.*").append(";").append("\n");
+        builder.append("import ").append("java.nio.charset.*").append(";").append("\n");
+        builder.append("import ").append("java.concurrent.atomic.*").append(";").append("\n");
+        builder.append("import ").append("java.concurrent.locks.*").append(";").append("\n");
+        builder.append("import ").append("java.time.chrono.*").append(";").append("\n");
+        builder.append("import ").append("java.time.format.*").append(";").append("\n");
+        builder.append("import ").append("java.time.temporal.*").append(";").append("\n");
+        builder.append("import ").append("java.time.zone.*").append(";").append("\n");
+
         builder.append(importSegment).append("\n");
         builder.append("public class ").append(CLASS_NAME_HOLDER).append("{").append("\n");
         builder.append(memberSegment).append("\n");
