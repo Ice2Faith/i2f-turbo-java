4cb497e67299d1a7b3a169a1413cba2b4152ae6e
Ice2Faith <Ugex_Savelar@163.com>
2025-02-24 10:01:32 +0800

    feat(procedure): jdbc procedure springboot starter scan log

--- i2f-springboot/i2f-springboot-jdbc-bql-starter/src/main/java/i2f/springboot/jdbc/bql/procedure/SpringJdbcProcedureNodeMapCacheSupplier.java
+++ i2f-springboot/i2f-springboot-jdbc-bql-starter/src/main/java/i2f/springboot/jdbc/bql/procedure/SpringJdbcProcedureNodeMapCacheSupplier.java
@@ -39,11 +39,14 @@ public class SpringJdbcProcedureNodeMapCacheSupplier extends AbstractJdbcProcedu
         if (xmlLocations == null || xmlLocations.isEmpty()) {
             return ret;
         }
+        long beginTs=System.currentTimeMillis();
+        log.info("procedure resource finding ...");
         Set<Resource> resources = new HashSet<>();
         for (String location : xmlLocations) {
             if (location == null || location.isEmpty()) {
                 continue;
             }
+            log.info("procedure scan resources: "+location);
             try {
                 Resource[] arr = resourcePatternResolver.getResources(location);
                 if (arr != null) {
@@ -53,37 +56,65 @@ public class SpringJdbcProcedureNodeMapCacheSupplier extends AbstractJdbcProcedu
 
             }
         }
+        int resoucesCnt= resources.size();
+        int successCnt=0;
+        int nodeCnt=0;
+        int failureCnt=0;
+        log.info("procedure resource found "+resoucesCnt+".");
+        log.info("procedure resource parsing ...");
+
         for (Resource resource : resources) {
             try (InputStream is = resource.getInputStream()) {
                 XmlNode node = JdbcProcedureParser.parse(is);
                 String id = node.getTagAttrMap().get("id");
+                String filename = resource.getFilename();
+                String fileNameId=filename;
+                if (filename != null) {
+                    int idx = filename.lastIndexOf(".");
+                    if (idx >= 0) {
+                        fileNameId = filename.substring(0, idx);
+                    }
+                }
+                if(id!=null && fileNameId!=null){
+                    if(!id.equals(fileNameId)){
+                        log.warn("resource filename not equal id, filename="+filename+", id="+id);
+                    }
+                }
                 if (id == null) {
-                    String filename = resource.getFilename();
                     if (filename != null) {
-                        id = filename;
-                        int idx = filename.lastIndexOf(".");
-                        if (idx >= 0) {
-                            id = filename.substring(0, idx);
-                        }
+                        id = fileNameId;
                     }
                 }
                 if (id != null) {
                     ret.put(id, node);
+                    successCnt++;
+                    nodeCnt++;
+                    log.debug("load procedure resource: id="+id+", filename="+filename);
 
                     Map<String, XmlNode> next = new HashMap<>();
                     JdbcProcedureParser.resolveEmbedIdNode(node, next);
                     for (Map.Entry<String, XmlNode> entry : next.entrySet()) {
-                        ret.put(entry.getKey(), entry.getValue());
                         if(!entry.getKey().equals(id)){
+                            nodeCnt++;
                             String childId = id + "." + entry.getKey();
+                            log.debug("load procedure child node resource: id="+childId+", filename="+filename);
                             ret.put(childId, entry.getValue());
                         }
                     }
                 }
             } catch (Exception e) {
-                log.warn(resource.getFilename()+" parse error: "+e.getMessage(), e);
+                failureCnt++;
+                log.warn(resource.getFilename() + " parse error: " + e.getMessage(), e);
             }
         }
+        long useTs=System.currentTimeMillis()-beginTs;
+        log.info("procedure resource parsed"
+                +", use seconds="+Math.round(useTs/1000.0)
+                +", resource count="+resoucesCnt
+                +", success count="+successCnt
+                +", failure count="+failureCnt
+                +", node count="+nodeCnt
+                +", result count"+ret.size());
         return ret;
     }
 
