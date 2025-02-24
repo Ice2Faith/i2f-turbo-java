b8e3aec7e57161cf7a3f74f16f04f1caa7d3e813
Ice2Faith <Ugex_Savelar@163.com>
2025-02-24 11:52:09 +0800

    feat(procedure): jdbc procedure springboot starter scan log

--- i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/caller/JdbcProcedureExecutorCaller.java
+++ i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/caller/JdbcProcedureExecutorCaller.java
@@ -1,6 +1,10 @@
 package i2f.jdbc.procedure.caller;
 
+import i2f.container.builder.map.MapBuilder;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  * @author Ice2Faith
@@ -8,7 +12,20 @@ import java.util.Map;
  */
 public interface JdbcProcedureExecutorCaller {
 
+    default <T> T invoke(String procedureId, Consumer<MapBuilder<String,Object,? extends Map<String,Object>>> consumer){
+        MapBuilder<String, Object, HashMap<String, Object>> builder = new MapBuilder<>(new HashMap<String, Object>());
+        consumer.accept(builder);
+        return invoke(procedureId,builder.get());
+    }
+
     <T> T invoke(String procedureId, Map<String, Object> params);
 
+    default void call(String procedureId, Consumer<MapBuilder<String,Object,? extends Map<String,Object>>> consumer){
+        MapBuilder<String, Object, HashMap<String, Object>> builder = new MapBuilder<>(new HashMap<String, Object>());
+        consumer.accept(builder);
+        call(procedureId,builder.get());
+    }
+
     void call(String procedureId, Map<String, Object> params);
+
 }
--- i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/executor/JavaCallerHelper.java
+++ i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/executor/JavaCallerHelper.java
@@ -1,11 +1,13 @@
 package i2f.jdbc.procedure.executor;
 
+import i2f.container.builder.map.MapBuilder;
 import i2f.jdbc.procedure.caller.JdbcProcedureExecutorCaller;
 import i2f.jdbc.procedure.caller.impl.DefaultJdbcProcedureExecutorCaller;
 import i2f.jdbc.procedure.context.ContextFunctions;
 import i2f.jdbc.procedure.context.ExecuteContext;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -21,44 +23,48 @@ public class JavaCallerHelper extends ContextFunctions implements JdbcProcedureE
     public JavaCallerHelper(ExecuteContext context, JdbcProcedureExecutor executor) {
         this.context = context;
         this.executor = executor;
-        this.caller=new DefaultJdbcProcedureExecutorCaller(executor,context);
+        this.caller = new DefaultJdbcProcedureExecutorCaller(executor, context);
     }
 
-    public <T> T visit(String expression){
-        return (T)executor.visit(expression,context.getParams());
+    public <T> T visit(String expression) {
+        return (T) executor.visit(expression, context.getParams());
     }
 
-    public boolean test(String expression){
-        return executor.test(expression,context.getParams());
+    public boolean test(String expression) {
+        return executor.test(expression, context.getParams());
     }
 
-    public <T> T eval(String expression){
-        return (T)executor.eval(expression,context.getParams());
+    public <T> T eval(String expression) {
+        return (T) executor.eval(expression, context.getParams());
     }
 
-    public String render(String expression){
-        return executor.render(expression,context.getParams());
+    public String render(String expression) {
+        return executor.render(expression, context.getParams());
     }
 
-    public <T> T get(String expression){
+    public <T> T get(String expression) {
         return visit(expression);
     }
 
-    public void set(String expression,Object value){
-        executor.setParamsObject(context.getParams(),expression,value);
+    public void set(String expression, Object value) {
+        executor.setParamsObject(context.getParams(), expression, value);
     }
 
-    public JdbcProcedureExecutorCaller caller(){
+    public JdbcProcedureExecutorCaller caller() {
         return this.caller;
     }
 
     @Override
     public <T> T invoke(String procedureId, Map<String, Object> params) {
-        return this.caller.invoke(procedureId,params);
+        return this.caller.invoke(procedureId, params);
     }
 
     @Override
     public void call(String procedureId, Map<String, Object> params) {
-        this.caller.call(procedureId,params);
+        this.caller.call(procedureId, params);
+    }
+
+    public MapBuilder<String,Object,? extends Map<String,Object>> newHashMap(){
+        return new MapBuilder<>(new HashMap<>(),String.class,Object.class);
     }
 }
