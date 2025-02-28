8273d5d4932c1a06c630c52eee661f4ce94b5579
Ice2Faith <Ugex_Savelar@163.com>
2025-02-28 11:57:51 +0800

    perf(ognl/velocity): turbo ognl / velocity perf

--- i2f-extension/i2f-extension-ognl/src/main/java/i2f/extension/ognl/OgnlUtil.java
+++ i2f-extension/i2f-extension-ognl/src/main/java/i2f/extension/ognl/OgnlUtil.java
@@ -1,18 +1,41 @@
 package i2f.extension.ognl;
 
+import i2f.lru.LruMap;
 import ognl.Ognl;
 import ognl.OgnlContext;
+import ognl.OgnlException;
 
 /**
  * @author Ice2Faith
  * @date 2024/10/9 13:49
  */
 public class OgnlUtil {
+    public static final LruMap<String,Object> EXPRESSION_MAP=new LruMap<>(4096);
+
     public static Object evaluateExpression(String expression, Object root) throws Exception {
         OgnlContext context = new OgnlContext(null, null, DefaultMemberAccess.INSTANCE);
         context.put("$root", root);
         context.setRoot(root);
-        Object tree = Ognl.parseExpression(expression);
+        Object tree=parseExpressionTreeNode(expression);
         return Ognl.getValue(tree, context, context.getRoot());
     }
+
+    public static Object parseExpressionTreeNode(String expression) throws OgnlException {
+        try {
+            Object ret = EXPRESSION_MAP.get(expression);
+            if(ret!=null){
+                return ret;
+            }
+        } catch (Exception e) {
+
+        }
+        Object tree = Ognl.parseExpression(expression);
+        try {
+            EXPRESSION_MAP.put(expression,tree);
+        } catch (Exception e) {
+
+        }
+        return tree;
+    }
+
 }
