f409a826c5b61159ee757e950dab2ce200887daf
Ice2Faith <Ugex_Savelar@163.com>
2025-03-05 10:27:00 +0800

    fix(tiny-script): runtime error exception with source location

---
+++ i2f-extension/i2f-extension-antlr4/src/main/java/i2f/extension/antlr4/script/tiny/impl/exception/TinyScriptException.java
@@ -0,0 +1,26 @@
+package i2f.extension.antlr4.script.tiny.impl.exception;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/3/5 9:48
+ */
+public class TinyScriptException extends RuntimeException{
+    public TinyScriptException() {
+    }
+
+    public TinyScriptException(String message) {
+        super(message);
+    }
+
+    public TinyScriptException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TinyScriptException(Throwable cause) {
+        super(cause);
+    }
+
+    public TinyScriptException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
