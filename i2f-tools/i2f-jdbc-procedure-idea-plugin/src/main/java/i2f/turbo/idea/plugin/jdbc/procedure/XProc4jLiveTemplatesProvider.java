ed4c5d5424eecff6cbd6596e23c5e3d336982fd5
Ice2Faith <Ugex_Savelar@163.com>
2025-03-26 23:48:41 +0800

    feat(xproc4j): live templates for xproc4j

---
+++ i2f-tools/i2f-jdbc-procedure-idea-plugin/src/main/java/i2f/turbo/idea/plugin/jdbc/procedure/XProc4jLiveTemplatesProvider.java
@@ -0,0 +1,21 @@
+package i2f.turbo.idea.plugin.jdbc.procedure;
+
+import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/3/26 23:35
+ * @desc
+ */
+public class XProc4jLiveTemplatesProvider implements DefaultLiveTemplatesProvider {
+    @Override
+    public String[] getDefaultLiveTemplateFiles() {
+        return new String[]{"assets/liveTemplates/XProc4j.xml"};
+    }
+
+    @Override
+    public String @Nullable [] getHiddenLiveTemplateFiles() {
+        return new String[0];
+    }
+}
