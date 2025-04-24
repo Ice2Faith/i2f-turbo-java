e49677003a5eb74d7993047765cc72a6d61433a8
Ice2Faith <Ugex_Savelar@163.com>
2025-04-24 17:33:34 +0800

    feat(xproc4j): add PreparedParamsEvent for support custom modify params on global, add XmlExecUseTimeEvent for support statistic perform or slow node.

---
+++ i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/node/event/XmlExecUseTimeEvent.java
@@ -0,0 +1,22 @@
+package i2f.jdbc.procedure.node.event;
+
+import i2f.jdbc.procedure.executor.event.ExecutorContextEvent;
+import i2f.jdbc.procedure.node.ExecutorNode;
+import i2f.jdbc.procedure.parser.data.XmlNode;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/4/24 14:05
+ */
+@Data
+@NoArgsConstructor
+public class XmlExecUseTimeEvent extends ExecutorContextEvent {
+    protected ExecutorNode executorNode;
+    protected Map<String,Object> pointContext;
+    protected XmlNode node;
+    protected long useTs;
+}
