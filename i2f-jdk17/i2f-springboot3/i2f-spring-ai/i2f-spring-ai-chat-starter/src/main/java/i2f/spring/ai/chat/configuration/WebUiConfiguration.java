ffda0c319ea72b26cf59839190838ec23fad6da3
Ice2Faith <Ugex_Savelar@163.com>
2025-05-28 09:02:17 +0800

    feat(spring-ai): chat-ai view forward/redirect support

--- i2f-jdk17/i2f-springboot3/i2f-spring-ai/i2f-spring-ai-chat-starter/src/main/java/i2f/spring/ai/chat/configuration/WebUiConfiguration.java
+++ i2f-jdk17/i2f-springboot3/i2f-spring-ai/i2f-spring-ai-chat-starter/src/main/java/i2f/spring/ai/chat/configuration/WebUiConfiguration.java
@@ -1,11 +1,14 @@
 package i2f.spring.ai.chat.configuration;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -13,10 +16,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @date 2025/4/27 16:31
  */
 @Slf4j
+@Data
 @ConditionalOnExpression("${i2f.ai.chat.web-ui.enable:true}")
 @Configuration
 public class WebUiConfiguration implements WebMvcConfigurer, EnvironmentAware {
     protected Environment environment;
+
+    @Value("${i2f.ai.chat.web-ui.allow-redirect-root-url.enable:false}")
+    protected boolean allowRedirectRootUrl=false;
 
     @Override
     public void setEnvironment(Environment environment) {
@@ -30,5 +37,15 @@ public class WebUiConfiguration implements WebMvcConfigurer, EnvironmentAware {
                 .setCachePeriod(3600) // 设置缓存时间（可选）
                 .resourceChain(true); // 启用资源链（可选）
         log.info("Chat-AI simple web-ui: http://localhost:" + environment.getProperty("server.port", "8080") + "/chat-ai/index.html");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/chat-ai")
+                .setViewName("forward:/chat-ai/index.html");
+        if(allowRedirectRootUrl) {
+            registry.addViewController("/")
+                    .setViewName("redirect:/chat-ai");
+        }
     }
 }
