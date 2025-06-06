4609c8f6152bac3d6d640fa1db8c1e9782905085
Ice2Faith <Ugex_Savelar@163.com>
2025-06-06 09:54:37 +0800

    feat(browser-selenium): selenium search engine scratch

--- i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/BiYingSearch.java
+++ i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/BiYingSearch.java
@@ -58,7 +58,24 @@ public class BiYingSearch {
         AtomicInteger maxFetchCount = new AtomicInteger(maxArticleCount);
         // 打开目标网页
         WebDriver driver = BrowserSelenium.getWebDriver(null, true, driverPath);
+        if(true) {
+            driver.get("https://cn.bing.com/");
 
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+            try {
+                wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".sbox_cn form"), 0));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            WebElement inputElem = driver.findElement(By.cssSelector(".sbox_cn form #sb_form_q"));
+            inputElem.click();
+            inputElem.sendKeys(question);
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(RANDOM.nextInt(3) + 1));
+            WebElement enterElem = driver.findElement(By.cssSelector(".sbox_cn form #search_icon"));
+            enterElem.click();
+        }
         try {
 
             while (true) {
@@ -83,7 +100,9 @@ public class BiYingSearch {
                     driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
                 }
                 try {
-                    driver.navigate().to(entry.getKey().getUrl());
+                    if(!Objects.equals(driver.getCurrentUrl(),entry.getKey().getUrl())) {
+                        driver.navigate().to(entry.getKey().getUrl());
+                    }
                 } catch (Exception e) {
                     continue;
                 }
