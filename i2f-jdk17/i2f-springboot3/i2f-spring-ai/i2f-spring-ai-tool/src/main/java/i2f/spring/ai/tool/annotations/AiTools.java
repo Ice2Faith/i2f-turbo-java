f060155d2829a0670d801e9c06839fda43454ba3
Ice2Faith <Ugex_Savelar@163.com>
2025-06-06 20:56:58 +0800

    feat(spring-ai): spring-ai with mcp

--- i2f-jdk17/i2f-springboot3/i2f-spring-ai/i2f-spring-ai-tool/src/main/java/i2f/spring/ai/tool/annotations/AiTools.java
+++ i2f-jdk17/i2f-springboot3/i2f-spring-ai/i2f-spring-ai-tool/src/main/java/i2f/spring/ai/tool/annotations/AiTools.java
@@ -1,4 +1,4 @@
-package i2f.spring.ai.chat.annotations;
+package i2f.spring.ai.tool.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -13,6 +13,6 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface EnabledTools {
+public @interface AiTools {
 
 }
