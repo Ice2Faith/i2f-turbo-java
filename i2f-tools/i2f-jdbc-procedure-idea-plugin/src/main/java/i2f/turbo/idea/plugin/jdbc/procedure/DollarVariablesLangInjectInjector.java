3d7f4dd71e00f4ade656b9c10063c8ac93dc92ed
Ice2Faith <Ugex_Savelar@163.com>
2025-06-25 14:59:18 +0800

    feat(xproc-plugin): highlight dollar variables for ${} & #{} & $!{} & #!{}

---
+++ i2f-tools/i2f-jdbc-procedure-idea-plugin/src/main/java/i2f/turbo/idea/plugin/jdbc/procedure/DollarVariablesLangInjectInjector.java
@@ -0,0 +1,121 @@
+package i2f.turbo.idea.plugin.jdbc.procedure;
+
+import com.intellij.lang.injection.MultiHostInjector;
+import com.intellij.lang.injection.MultiHostRegistrar;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.HighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.event.EditorMouseEvent;
+import com.intellij.openapi.editor.event.EditorMouseListener;
+import com.intellij.openapi.editor.markup.HighlighterTargetArea;
+import com.intellij.openapi.editor.markup.MarkupModel;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiLiteral;
+import com.intellij.psi.PsiLiteralExpression;
+import com.intellij.psi.PsiLiteralValue;
+import com.intellij.psi.util.PsiEditorUtil;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.psi.xml.XmlText;
+import i2f.match.regex.RegexUtil;
+import i2f.match.regex.data.RegexMatchItem;
+import i2f.turbo.idea.plugin.tinyscript.grammar.psi.elements.TinyScriptConstMultilineString;
+import i2f.turbo.idea.plugin.tinyscript.grammar.psi.elements.TinyScriptConstRenderString;
+import i2f.turbo.idea.plugin.tinyscript.grammar.psi.elements.TinyScriptConstString;
+import org.apache.commons.collections.map.HashedMap;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/6/25 10:18
+ */
+public class DollarVariablesLangInjectInjector implements MultiHostInjector {
+    public static final Logger log = Logger.getInstance(DollarVariablesLangInjectInjector.class);
+
+    @Override
+    public void getLanguagesToInject(@NotNull MultiHostRegistrar multiHostRegistrar, @NotNull PsiElement psiElement) {
+        highlighterDollarVariables(psiElement);
+    }
+
+    @Override
+    public @NotNull List<? extends Class<? extends PsiElement>> elementsToInjectIn() {
+        return List.copyOf(elementTypeLayerMapping.keySet());
+    }
+
+    public static final Map<Class<? extends PsiElement>,Integer> elementTypeLayerMapping=new LinkedHashMap<>(){
+        {
+            int topLayer=99999;
+            int normalLayer=99999;//100
+            // xml 字符串
+            put(XmlText.class,normalLayer);
+            put(XmlAttribute.class,normalLayer);
+            put(XmlAttributeValue.class,topLayer);
+            // java 字符串
+            put(PsiLiteralExpression.class,topLayer);
+            // ts字符串
+            put(TinyScriptConstString.class,topLayer);
+            put(TinyScriptConstRenderString.class,topLayer);
+            put(TinyScriptConstMultilineString.class,topLayer);
+            // IDEA 标准的字符串
+            put(PsiLiteral.class,topLayer);
+            put(PsiLiteralValue.class,topLayer);
+        }
+    };
+
+
+    public static void highlighterDollarVariables(PsiElement context) {
+        if(context==null){
+            return;
+        }
+        Editor editor = PsiEditorUtil.findEditor(context);
+        if(editor==null){
+            return;
+        }
+        Class<? extends PsiElement> clazz = context.getClass();
+        int layer=100;
+        Integer mappingLayer = elementTypeLayerMapping.get(clazz);
+        if(mappingLayer==null){
+            for (Map.Entry<Class<? extends PsiElement>, Integer> entry : elementTypeLayerMapping.entrySet()) {
+                Class<? extends PsiElement> item = entry.getKey();
+                if(item.isAssignableFrom(clazz)){
+                    mappingLayer=entry.getValue();
+                    break;
+                }
+            }
+        }
+        if(mappingLayer!=null){
+            layer=mappingLayer;
+        }
+//        log.warn("dollar-vars-layer:"+clazz.getSimpleName()+":"+layer);
+        String text= context.getText();
+        int startOffset = context.getTextOffset();
+        MarkupModel markupModel = editor.getMarkupModel();
+        List<RegexMatchItem> list = RegexUtil.regexFinds(text, "[$#]([!])?\\{[^\\}]*\\}");
+        for (RegexMatchItem item : list) {
+//            log.warn("dollar-vars:"+item.matchStr+"["+item.idxStart+","+item.idxEnd+"]");
+            TextAttributesKey key=DefaultLanguageHighlighterColors.NUMBER;
+//            if(item.matchStr.startsWith("$")){
+//                key=DefaultLanguageHighlighterColors.METADATA;
+//            }
+            if(item.matchStr.replaceAll("\\s+","").contains("{}")){
+                key= HighlighterColors.BAD_CHARACTER;
+            }
+
+            markupModel.addRangeHighlighter(
+                    key,
+                    startOffset+item.idxStart,
+                    startOffset+item.idxEnd,layer,
+                    HighlighterTargetArea.EXACT_RANGE
+            );
+        }
+    }
+}
