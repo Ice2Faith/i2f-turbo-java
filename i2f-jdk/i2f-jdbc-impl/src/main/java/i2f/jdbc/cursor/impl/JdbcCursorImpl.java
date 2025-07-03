9ce299c85b14ecc3763e1109b2c66904e01ddda1
Ice2Faith <Ugex_Savelar@163.com>
2025-07-03 11:52:55 +0800

    refactor(jdbc): refactor jdbc

--- i2f-jdk/i2f-jdbc-impl/src/main/java/i2f/jdbc/cursor/impl/JdbcCursorImpl.java
+++ i2f-jdk/i2f-jdbc-impl/src/main/java/i2f/jdbc/cursor/impl/JdbcCursorImpl.java
@@ -1,39 +1,79 @@
 package i2f.jdbc.cursor.impl;
 
+import i2f.jdbc.JdbcResolver;
 import i2f.jdbc.cursor.JdbcCursor;
-import i2f.jdbc.std.func.SQLBiFunction;
-import i2f.jdbc.std.func.SQLFunction;
+import i2f.jdbc.data.QueryColumn;
 
 import java.io.Closeable;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * @author Ice2Faith
  * @date 2025/6/25 16:10
  */
-public class JdbcCursorImpl<E, CTX> implements JdbcCursor<E>, Closeable, Iterable<E> {
+public class JdbcCursorImpl<E> implements JdbcCursor<E>, Closeable, Iterable<E> {
     protected Statement stat;
     protected ResultSet rs;
-    protected SQLFunction<ResultSet, CTX> contextInitializer;
-    protected SQLBiFunction<CTX, ResultSet, E> rowConvertor;
-    protected AtomicReference<CTX> context;
+    protected ResultSetMetaData metaData;
+    protected List<QueryColumn> columns;
+    protected Function<Map<String, Object>, E> rowConvertor;
     protected boolean hasMore = false;
 
-    public JdbcCursorImpl(Statement stat, ResultSet rs, SQLBiFunction<CTX, ResultSet, E> rowConvertor, SQLFunction<ResultSet, CTX> contextInitializer) {
+    public JdbcCursorImpl(Statement stat, ResultSet rs,
+                          ResultSetMetaData metaData,
+                          List<QueryColumn> columns,
+                          Function<Map<String, Object>, E> rowConvertor) {
         this.stat = stat;
         this.rs = rs;
-        this.contextInitializer = contextInitializer;
+        this.metaData = metaData;
+        this.columns = columns;
         this.rowConvertor = rowConvertor;
     }
 
+    public JdbcCursorImpl(Statement stat, ResultSet rs,
+                          Class<E> beanClass,
+                          Function<String, String> columnNameMapper) throws SQLException {
+        this.stat = stat;
+        this.rs = rs;
+        this.metaData = rs.getMetaData();
+        this.columns = JdbcResolver.parseResultSetColumns(metaData, columnNameMapper);
+        this.rowConvertor = JdbcResolver.createDefaultRowConvertor(columns, beanClass);
+    }
 
+    public JdbcCursorImpl(Statement stat, ResultSet rs,
+                          Function<String, String> columnNameMapper,
+                          Function<Map<String, Object>, E> rowConvertor) throws SQLException {
+        this.stat = stat;
+        this.rs = rs;
+        this.metaData = rs.getMetaData();
+        this.columns = JdbcResolver.parseResultSetColumns(metaData, columnNameMapper);
+        this.rowConvertor = rowConvertor;
+    }
+
+    public JdbcCursorImpl(Statement stat, ResultSet rs,
+                          Class<E> beanClass,
+                          Function<String, String> columnNameMapper,
+                          BiFunction<List<QueryColumn>, Class<E>, Function<Map<String, Object>, E>> rowConvertorBuilder) throws SQLException {
+        this.stat = stat;
+        this.rs = rs;
+        this.metaData = rs.getMetaData();
+        this.columns = JdbcResolver.parseResultSetColumns(metaData, columnNameMapper);
+        this.rowConvertor = rowConvertorBuilder.apply(columns, beanClass);
+    }
+
+    @Override
     public Statement getStatement() {
         return stat;
     }
 
+    @Override
     public ResultSet getResultSet() {
         return rs;
     }
@@ -73,11 +113,8 @@ public class JdbcCursorImpl<E, CTX> implements JdbcCursor<E>, Closeable, Iterabl
 
     @Override
     public E nextRow() throws SQLException {
-        if (context == null) {
-            CTX ctx = contextInitializer.apply(rs);
-            context = new AtomicReference<>(ctx);
-        }
-        E ret = rowConvertor.apply(context.get(), rs);
+        Map<String, Object> map = JdbcResolver.convertResultSetRowAsMap(columns, rs);
+        E ret = rowConvertor.apply(map);
         hasMore = false;
         return ret;
     }
