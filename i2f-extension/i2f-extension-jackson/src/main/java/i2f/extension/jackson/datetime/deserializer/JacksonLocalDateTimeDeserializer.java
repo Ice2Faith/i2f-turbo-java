5da3475621abbd9ced4c78587fc97fb9a574a83c
Ice2Faith <Ugex_Savelar@163.com>
2025-07-10 18:12:46 +0800

    fix(jackson): spring jackson serialize config

--- i2f-extension/i2f-extension-jackson/src/main/java/i2f/extension/jackson/datetime/deserializer/JacksonLocalDateTimeDeserializer.java
+++ i2f-extension/i2f-extension-jackson/src/main/java/i2f/extension/jackson/datetime/deserializer/JacksonLocalDateTimeDeserializer.java
@@ -1,10 +1,14 @@
-package i2f.extension.jackson.datetime;
+package i2f.extension.jackson.datetime.deserializer;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
 import i2f.convert.obj.ObjectConvertor;
-import i2f.extension.jackson.base.BaseJacksonContextDeserializer;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -15,14 +19,29 @@ import java.time.format.DateTimeFormatter;
  * @date 2023/6/16 23:18
  * @desc
  */
-public class JacksonLocalDateTimeDeserializer extends BaseJacksonContextDeserializer<LocalDateTime> {
+public class JacksonLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> implements ContextualDeserializer {
     private DateTimeFormatter formatter;
-
-    public JacksonLocalDateTimeDeserializer() {
-    }
 
     public JacksonLocalDateTimeDeserializer(DateTimeFormatter formatter) {
         this.formatter = formatter;
+    }
+
+    @Override
+    public JsonDeserializer<?> createContextual(DeserializationContext deserializationContext, BeanProperty beanProperty) throws JsonMappingException {
+        if (beanProperty == null) {
+            return this;
+        }
+        JsonFormat ann = beanProperty.getAnnotation(JsonFormat.class);
+        if (ann == null) {
+            ann = beanProperty.getAnnotation(JsonFormat.class);
+        }
+        if (ann != null) {
+            String pattern = ann.pattern();
+            if (pattern != null && !pattern.isEmpty()) {
+                return new JacksonLocalDateTimeDeserializer(DateTimeFormatter.ofPattern(pattern));
+            }
+        }
+        return this;
     }
 
     @Override
@@ -50,13 +69,6 @@ public class JacksonLocalDateTimeDeserializer extends BaseJacksonContextDeserial
                 return LocalDateTime.parse(str, formatter);
             }
         } catch (Exception e) {
-
-        }
-        try {
-            if (formatPatten != null) {
-                return ObjectConvertor.parseLocalDateTime(formatPatten, str);
-            }
-        } catch (Exception e1) {
 
         }
         return ObjectConvertor.parseLocalDateTime(str);
