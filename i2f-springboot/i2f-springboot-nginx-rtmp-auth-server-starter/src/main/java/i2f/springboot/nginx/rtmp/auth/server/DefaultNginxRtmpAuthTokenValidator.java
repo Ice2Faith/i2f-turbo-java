9c49ae5c52dc70c1dcc917f3f998b103ad33aaad
Ice2Faith <Ugex_Savelar@163.com>
2025-08-03 15:35:20 +0800

    feat(nginx-rtmp): add NginxRtmpAuthController

---
+++ i2f-springboot/i2f-springboot-nginx-rtmp-auth-server-starter/src/main/java/i2f/springboot/nginx/rtmp/auth/server/DefaultNginxRtmpAuthTokenValidator.java
@@ -0,0 +1,32 @@
+package i2f.springboot.nginx.rtmp.auth.server;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/8/3 15:23
+ */
+@ConditionalOnExpression("${i2f.springboot.nginx.rtmp.auth.default-validator.enable:true}")
+@ConfigurationProperties(prefix = "i2f.springboot.nginx.rtmp.auth.default-validator")
+@Slf4j
+@Component
+public class DefaultNginxRtmpAuthTokenValidator implements NginxRtmpAuthTokenValidator{
+    private String accessToken;
+
+    @Override
+    public boolean validate(HttpServletRequest request, String token, Map<String, Object> nginxData) {
+        if (!StringUtils.isEmpty(token)) {
+            if (!token.equals(accessToken)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
