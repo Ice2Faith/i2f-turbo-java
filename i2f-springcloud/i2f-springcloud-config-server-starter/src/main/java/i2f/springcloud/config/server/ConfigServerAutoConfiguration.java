4cf5f09528c37d21e017108ac5654ede928f0e76
Ice2Faith <Ugex_Savelar@163.com>
2025-08-03 17:57:39 +0800

    feat(cloud-spring): add spring cloud spring

---
+++ i2f-springcloud/i2f-springcloud-config-server-starter/src/main/java/i2f/springcloud/config/server/ConfigServerAutoConfiguration.java
@@ -0,0 +1,25 @@
+package i2f.springcloud.config.server;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ltb
+ * @date 2022/6/12 20:37
+ * @desc
+ */
+@Slf4j
+@Data
+@NoArgsConstructor
+@ConditionalOnExpression("${i2f.springcloud.config-server.enable:true}")
+@ConfigurationProperties(prefix = "i2f.springcloud.config-server")
+@Configuration
+@EnableConfigServer
+public class ConfigServerAutoConfiguration {
+
+}
