cecf316f863018db07700dab8fcf2778868721fa
Ice2Faith <Ugex_Savelar@163.com>
2025-08-14 21:34:45 +0800

    feat(sm-crypto): compare test

--- i2f-jdk/i2f-sm-crypto/src/test/java/i2f/sm/crypto/test/TestSm.java
+++ i2f-jdk/i2f-sm-crypto/src/test/java/i2f/sm/crypto/test/TestSm.java
@@ -1,10 +1,12 @@
 package i2f.sm.crypto.test;
 
 import i2f.sm.crypto.sm2.KeyPair;
-import i2f.sm.crypto.sm2.Sm2Cipher;
+import i2f.sm.crypto.sm2.Sm2;
 import i2f.sm.crypto.sm2.Utils;
 import i2f.sm.crypto.sm3.Sm3;
 import i2f.sm.crypto.sm4.Sm4;
+
+import java.util.Arrays;
 
 /**
  * @author Ice2Faith
@@ -14,7 +16,6 @@ import i2f.sm.crypto.sm4.Sm4;
 public class TestSm {
     public static void main(String[] args) throws Exception {
 
-
         testSm3();
 
         testSm4();
@@ -23,7 +24,7 @@ public class TestSm {
     }
 
     public static void testSm3() throws Exception {
-        System.out.println("Testing Sm3 ==============");
+        System.out.println("\n\nTesting Sm3 ==============");
         String text = "Hello你好 World世界!";
         System.out.println("text: " + text);
         String s1 = Sm3.sm3(text);
@@ -31,10 +32,11 @@ public class TestSm {
         String s2 = Sm3.sm3(text);
         System.out.println("s2: " + s2);
         assert s1.equals(s2);
+
     }
 
     public static void testSm4() throws Exception {
-        System.out.println("Testing Sm4 ==============");
+        System.out.println("\n\nTesting Sm4 ==============");
         String key = Sm4.generateHexKey();
         String text = "Hello你好 World世界!";
         System.out.println("text: " + text);
@@ -46,21 +48,24 @@ public class TestSm {
     }
 
     public static void testSm2() throws Exception {
-        System.out.println("Testing Sm2 ==============");
+        System.out.println("\n\nTesting Sm2 ==============");
         KeyPair key = Utils.generateKeyPairHex();
         String text = "Hello你好 World世界!";
         System.out.println("text: " + text);
         System.out.println("========sign/verify");
-        String sign = Sm2Cipher.doSignature(text, key.getPrivateKey());
+        String sign = Sm2.doSignature(text, key.getPrivateKey());
         System.out.println("sign: " + sign);
-        boolean ok = Sm2Cipher.doVerifySignature(text, sign, key.getPublicKey());
+        boolean ok = Sm2.doVerifySignature(text, sign, key.getPublicKey());
         System.out.println("ok: " + ok);
 
-        System.out.println("========encrypt/decrypt");
-        String enc = Sm2Cipher.doEncrypt(text, key.getPublicKey(),null);
+
+        System.out.println("\n\n========encrypt/decrypt");
+        String enc = Sm2.doEncrypt(text, key.getPublicKey());
         System.out.println("enc: " + enc);
-        String dec = Sm2Cipher.doDecrypt(enc, key.getPrivateKey(),null);
+        String dec = Sm2.doDecrypt(enc, key.getPrivateKey());
         System.out.println("dec: " + dec);
         assert text.equals(dec);
+        assert Arrays.equals(text.getBytes("UTF-8"), dec.getBytes("UTF-8"));
+
     }
 }
