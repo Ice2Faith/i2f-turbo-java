ea4feb84e69d3e14de68ebeded6f7a17201df9d9
Ice2Faith <Ugex_Savelar@163.com>
2025-08-15 10:48:15 +0800

    fix(sm-crypto): sm2

--- i2f-jdk/i2f-sm-crypto/src/main/java/i2f/sm/crypto/sm2/Sm2Cipher.java
+++ i2f-jdk/i2f-sm-crypto/src/main/java/i2f/sm/crypto/sm2/Sm2Cipher.java
@@ -26,10 +26,76 @@ public class Sm2Cipher {
 
     public static final EcParam EC_PARAM = Utils.generateEcparam();
 
-    public static String doEncrypt(String msg, String publicKey, CipherMode cipherMode) {
-        byte[] msgArr = Utils.hexToArray(Utils.utf8ToHex(msg));
-        byte[] retArr = doEncrypt(msgArr, publicKey, cipherMode);
-        return CipUtils.arrayToHex(retArr);
+//    public static String doEncrypt(String msg, String publicKey, CipherMode cipherMode) {
+//        byte[] msgArr = Utils.hexToArray(Utils.utf8ToHex(msg));
+//        byte[] retArr = doEncrypt(msgArr, publicKey, cipherMode);
+//        return CipUtils.arrayToHex(retArr);
+//    }
+
+    public static String doEncrypt(String msgStr, String publicKeyHex, CipherMode cipherMode) {
+        if (cipherMode == null) {
+            cipherMode = CipherMode.C1C3C2;
+        }
+        byte[] msg = Utils.hexToArray(Utils.utf8ToHex(msgStr));
+        EcPointFp publicKey = Utils.getGlobalCurve().decodePointHex(publicKeyHex); // 先将公钥转成点
+
+    KeyPair keypair = Utils.generateKeyPairHex();
+    BigInteger k = new BigInteger(keypair.privateKey, 16); // 随机数 k
+
+        // c1 = k * G
+        String c1 = keypair.publicKey;
+        if (c1.length() > 128) {
+            c1 = c1.substring(c1.length() - 128);
+        }
+
+        // (x2, y2) = k * publicKey
+    EcPointFp p = publicKey.multiply(k);
+    byte[] x2 = Utils.hexToArray(CipUtils.leftPad(p.getX().toBigInteger().toString(16), 64));
+    byte[] y2 = Utils.hexToArray(CipUtils.leftPad(p.getY().toBigInteger().toString(16), 64));
+
+        // c3 = hash(x2 || msg || y2)
+    // const c3 = _.arrayToHex(sm3([].concat(x2, msg, y2)))
+        byte[] tmp = new byte[x2.length + msg.length + y2.length];
+        System.arraycopy(x2, 0, tmp, 0, x2.length);
+        System.arraycopy(msg, 0, tmp, x2.length, msg.length);
+        System.arraycopy(y2, 0, tmp, x2.length + msg.length, y2.length);
+        String c3 = CipUtils.arrayToHex(Sm3Inner.sm3(tmp));
+
+        AtomicInteger ct = new AtomicInteger(1);
+        AtomicInteger offset = new AtomicInteger(0);
+        AtomicReference<byte[]> t = new AtomicReference<>(new byte[256 / 8]); // 256 位
+        // const z = [].concat(x2, y2);
+        byte[] z = new byte[x2.length + y2.length];
+        System.arraycopy(x2, 0, z, 0, x2.length);
+        System.arraycopy(y2, 0, z, x2.length, y2.length);
+        Runnable nextT = () -> {
+            // (1) Hai = hash(z || ct)
+            // (2) ct++
+            // [...z, ct >> 24 & 0x00ff, ct >> 16 & 0x00ff, ct >> 8 & 0x00ff, ct & 0x00ff]
+            byte[] buff = new byte[z.length + 4];
+            System.arraycopy(z, 0, buff, 0, z.length);
+            buff[z.length + 0] = (byte) ((ct.get() >>> 24) & 0x0ff);
+            buff[z.length + 1] = (byte) ((ct.get() >>> 16) & 0x0ff);
+            buff[z.length + 2] = (byte) ((ct.get() >>> 8) & 0x0ff);
+            buff[z.length + 3] = (byte) ((ct.get()) & 0x0ff);
+            t.set(Sm3Inner.sm3(buff));
+            ct.incrementAndGet();
+            offset.set(0);
+        };
+        nextT.run(); // 先生成 Ha1
+
+        for (int i = 0, len = msg.length; i < len; i++) {
+            // t = Ha1 || Ha2 || Ha3 || Ha4
+            if (offset.get() == t.get().length){
+                nextT.run();
+            }
+
+            // c2 = msg ^ t
+            msg[i] ^= t.get()[offset.getAndIncrement()] & 0x0ff;
+        }
+        String c2 = CipUtils.arrayToHex(msg);
+
+        return cipherMode == CipherMode.C1C2C3 ? c1 + c2 + c3 : c1 + c3 + c2;
     }
 
     /**
@@ -76,9 +142,9 @@ public class Sm2Cipher {
             // [...z, ct >> 24 & 0x00ff, ct >> 16 & 0x00ff, ct >> 8 & 0x00ff, ct & 0x00ff]
             byte[] buff = new byte[z.length + 4];
             System.arraycopy(z, 0, buff, 0, z.length);
-            buff[z.length + 0] = (byte) ((ct.get() >> 24) & 0x0ff);
-            buff[z.length + 1] = (byte) ((ct.get() >> 16) & 0x0ff);
-            buff[z.length + 2] = (byte) ((ct.get() >> 8) & 0x0ff);
+            buff[z.length + 0] = (byte) ((ct.get() >>> 24) & 0x0ff);
+            buff[z.length + 1] = (byte) ((ct.get() >>> 16) & 0x0ff);
+            buff[z.length + 2] = (byte) ((ct.get() >>> 8) & 0x0ff);
             buff[z.length + 3] = (byte) ((ct.get()) & 0x0ff);
             t.set(Sm3Inner.sm3(buff));
             ct.incrementAndGet();
@@ -111,10 +177,78 @@ public class Sm2Cipher {
         return ret;
     }
 
-    public static String doDecrypt(String encryptData, String privateKey, CipherMode cipherMode) {
-        byte[] encArr = Utils.hexToArray(encryptData);
-        byte[] retArr = doDecrypt(encArr, privateKey, cipherMode);
-        return Utils.arrayToUtf8(retArr);
+//    public static String doDecrypt(String encryptData, String privateKey, CipherMode cipherMode) {
+//        byte[] encArr = Utils.hexToArray(encryptData);
+//        byte[] retArr = doDecrypt(encArr, privateKey, cipherMode);
+//        return Utils.arrayToUtf8(retArr);
+//    }
+
+    public static String doDecrypt(String encryptData, String privateKeyHex, CipherMode cipherMode) {
+        if (cipherMode == null) {
+            cipherMode = CipherMode.C1C3C2;
+        }
+        BigInteger privateKey = new BigInteger(privateKeyHex, 16);
+
+        String c3 = encryptData.substring(128, 128+64);
+        String c2 = encryptData.substring(128 + 64);
+
+        if (cipherMode == CipherMode.C1C2C3) {
+            c3 = encryptData.substring(encryptData.length() - 64);
+            c2 = encryptData.substring(128, 128+(encryptData.length() - 128 - 64));
+        }
+
+    byte[] msg = Utils.hexToArray(c2);
+    EcPointFp c1 = Utils.getGlobalCurve().decodePointHex("04" + encryptData.substring(0, 128));
+
+    EcPointFp p = c1.multiply(privateKey);
+    byte[] x2 = Utils.hexToArray(CipUtils.leftPad(p.getX().toBigInteger().toString(16), 64));
+    byte[] y2 = Utils.hexToArray(CipUtils.leftPad(p.getY().toBigInteger().toString(16), 64));
+
+
+        AtomicInteger ct = new AtomicInteger(1);
+        AtomicInteger offset = new AtomicInteger(0);
+        AtomicReference<byte[]> t = new AtomicReference<>(new byte[256 / 8]); // 256 位
+        // const z = [].concat(x2, y2);
+        byte[] z = new byte[x2.length + y2.length];
+        System.arraycopy(x2, 0, z, 0, x2.length);
+        System.arraycopy(y2, 0, z, x2.length, y2.length);
+        Runnable nextT = () -> {
+            // (1) Hai = hash(z || ct)
+            // (2) ct++
+            // [...z, ct >> 24 & 0x00ff, ct >> 16 & 0x00ff, ct >> 8 & 0x00ff, ct & 0x00ff]
+            byte[] buff = new byte[z.length + 4];
+            System.arraycopy(z, 0, buff, 0, z.length);
+            buff[z.length + 0] = (byte) ((ct.get() >>> 24) & 0x0ff);
+            buff[z.length + 1] = (byte) ((ct.get() >>> 16) & 0x0ff);
+            buff[z.length + 2] = (byte) ((ct.get() >>> 8) & 0x0ff);
+            buff[z.length + 3] = (byte) ((ct.get()) & 0x0ff);
+            t.set(Sm3Inner.sm3(buff));
+            ct.incrementAndGet();
+            offset.set(0);
+        };
+        nextT.run(); // 先生成 Ha1
+
+        for (int i = 0, len = msg.length; i < len; i++) {
+            // t = Ha1 || Ha2 || Ha3 || Ha4
+            if (offset.get() == t.get().length) {
+                nextT.run();
+            }
+
+            // c2 = msg ^ t
+            msg[i] ^= t.get()[offset.getAndIncrement()] & 0x0ff;
+        }
+
+        // c3 = hash(x2 || msg || y2)
+        byte[] tmp = new byte[x2.length + msg.length + y2.length];
+        System.arraycopy(x2, 0, tmp, 0, x2.length);
+        System.arraycopy(msg, 0, tmp, x2.length, msg.length);
+        System.arraycopy(y2, 0, tmp, x2.length + msg.length, y2.length);
+        String checkC3 = CipUtils.arrayToHex(Sm3Inner.sm3(tmp));
+
+        if (checkC3.equalsIgnoreCase(c3)) {
+            return  Utils.arrayToUtf8(msg);
+        }
+        throw new SmException("decrypt error");
     }
 
     /**
@@ -156,10 +290,11 @@ public class Sm2Cipher {
             // [...z, ct >> 24 & 0x00ff, ct >> 16 & 0x00ff, ct >> 8 & 0x00ff, ct & 0x00ff]
             byte[] buff = new byte[z.length + 4];
             System.arraycopy(z, 0, buff, 0, z.length);
-            buff[z.length + 0] = (byte) ((ct.get() >> 24) & 0x0ff);
-            buff[z.length + 1] = (byte) ((ct.get() >> 16) & 0x0ff);
-            buff[z.length + 2] = (byte) ((ct.get() >> 8) & 0x0ff);
+            buff[z.length + 0] = (byte) ((ct.get() >>> 24) & 0x0ff);
+            buff[z.length + 1] = (byte) ((ct.get() >>> 16) & 0x0ff);
+            buff[z.length + 2] = (byte) ((ct.get() >>> 8) & 0x0ff);
             buff[z.length + 3] = (byte) ((ct.get()) & 0x0ff);
+
             t.set(Sm3Inner.sm3(buff));
             ct.incrementAndGet();
             offset.set(0);
--- i2f-jdk/i2f-sm-crypto/src/test/java/i2f/sm/crypto/test/TestCmpSm.java
+++ i2f-jdk/i2f-sm-crypto/src/test/java/i2f/sm/crypto/test/TestCmpSm.java
@@ -16,14 +16,16 @@ import java.util.Arrays;
 public class TestCmpSm {
     public static void main(String[] args) throws Exception {
 
-        testSm3();
+        boolean perf = false;
 
-        testSm4();
+        testSm3(perf);
 
-        testSm2();
+        testSm4(perf);
+
+        testSm2(perf);
     }
 
-    public static void testSm3() throws Exception {
+    public static void testSm3(boolean perf) throws Exception {
         System.out.println("\n\nTesting Sm3 ==============");
         String text = "Hello你好 World世界!";
         System.out.println("text: " + text);
@@ -43,26 +45,28 @@ public class TestCmpSm {
 
         assert s1.equals(cmpS1);
 
-        long bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            String bs = Sm3.sm3(text);
-        }
-        long ets = System.currentTimeMillis();
-        long uts = ets - bts;
-        System.out.println("use time: " + uts + "ms");
+        if (perf) {
+            long bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                String bs = Sm3.sm3(text);
+            }
+            long ets = System.currentTimeMillis();
+            long uts = ets - bts;
+            System.out.println("use time: " + uts + "ms");
 
-        bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            String bs = com.antherd.smcrypto.sm3.Sm3.sm3(text);
-        }
-        ets = System.currentTimeMillis();
-        long cmpUts = ets - bts;
-        System.out.println("use time: " + cmpUts + "ms");
+            bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                String bs = com.antherd.smcrypto.sm3.Sm3.sm3(text);
+            }
+            ets = System.currentTimeMillis();
+            long cmpUts = ets - bts;
+            System.out.println("use time: " + cmpUts + "ms");
 
-        System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
+            System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
+        }
     }
 
-    public static void testSm4() throws Exception {
+    public static void testSm4(boolean perf) throws Exception {
         System.out.println("\n\nTesting Sm4 ==============");
         String key = Sm4.generateHexKey();
         String text = "Hello你好 World世界!";
@@ -81,6 +85,28 @@ public class TestCmpSm {
         System.out.println("cmpDec: " + cmpDec);
         assert text.equals(cmpDec);
 
+        if (perf) {
+            long bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                String e = Sm4.encrypt(text, key);
+                String d = Sm4.decrypt(e, key);
+            }
+            long ets = System.currentTimeMillis();
+            long uts = ets - bts;
+            System.out.println("use time: " + uts + "ms");
+
+            bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                String e = com.antherd.smcrypto.sm4.Sm4.encrypt(text, key);
+                String d = com.antherd.smcrypto.sm4.Sm4.decrypt(e, key);
+            }
+            ets = System.currentTimeMillis();
+            long cmpUts = ets - bts;
+            System.out.println("use time: " + cmpUts + "ms");
+
+            System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
+        }
+
         String cmp2Dec = com.antherd.smcrypto.sm4.Sm4.decrypt(enc, key);
         System.out.println("cmp2Dec: " + cmp2Dec);
         assert text.equals(cmp2Dec);
@@ -90,51 +116,33 @@ public class TestCmpSm {
         assert text.equals(cmp3Dec);
 
 
-        long bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            String e = Sm4.encrypt(text, key);
-            String d = Sm4.decrypt(e, key);
-        }
-        long ets = System.currentTimeMillis();
-        long uts = ets - bts;
-        System.out.println("use time: " + uts + "ms");
-
-        bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            String e = com.antherd.smcrypto.sm4.Sm4.encrypt(text, key);
-            String d = com.antherd.smcrypto.sm4.Sm4.decrypt(e, key);
-        }
-        ets = System.currentTimeMillis();
-        long cmpUts = ets - bts;
-        System.out.println("use time: " + cmpUts + "ms");
-
-        System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
     }
 
-    public static void testSm2() throws Exception {
+    public static void testSm2(boolean perf) throws Exception {
         System.out.println("\n\nTesting Sm2 ==============");
         KeyPair key = Sm2.generateKeyPairHex();
         String text = "Hello你好 World世界!";
         System.out.println("text: " + text);
 
+        if (perf) {
+            long bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                KeyPair k = Sm2.generateKeyPairHex();
+            }
+            long ets = System.currentTimeMillis();
+            long uts = ets - bts;
+            System.out.println("use time: " + uts + "ms");
 
-        long bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            KeyPair k = Sm2.generateKeyPairHex();
+            bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                Keypair k = com.antherd.smcrypto.sm2.Sm2.generateKeyPairHex();
+            }
+            ets = System.currentTimeMillis();
+            long cmpUts = ets - bts;
+            System.out.println("use time: " + cmpUts + "ms");
+
+            System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
         }
-        long ets = System.currentTimeMillis();
-        long uts = ets - bts;
-        System.out.println("use time: " + uts + "ms");
-
-        bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            Keypair k = com.antherd.smcrypto.sm2.Sm2.generateKeyPairHex();
-        }
-        ets = System.currentTimeMillis();
-        long cmpUts = ets - bts;
-        System.out.println("use time: " + cmpUts + "ms");
-
-        System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
 
         System.out.println("========sign/verify");
         String sign = Sm2.doSignature(text, key.getPrivateKey());
@@ -148,32 +156,33 @@ public class TestCmpSm {
         boolean cmpOk = com.antherd.smcrypto.sm2.Sm2.doVerifySignature(text, cmpSign, key.getPublicKey());
         System.out.println(cmpOk);
 
+        if (perf) {
+            long bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                String s = Sm2.doSignature(text, key.getPrivateKey());
+                boolean o = Sm2.doVerifySignature(text, sign, key.getPublicKey());
+            }
+            long ets = System.currentTimeMillis();
+            long uts = ets - bts;
+            System.out.println("use time: " + uts + "ms");
+
+            bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                String s = com.antherd.smcrypto.sm2.Sm2.doSignature(text, key.getPrivateKey());
+                boolean o = com.antherd.smcrypto.sm2.Sm2.doVerifySignature(text, s, key.getPublicKey());
+            }
+            ets = System.currentTimeMillis();
+            long cmpUts = ets - bts;
+            System.out.println("use time: " + cmpUts + "ms");
+
+            System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
+        }
+
         boolean cmp2Ok = Sm2.doVerifySignature(text, cmpSign, key.getPublicKey());
         System.out.println("cmp2Ok: " + cmp2Ok);
 
         boolean cmp3Ok = com.antherd.smcrypto.sm2.Sm2.doVerifySignature(text, sign, key.getPublicKey());
         System.out.println("cmp3Ok: " + cmp3Ok);
-
-
-        bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            String s = Sm2.doSignature(text, key.getPrivateKey());
-            boolean o = Sm2.doVerifySignature(text, sign, key.getPublicKey());
-        }
-        ets = System.currentTimeMillis();
-        uts = ets - bts;
-        System.out.println("use time: " + uts + "ms");
-
-        bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            String s = com.antherd.smcrypto.sm2.Sm2.doSignature(text, key.getPrivateKey());
-            boolean o = com.antherd.smcrypto.sm2.Sm2.doVerifySignature(text, s, key.getPublicKey());
-        }
-        ets = System.currentTimeMillis();
-        cmpUts = ets - bts;
-        System.out.println("use time: " + cmpUts + "ms");
-
-        System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
 
 
         System.out.println("\n\n========encrypt/decrypt");
@@ -193,35 +202,38 @@ public class TestCmpSm {
         assert text.equals(cmpDec);
         assert Arrays.equals(text.getBytes("UTF-8"), cmpDec.getBytes("UTF-8"));
 
-        String cmp2Dec = Sm2.doDecrypt(cmpEnc, key.getPrivateKey());
-        System.out.println("cmp2Dec: " + cmp2Dec);
-        assert text.equals(cmp2Dec);
-        assert Arrays.equals(text.getBytes("UTF-8"), cmp2Dec.getBytes("UTF-8"));
+        if (perf) {
+            long bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                String e = Sm2.doEncrypt(text, key.getPublicKey());
+                String d = Sm2.doDecrypt(e, key.getPrivateKey());
+            }
+            long ets = System.currentTimeMillis();
+            long uts = ets - bts;
+            System.out.println("use time: " + uts + "ms");
+
+            bts = System.currentTimeMillis();
+            for (int i = 0; i < 1000; i++) {
+                String e = com.antherd.smcrypto.sm2.Sm2.doEncrypt(text, key.getPublicKey());
+                String d = com.antherd.smcrypto.sm2.Sm2.doDecrypt(e, key.getPrivateKey());
+            }
+            ets = System.currentTimeMillis();
+            long cmpUts = ets - bts;
+            System.out.println("use time: " + cmpUts + "ms");
+
+            System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
+        }
 
         String cmp3Dec = com.antherd.smcrypto.sm2.Sm2.doDecrypt(enc, key.getPrivateKey());
         System.out.println("cmp3Dec: " + cmp3Dec);
         assert text.equals(cmp3Dec);
         assert Arrays.equals(text.getBytes("UTF-8"), cmp3Dec.getBytes("UTF-8"));
 
+        String cmp2Dec = Sm2.doDecrypt(cmpEnc, key.getPrivateKey());
+        System.out.println("cmp2Dec: " + cmp2Dec);
+        assert text.equals(cmp2Dec);
+        assert Arrays.equals(text.getBytes("UTF-8"), cmp2Dec.getBytes("UTF-8"));
 
-        bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            String e = Sm2.doEncrypt(text, key.getPublicKey());
-            String d = Sm2.doDecrypt(e, key.getPrivateKey());
-        }
-        ets = System.currentTimeMillis();
-        uts = ets - bts;
-        System.out.println("use time: " + uts + "ms");
 
-        bts = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            String e = com.antherd.smcrypto.sm2.Sm2.doEncrypt(text, key.getPublicKey());
-            String d = com.antherd.smcrypto.sm2.Sm2.doDecrypt(e, key.getPrivateKey());
-        }
-        ets = System.currentTimeMillis();
-        cmpUts = ets - bts;
-        System.out.println("use time: " + cmpUts + "ms");
-
-        System.out.println("speed rate:" + (cmpUts * 1.0 / uts));
     }
 }
