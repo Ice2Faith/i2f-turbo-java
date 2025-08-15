3596c20201fd3be5afeaf5b38dfd13cf43b0c053
Ice2Faith <Ugex_Savelar@163.com>
2025-08-15 10:48:15 +0800

    fix(sm-crypto): sm2

---
+++ i2f-jdk/i2f-sm-crypto/src/test/java/i2f/sm/crypto/test/TestUnit.java
@@ -0,0 +1,16 @@
+package i2f.sm.crypto.test;
+
+import i2f.codec.bytes.raw.HexStringByteCodec;
+import i2f.sm.crypto.sm2.Utils;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/8/15 10:17
+ */
+public class TestUnit {
+    public static void main(String[] args){
+        byte[] arr = Utils.hexToArray(Utils.utf8ToHex("hello你好"));
+        System.out.println(HexStringByteCodec.INSTANCE.encode(arr));
+
+    }
+}
