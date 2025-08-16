1ece7465d24c8b12b34446c334c5bf8dcbec7383
Ice2Faith <Ugex_Savelar@163.com>
2025-08-16 17:32:10 +0800

    fix(sm-crypto): sm2

--- i2f-jdk/i2f-sm-crypto/src/main/java/i2f/sm/crypto/sm2/Sm2Cipher.java
+++ i2f-jdk/i2f-sm-crypto/src/main/java/i2f/sm/crypto/sm2/Sm2Cipher.java
@@ -26,21 +26,21 @@ public class Sm2Cipher {
 
     public static final EcParam EC_PARAM = Utils.generateEcparam();
 
-//    public static String doEncrypt(String msg, String publicKey, CipherMode cipherMode) {
-//        byte[] msgArr = Utils.hexToArray(Utils.utf8ToHex(msg));
-//        byte[] retArr = doEncrypt(msgArr, publicKey, cipherMode);
-//        return CipUtils.arrayToHex(retArr);
-//    }
+    public static String doEncrypt(String msg, String publicKey, CipherMode cipherMode) {
+        byte[] msgArr = Utils.hexToArray(Utils.utf8ToHex(msg));
+        byte[] retArr = doEncrypt(msgArr, publicKey, cipherMode);
+        return CipUtils.arrayToHex(retArr);
+    }
 
-    public static String doEncrypt(String msgStr, String publicKeyHex, CipherMode cipherMode) {
+    public static String doEncrypt2(String msgStr, String publicKeyHex, CipherMode cipherMode) {
         if (cipherMode == null) {
             cipherMode = CipherMode.C1C3C2;
         }
         byte[] msg = Utils.hexToArray(Utils.utf8ToHex(msgStr));
         EcPointFp publicKey = Utils.getGlobalCurve().decodePointHex(publicKeyHex); // 先将公钥转成点
 
-    KeyPair keypair = Utils.generateKeyPairHex();
-    BigInteger k = new BigInteger(keypair.privateKey, 16); // 随机数 k
+        KeyPair keypair = Utils.generateKeyPairHex();
+        BigInteger k = new BigInteger(keypair.privateKey, 16); // 随机数 k
 
         // c1 = k * G
         String c1 = keypair.publicKey;
@@ -49,12 +49,12 @@ public class Sm2Cipher {
         }
 
         // (x2, y2) = k * publicKey
-    EcPointFp p = publicKey.multiply(k);
-    byte[] x2 = Utils.hexToArray(CipUtils.leftPad(p.getX().toBigInteger().toString(16), 64));
-    byte[] y2 = Utils.hexToArray(CipUtils.leftPad(p.getY().toBigInteger().toString(16), 64));
+        EcPointFp p = publicKey.multiply(k);
+        byte[] x2 = Utils.hexToArray(CipUtils.leftPad(p.getX().toBigInteger().toString(16), 64));
+        byte[] y2 = Utils.hexToArray(CipUtils.leftPad(p.getY().toBigInteger().toString(16), 64));
 
         // c3 = hash(x2 || msg || y2)
-    // const c3 = _.arrayToHex(sm3([].concat(x2, msg, y2)))
+        // const c3 = _.arrayToHex(sm3([].concat(x2, msg, y2)))
         byte[] tmp = new byte[x2.length + msg.length + y2.length];
         System.arraycopy(x2, 0, tmp, 0, x2.length);
         System.arraycopy(msg, 0, tmp, x2.length, msg.length);
@@ -86,7 +86,7 @@ public class Sm2Cipher {
 
         for (int i = 0, len = msg.length; i < len; i++) {
             // t = Ha1 || Ha2 || Ha3 || Ha4
-            if (offset.get() == t.get().length){
+            if (offset.get() == t.get().length) {
                 nextT.run();
             }
 
@@ -177,32 +177,32 @@ public class Sm2Cipher {
         return ret;
     }
 
-//    public static String doDecrypt(String encryptData, String privateKey, CipherMode cipherMode) {
-//        byte[] encArr = Utils.hexToArray(encryptData);
-//        byte[] retArr = doDecrypt(encArr, privateKey, cipherMode);
-//        return Utils.arrayToUtf8(retArr);
-//    }
+    public static String doDecrypt(String encryptData, String privateKey, CipherMode cipherMode) {
+        byte[] encArr = Utils.hexToArray(encryptData);
+        byte[] retArr = doDecrypt(encArr, privateKey, cipherMode);
+        return Utils.arrayToUtf8(retArr);
+    }
 
-    public static String doDecrypt(String encryptData, String privateKeyHex, CipherMode cipherMode) {
+    public static String doDecrypt2(String encryptData, String privateKeyHex, CipherMode cipherMode) {
         if (cipherMode == null) {
             cipherMode = CipherMode.C1C3C2;
         }
         BigInteger privateKey = new BigInteger(privateKeyHex, 16);
 
-        String c3 = encryptData.substring(128, 128+64);
+        String c3 = encryptData.substring(128, 128 + 64);
         String c2 = encryptData.substring(128 + 64);
 
         if (cipherMode == CipherMode.C1C2C3) {
             c3 = encryptData.substring(encryptData.length() - 64);
-            c2 = encryptData.substring(128, 128+(encryptData.length() - 128 - 64));
+            c2 = encryptData.substring(128, 128 + (encryptData.length() - 128 - 64));
         }
 
-    byte[] msg = Utils.hexToArray(c2);
-    EcPointFp c1 = Utils.getGlobalCurve().decodePointHex("04" + encryptData.substring(0, 128));
+        byte[] msg = Utils.hexToArray(c2);
+        EcPointFp c1 = Utils.getGlobalCurve().decodePointHex("04" + encryptData.substring(0, 128));
 
-    EcPointFp p = c1.multiply(privateKey);
-    byte[] x2 = Utils.hexToArray(CipUtils.leftPad(p.getX().toBigInteger().toString(16), 64));
-    byte[] y2 = Utils.hexToArray(CipUtils.leftPad(p.getY().toBigInteger().toString(16), 64));
+        EcPointFp p = c1.multiply(privateKey);
+        byte[] x2 = Utils.hexToArray(CipUtils.leftPad(p.getX().toBigInteger().toString(16), 64));
+        byte[] y2 = Utils.hexToArray(CipUtils.leftPad(p.getY().toBigInteger().toString(16), 64));
 
 
         AtomicInteger ct = new AtomicInteger(1);
@@ -246,7 +246,7 @@ public class Sm2Cipher {
         String checkC3 = CipUtils.arrayToHex(Sm3Inner.sm3(tmp));
 
         if (checkC3.equalsIgnoreCase(c3)) {
-            return  Utils.arrayToUtf8(msg);
+            return Utils.arrayToUtf8(msg);
         }
         throw new SmException("decrypt error");
     }
@@ -324,10 +324,12 @@ public class Sm2Cipher {
 
         return msg;
     }
+
     public static String doSignature(String msg, String privateKey
-    ){
-        return doSignature(msg,privateKey,null,false,false,null,null);
+    ) {
+        return doSignature(msg, privateKey, null, false, false, null, null);
     }
+
     public static String doSignature(String msg, String privateKey,
                                      List<Point> pointPool, boolean der, boolean hash, String publicKey, String userId
     ) {
@@ -460,6 +462,7 @@ public class Sm2Cipher {
         byte[] retArr = getHash(hashBytes, publicKey, userId);
         return CipUtils.arrayToHex(retArr);
     }
+
     /**
      * sm3杂凑算法
      */
