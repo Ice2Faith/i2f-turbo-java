dd484d405b14053424d5a97d8092dec34d2cd06e
Ice2Faith <Ugex_Savelar@163.com>
2025-08-16 12:57:15 +0800

    fix(sm-crypto): web

---
+++ i2f-jdk/i2f-sm-crypto/src/main/web/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import {sm2,sm3,sm4}  from '../sm-crypto/index'
+export default {
+  data:()=>{
+    return {
+      msg: sm2.generateKeyPairHex()
+    }
+  },
+  created(){
+    console.log({sm2,sm3,sm4})
+  },
+  mounted(){
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
