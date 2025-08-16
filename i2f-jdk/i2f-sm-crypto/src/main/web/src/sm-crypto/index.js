d31f8d0f61da5565ec21b1292801c0b76f35b9e8
Ice2Faith <Ugex_Savelar@163.com>
2025-08-16 12:57:15 +0800

    fix(sm-crypto): web

---
+++ i2f-jdk/i2f-sm-crypto/src/main/web/src/sm-crypto/index.js
@@ -0,0 +1,13 @@
+import * as _sm2 from './sm2/index'
+import * as _sm3 from './sm3/index'
+import * as _sm4 from './sm4/index'
+
+const _sm={
+    sm2: {..._sm2},
+    sm3: _sm3.default,
+    sm4: {..._sm4},
+}
+
+export const sm2=_sm.sm2
+export const sm3 =_sm.sm3
+export const sm4=_sm.sm4
