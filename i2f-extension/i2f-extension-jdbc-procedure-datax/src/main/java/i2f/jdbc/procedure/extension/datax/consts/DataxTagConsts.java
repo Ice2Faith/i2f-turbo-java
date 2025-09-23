b2fc399d89918b103bf45ce1c956983e28f30f18
Ice2Faith <Ugex_Savelar@163.com>
2025-09-23 16:37:35 +0800

    feat(xproc4j): xproc4j support datax by i2f-extension-jdbc-procedure-datax

---
+++ i2f-extension/i2f-extension-jdbc-procedure-datax/src/main/java/i2f/jdbc/procedure/extension/datax/consts/DataxTagConsts.java
@@ -0,0 +1,9 @@
+package i2f.jdbc.procedure.extension.datax.consts;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/9/23 16:11
+ */
+public interface DataxTagConsts {
+    String DATAX_EXEC="datax-exec";
+}
