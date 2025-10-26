5cffd54fc6a669563885bcd1800ca074db52ad2e
Ice2Faith <Ugex_Savelar@163.com>
2025-10-26 16:40:58 +0800

    feat(TinyScript): TinyScript plugin update

---
+++ i2f-tools/i2f-jdbc-procedure-idea-plugin/src/main/java/i2f/turbo/idea/plugin/tinyscript/grammar/psi/elements/TinyScriptDeclareFunction.java
@@ -0,0 +1,19 @@
+// This is a generated file. Not intended for manual editing.
+package i2f.turbo.idea.plugin.tinyscript.grammar.psi.elements;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
+public interface TinyScriptDeclareFunction extends PsiElement {
+
+  @Nullable
+  TinyScriptParameterList getParameterList();
+
+  @NotNull
+  TinyScriptScriptBlock getScriptBlock();
+
+  @NotNull
+  PsiElement getNaming();
+
+}
