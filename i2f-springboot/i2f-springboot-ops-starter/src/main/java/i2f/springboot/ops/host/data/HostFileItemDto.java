fbc6702cfe4e7055b66eb1f7bc71ee2102cf13f7
Ice2Faith <Ugex_Savelar@163.com>
2025-11-04 23:48:58 +0800

    feat(ops): ops admin for springboot

---
+++ i2f-springboot/i2f-springboot-ops-starter/src/main/java/i2f/springboot/ops/host/data/HostFileItemDto.java
@@ -0,0 +1,18 @@
+package i2f.springboot.ops.host.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/11/4 23:31
+ * @desc
+ */
+@Data
+@NoArgsConstructor
+public class HostFileItemDto {
+    protected String path;
+    protected String name;
+    protected String type; // file,dir
+    protected long size;
+}
