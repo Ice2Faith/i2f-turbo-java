c86ceeba6afe1ea894a59a0561dab4d801dc16ad
Ice2Faith <Ugex_Savelar@163.com>
2025-11-08 17:51:35 +0800

    feat(ops): ops support xxl-job run

---
+++ i2f-springboot/i2f-springboot-ops-starter/src/main/java/i2f/springboot/ops/xxljob/data/XxlJobOperateDto.java
@@ -0,0 +1,19 @@
+package i2f.springboot.ops.xxljob.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/11/4 21:53
+ * @desc
+ */
+@Data
+@NoArgsConstructor
+public class XxlJobOperateDto {
+    protected String method;
+    protected String param;
+
+    protected Boolean async;
+    protected Long waitForSeconds;
+}
