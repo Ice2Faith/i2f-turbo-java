8f6294e889529f554046657717b43d4d0c70500e
Ice2Faith <Ugex_Savelar@163.com>
2025-11-13 20:31:33 +0800

    feat(ops): ops host update

---
+++ i2f-springboot/i2f-springboot-ops-starter/src/main/java/i2f/springboot/ops/host/NetworkUtil.java
@@ -0,0 +1,79 @@
+package i2f.springboot.ops.host;
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.*;
+
+/**
+ * @author Ice2Faith
+ * @date 2025/11/13 19:30
+ * @desc
+ */
+public class NetworkUtil {
+    public static void main(String[] args){
+        List<Map.Entry<InetAddress, NetworkInterface>> list = getUsefulAddresses();
+        for (Map.Entry<InetAddress, NetworkInterface> entry : list) {
+            System.out.println("===========");
+            System.out.println(entry.getKey().getHostAddress());
+            System.out.println(entry.getValue().getName());
+        }
+    }
+    public static List<Map .Entry<InetAddress,NetworkInterface>> getUsefulAddresses(){
+        List<Map.Entry<InetAddress,NetworkInterface>> ret=new ArrayList<>();
+        try {
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()){
+                NetworkInterface networkInterface = networkInterfaces.nextElement();
+                try {
+                    if(!networkInterface.isUp()){
+                        continue;
+                    }
+                    if(networkInterface.isLoopback()){
+                        continue;
+                    }
+                    Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
+                    while(inetAddresses.hasMoreElements()){
+                        InetAddress inetAddress = inetAddresses.nextElement();
+                        try {
+                            if(inetAddress.isMulticastAddress()){
+                                continue;
+                            }
+                            if(inetAddress.isLoopbackAddress()){
+                                continue;
+                            }
+                            ret.add(new AbstractMap.SimpleEntry<>(inetAddress,networkInterface));
+                        } catch (Exception e) {
+
+                        }
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        ret.sort((v1,v2)->{
+            NetworkInterface n1 = v1.getValue();
+            NetworkInterface n2 = v2.getValue();
+            if(n1.isVirtual() == n2.isVirtual()){
+                InetAddress k1 = v1.getKey();
+                InetAddress k2 = v2.getKey();
+                if(k1 instanceof Inet4Address && k2 instanceof Inet4Address){
+                    return 0;
+                }else if(k1 instanceof Inet4Address){
+                    return -1;
+                }else{
+                    return 1;
+                }
+            }else if(n1.isVirtual()){
+                return 1;
+            }else {
+                return -1;
+            }
+        });
+        return ret;
+    }
+}
