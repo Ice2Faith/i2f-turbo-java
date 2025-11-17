3d74a919c4c7caeebfb3d1f4c008aeee4f4263ba
Ice2Faith <Ugex_Savelar@163.com>
2025-11-17 16:35:25 +0800

    feat(ops): ops app update

--- i2f-springboot/i2f-springboot-ops-starter/src/main/java/i2f/springboot/ops/util/NetworkUtil.java
+++ i2f-springboot/i2f-springboot-ops-starter/src/main/java/i2f/springboot/ops/util/NetworkUtil.java
@@ -49,7 +49,9 @@ public class NetworkUtil {
         ret.sort((v1,v2)->{
             NetworkInterface n1 = v1.getValue();
             NetworkInterface n2 = v2.getValue();
-            if(n1.isVirtual() == n2.isVirtual()){
+            boolean n1Virtual=isVirtualOrSoftwareInterface(n1);
+            boolean n2Virtual=isVirtualOrSoftwareInterface(n2);
+            if(n1Virtual == n2Virtual){
                 InetAddress k1 = v1.getKey();
                 InetAddress k2 = v2.getKey();
                 if(k1 instanceof Inet4Address && k2 instanceof Inet4Address){
@@ -59,12 +61,45 @@ public class NetworkUtil {
                 }else{
                     return 1;
                 }
-            }else if(n1.isVirtual()){
+            }else if(n1Virtual){
                 return 1;
             }else {
                 return -1;
             }
         });
         return ret;
+    }
+
+    public static boolean isVirtualOrSoftwareInterface(NetworkInterface ni) {
+        try {
+            if(ni.isVirtual()){
+                return true;
+            }
+
+            String name = ni.getName().toLowerCase();
+            if( name.contains("virtual") ||
+                    name.contains("docker") ||
+                    name.contains("loop") ||
+                    name.contains("dummy")||
+                    name.contains("vmware") ||
+                    name.contains("vmnet") ||
+                    name.contains("veth")){
+                return true;
+            }
+
+            String displayName = ni.getDisplayName().toLowerCase();
+            if( displayName.contains("virtual") ||
+                    displayName.contains("docker") ||
+                    displayName.contains("loop") ||
+                    displayName.contains("dummy")||
+                    displayName.contains("vmware") ||
+                    displayName.contains("vmnet")||
+                    displayName.contains("veth")){
+                return true;
+            }
+        } catch (Exception e) {
+
+        }
+        return false;
     }
 }
