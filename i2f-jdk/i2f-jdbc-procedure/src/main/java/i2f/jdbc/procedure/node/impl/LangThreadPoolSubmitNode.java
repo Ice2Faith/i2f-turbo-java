818c0daeeaf3392381217817655e45a0c9e55fe9
Ice2Faith <Ugex_Savelar@163.com>
2025-11-24 11:51:04 +0800

    feat(xproc4j): fix package problem

--- i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/node/impl/LangThreadPoolSubmitNode.java
+++ i2f-jdk/i2f-jdbc-procedure/src/main/java/i2f/jdbc/procedure/node/impl/LangThreadPoolSubmitNode.java
@@ -6,8 +6,9 @@ import i2f.jdbc.procedure.consts.TagConsts;
 import i2f.jdbc.procedure.executor.JdbcProcedureExecutor;
 import i2f.jdbc.procedure.node.basic.AbstractExecutorNode;
 import i2f.jdbc.procedure.parser.data.XmlNode;
+import i2f.jdbc.procedure.signal.impl.ThrowSignalException;
 
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Consumer;
@@ -34,36 +35,41 @@ public class LangThreadPoolSubmitNode extends AbstractExecutorNode {
 
     @Override
     public void execInner(XmlNode node, Map<String, Object> context, JdbcProcedureExecutor executor) {
-        ExecutorService pool=(ExecutorService)executor.attrValue(AttrConsts.POOL, FeatureConsts.VISIT, node, context);
+        try {
+            ExecutorService pool = (ExecutorService) executor.attrValue(AttrConsts.POOL, FeatureConsts.VISIT, node, context);
 
-        Map<String,Object> callParams=executor.cloneParams(context);
+            Map<String, Object> callParams = executor.cloneParams(context);
 
-        for (Map.Entry<String, String> entry : node.getTagAttrMap().entrySet()) {
-            String name = entry.getKey();
-            if (AttrConsts.RESULT.equals(name)) {
-                continue;
+            for (Map.Entry<String, String> entry : node.getTagAttrMap().entrySet()) {
+                String name = entry.getKey();
+                if (AttrConsts.RESULT.equals(name)) {
+                    continue;
+                }
+                if (AttrConsts.POOL.equals(name)) {
+                    continue;
+                }
+                Object val = executor.attrValue(name, FeatureConsts.VISIT, node, context);
+                executor.visitSet(callParams, name, val);
             }
-            if (AttrConsts.POOL.equals(name)) {
-                continue;
-            }
-            Object val = executor.attrValue(name, FeatureConsts.VISIT, node, context);
-            executor.visitSet(callParams, name, val);
-        }
 
-        CountDownLatch latch = new CountDownLatch(1);
-        pool.submit(() -> {
-            try {
-                executor.execAsProcedure(node, callParams, true, true);
-            } catch (Throwable e) {
-                executor.logger().logWarn(() -> e.getMessage(), e);
-                e.printStackTrace();
-            } finally {
-                latch.countDown();
+            CountDownLatch latch = new CountDownLatch(1);
+            pool.submit(() -> {
+                try {
+                    executor.execAsProcedure(node, callParams, true, true);
+                } catch (Throwable e) {
+                    executor.logger().logWarn(() -> e.getMessage(), e);
+                    e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                }
+            });
+            String result = node.getTagAttrMap().get(AttrConsts.RESULT);
+            if (result != null) {
+                executor.visitSet(context, result, latch);
             }
-        });
-        String result = node.getTagAttrMap().get(AttrConsts.RESULT);
-        if (result != null) {
-            executor.visitSet(context, result, latch);
+        }catch (Throwable e){
+            e.printStackTrace();
+            throw new ThrowSignalException(e.getMessage(),e);
         }
     }
 
