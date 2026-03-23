edf522fd671cf2a3049b5fb45b7f82378b67a920
Ice2Faith <Ugex_Savelar@163.com>
2026-03-23 21:05:25 +0800

    test(ai-skill): test skill

--- i2f-extension/i2f-extension-ai-dashscope/src/main/java/i2f/extension/ai/dashscope/test/TestDashScopeAi.java
+++ i2f-extension/i2f-extension-ai-dashscope/src/main/java/i2f/extension/ai/dashscope/test/TestDashScopeAi.java
@@ -1,6 +1,9 @@
 package i2f.extension.ai.dashscope.test;
 
 import i2f.ai.std.ChatAi;
+import i2f.ai.std.skill.SkillDefinition;
+import i2f.ai.std.skill.SkillsHelper;
+import i2f.ai.std.skill.SkillsTools;
 import i2f.ai.std.tool.schema.JsonSchema;
 import i2f.ai.std.tool.test.TestSchemaPojo;
 import i2f.ai.std.tool.test.TestToolComponent;
@@ -17,7 +20,7 @@ import java.util.Map;
  * @desc
  */
 public class TestDashScopeAi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Map<String, Object> schema = JsonSchema.getTypeJsonSchema(TestSchemaPojo.class);
         System.out.println(schema);
 
@@ -28,10 +31,29 @@ public class TestDashScopeAi {
         provider.setApiKey(System.getenv("DASHSCOPE_AI_API_KEY"));
         ListableContext context = new ListableContext();
         context.addBean(new TestToolComponent());
+        context.addBean(new SkillsTools());
         provider.setContext(context);
 
+        Map<String, SkillDefinition> skillMap = SkillsHelper.scanFileSystemSkills();
+        if(!skillMap.isEmpty()){
+            StringBuilder builder=new StringBuilder();
+
+            builder.append("你拥有以下技能，如果有需要使用这些技能的时候，先阅读该技能的详细教程，" +
+                    "根据教程内容，决定是否还需要更多关于教程的资源需要调用。最终回答用户的问题。").append("\n");
+
+            builder.append("## 技能列表").append("\n");
+            for (Map.Entry<String, SkillDefinition> entry : skillMap.entrySet()) {
+                SkillDefinition definition = entry.getValue();
+                builder.append("### ").append(definition.getName()).append("\n")
+                        .append("- ").append(definition.getDescription()).append("\n")
+                        .append("\n");
+            }
+
+            provider.setSystem(builder.toString());
+        }
+
         ChatAi chatAi = provider.getChatAi();
-        String ret = chatAi.chat("北京的今天的天气怎么样，并且给出今天的日期");
+        String ret = chatAi.chat("北京的今天的天气怎么样，并且给出今天的日期，最后告诉我历史上的今天发生了什么");
         System.out.println(ret);
     }
 }
