d01a3ac5a5fedef6c19588f1a21cd7b03d0efb57
Ice2Faith <Ugex_Savelar@163.com>
2026-03-24 15:12:52 +0800

    feat(ai): ai-std support skills

--- i2f-jdk/i2f-ai-std/src/main/java/i2f/ai/std/skill/SkillsHelper.java
+++ i2f-jdk/i2f-ai-std/src/main/java/i2f/ai/std/skill/SkillsHelper.java
@@ -97,6 +97,36 @@ public class SkillsHelper {
         return ret;
     }
 
+    public static String convertSkillDefinitionsAsSystemPrompt(Map<String, SkillDefinition> skillMap) {
+        if (skillMap == null || skillMap.isEmpty()) {
+            return "";
+        }
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(
+                "## 技能(skill)设定\n" +
+                        "- 你拥有以下技能(skill)；\n" +
+                        "- 如果有需要使用这些技能的时候，使用下述的技能名称获取技能的详细教程(使用工具[getSkillDocument])；\n" +
+                        "- 每一个技能都没有其他资源文件和脚本文件可供使用，也就是这两个工具（[getSkillAsset]和[runSkillCommand]）不可使用；\n" +
+                        "- 除非这一个技能的详细教程中明确提及了，才会有相应的资源文件和脚本文件，才能够使用这两个工具；\n" +
+                        "- 如果这个技能的教程中有明确提及到了其他资源文件，才能获取提及到的资源文件阅读(使用工具[getSkillAsset])，***没有明确提及的资源不能使用工具获取***；\n" +
+                        "- 如果这个技能的教程中有明确提及需要运行的命令行脚本，才能在命令行运行提及到的脚本（使用工具[runSkillCommand]），***没有明确提及的脚本不能使用工具运行***；\n" +
+                        "- ***特别注意***，技能(skill)和工具(tool)不一样；\n" +
+                        "-***技能只能使用上面的三个工具进行调用***，***不允许获取教程中未明确提及的资源***，***不允许执行教程中未明确提及的脚本***；\n" +
+                        ""
+        ).append("\n");
+
+        builder.append("### 技能(skill)列表").append("\n");
+        for (Map.Entry<String, SkillDefinition> entry : skillMap.entrySet()) {
+            SkillDefinition definition = entry.getValue();
+            builder.append("#### ").append("技能名称：").append(definition.getName()).append("\n")
+                    .append("- ").append("技能描述：").append(definition.getDescription()).append("\n")
+                    .append("\n");
+        }
+
+        return builder.toString();
+    }
+
     public static URL getSkillResource(String path) throws Exception {
         URL url = ResourceUtil.getResource(path);
         if (url != null) {
@@ -125,7 +155,7 @@ public class SkillsHelper {
             if (item.isEmpty()) {
                 continue;
             }
-            if (!item.matches("^[a-zA-Z0-9\\-_]$")) {
+            if (!item.matches("^[a-zA-Z0-9\\-_\\.]+$")) {
                 throw new IllegalArgumentException("bad resourcePath accept!");
             }
             parts.add(item);
--- i2f-jdk/i2f-ai-std/src/main/java/i2f/ai/std/skill/SkillsTools.java
+++ i2f-jdk/i2f-ai-std/src/main/java/i2f/ai/std/skill/SkillsTools.java
@@ -20,33 +20,35 @@ import java.util.concurrent.TimeUnit;
 @Tools
 public class SkillsTools {
 
-    @Tool(description = "根据技能名称获取详细的 markdown 格式的技能教程")
+    @Tool(description = "获取技能(skill)的详细教程")
     public String getSkillDocument(@ToolParam(description = "技能名称，例如：search_website") String skillName) throws Exception {
-        if (skillName == null || !skillName.matches("^[a-zA-Z0-9\\-_]+$")) {
+        if (skillName == null || !skillName.matches("^[a-zA-Z0-9\\-_\\.]+$")) {
             throw new IllegalArgumentException("bad skillName accept");
         }
         URL url = null;
         for (String fileName : SkillsHelper.POSSIBLE_SKILL_FILE_NAME) {
             url = SkillsHelper.getSkillResource("skills/" + skillName + "/" + fileName);
             if (url != null) {
-                break;
+                if("file".equalsIgnoreCase(url.getProtocol())){
+                    File file = new File(url.toURI());
+                    if(file.exists() && file.isFile()){
+                        break;
+                    }
+                }
             }
         }
         if (url == null) {
             throw new IllegalStateException("skill [" + skillName + "] not found");
         }
+
         String text = StreamUtil.readString(url);
-        String[] arr = text.split("---", 3);
-        if (arr.length == 3) {
-            text = arr[2];
-        }
         return text;
     }
 
-    @Tool(description = "根据技能名称和资源路径获取技能中提及的关联资源")
+    @Tool(description = "获取技能(skill)中的其他资源文件")
     public String getSkillAsset(@ToolParam(description = "技能名称，例如：search_website") String skillName,
                                 @ToolParam(description = "资源路径，例如：script/test.py") String resourcePath) throws Exception {
-        if (skillName == null || !skillName.matches("^[a-zA-Z0-9\\-_]+$")) {
+        if (skillName == null || !skillName.matches("^[a-zA-Z0-9\\-_\\.]+$")) {
             throw new IllegalArgumentException("bad skillName accept");
         }
         resourcePath = SkillsHelper.safeSkillResourcePath(resourcePath);
@@ -55,14 +57,23 @@ public class SkillsTools {
         if (url == null) {
             throw new IllegalStateException("skill [" + skillName + "] asset [" + resourcePath + "] not found");
         }
+        if("file".equalsIgnoreCase(url.getProtocol())){
+            File file = new File(url.toURI());
+            if(!file.exists()){
+                throw new IllegalStateException("skill [" + skillName + "] asset file [" + resourcePath + "] not found");
+            }
+            if(!file.isFile()){
+                throw new IllegalStateException("skill [" + skillName + "] asset file [" + resourcePath + "] not is file");
+            }
+        }
         return StreamUtil.readString(url);
     }
 
-    @Tool(description = "根据技能名称和命令脚本资源路径在命令行调用这个脚本执行")
+    @Tool(description = "执行技能(skill)中的命令行脚本")
     public String runSkillCommand(@ToolParam(description = "技能名称，例如：search_website") String skillName,
-                                  @ToolParam(description = "资源路径，例如：script/test.py") String scriptPath,
-                                  @ToolParam(description = "命令行参数，例如：-o -l test.txt") List<String> commandArguments) throws Exception {
-        if (skillName == null || !skillName.matches("^[a-zA-Z0-9\\-_]+$")) {
+                                  @ToolParam(description = "脚本路径，例如：script/test.py") String scriptPath,
+                                  @ToolParam(description = "脚本的命令行参数，例如：-o -l test.txt") List<String> commandArguments) throws Exception {
+        if (skillName == null || !skillName.matches("^[a-zA-Z0-9\\-_\\.]+$")) {
             throw new IllegalArgumentException("bad skillName accept");
         }
         scriptPath = SkillsHelper.safeSkillResourcePath(scriptPath);
@@ -76,6 +87,12 @@ public class SkillsTools {
             throw new IllegalStateException("skill [" + skillName + "] script asset [" + scriptPath + "] not found");
         }
         File scriptFile = new File(url.toURI());
+        if(!scriptFile.exists()){
+            throw new IllegalStateException("skill [" + skillName + "] script file [" + scriptPath + "] not found");
+        }
+        if(!scriptFile.isFile()){
+            throw new IllegalStateException("skill [" + skillName + "] script file [" + scriptPath + "] not is file");
+        }
         String name = scriptFile.getName();
         String suffix = "";
         int idx = name.lastIndexOf(".");
