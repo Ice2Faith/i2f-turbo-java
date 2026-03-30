99a0068f8200b9fb43ec4dad31b53572decadb06
Ice2Faith <Ugex_Savelar@163.com>
2026-03-30 17:41:48 +0800

    feat(ai): support spring-ai starter

--- i2f-springboot/i2f-springboot-ai-starter/src/main/java/i2f/springboot/ai/json/JacksonAiJsonSerializer.java
+++ i2f-springboot/i2f-springboot-ai-starter/src/main/java/i2f/springboot/ai/json/JacksonAiJsonSerializer.java
@@ -3,9 +3,13 @@ package i2f.springboot.ai.json;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import i2f.serialize.std.str.json.IJsonSerializer;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
+@Data
+@NoArgsConstructor
 public class JacksonAiJsonSerializer implements IJsonSerializer {
     public static final JacksonAiJsonSerializer INSTANCE = new JacksonAiJsonSerializer();
     private ObjectMapper objectMapper = new ObjectMapper();
