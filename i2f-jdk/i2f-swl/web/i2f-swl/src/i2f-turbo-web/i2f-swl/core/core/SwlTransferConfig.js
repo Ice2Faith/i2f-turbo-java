8a753894fdcd3f02179b55d2a8289ea72f16ef57
Ice2Faith <Ugex_Savelar@163.com>
2026-04-07 22:06:28 +0800

    feat(swl): default swl swap key

--- i2f-jdk/i2f-swl/web/i2f-swl/src/i2f-turbo-web/i2f-swl/core/core/SwlTransferConfig.js
+++ i2f-jdk/i2f-swl/web/i2f-swl/src/i2f-turbo-web/i2f-swl/core/core/SwlTransferConfig.js
@@ -34,8 +34,24 @@ function SwlTransferConfig() {
  * 默认RSA交换公钥
  * @return {String}
  */
-SwlTransferConfig.DEFAULT_SWAP_PUBLIC_KEY=function(){
+SwlTransferConfig.DEFAULT_SWAP_RSA_PUBLIC_KEY = function () {
     return "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnhyhaYnyql1Vt0CFroJqDKNcQAvcHyigtjTijXwg5h4841Zlt8xl98HX7a8YZmUTIAnNeKF5tMX6OAYGl/GteUXuL8QFbj7nNSo9cDdT7ZEfK+rs+d/Pz7zxbuMI1UbUs+OFXIICLqOT+Ze1KJoBlm9r42qqvwMEYntKG8KI4bwIDAQAB";
+}
+
+/**
+ * 默认SM2交换公钥
+ * @return {String}
+ */
+SwlTransferConfig.DEFAULT_SWAP_SM2_PUBLIC_KEY = function () {
+    return "04ba6a9a137978cbee04f0783e14f7ea43eb81e2897c04c4561183b0bac1fbcf8c3aa63a5bd443bc1c58af06b55bb434322f5dbe296f780610f2d460beb41ef29a";
+}
+
+/**
+ * 默认非对称交换公钥
+ * @return {String}
+ */
+SwlTransferConfig.DEFAULT_SWAP_PUBLIC_KEY=function(){
+    return SwlTransferConfig.DEFAULT_SWAP_RSA_PUBLIC_KEY()
 }
 
 export default SwlTransferConfig
