635e74f803e126e1a18ef975aa6663db6134dc7a
Ice2Faith <Ugex_Savelar@163.com>
2026-05-12 18:39:47 +0800

    feat(xproc4j-plugin): add funic support

--- i2f-tools/i2f-jdbc-procedure-idea-plugin/src/main/java/i2f/turbo/idea/plugin/funic/FunicConsts.java
+++ i2f-tools/i2f-jdbc-procedure-idea-plugin/src/main/java/i2f/turbo/idea/plugin/funic/FunicConsts.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class FunicConsts {
     public static final String LANGUAGE_ID = "Funic";
 
-    public static final Icon ICON = IconLoader.getIcon("/assets/funic/favicon.ico", FunicConsts.class);
+    public static final Icon ICON = IconLoader.getIcon("/assets/funic/funic.svg", FunicConsts.class);
 
     public static final String FILE_DESCRIPTION = LANGUAGE_ID + " language file";
 
