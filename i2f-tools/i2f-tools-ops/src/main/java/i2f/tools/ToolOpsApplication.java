50c72290ada3272e93d83648bec312a68823a43d
Ice2Faith <Ugex_Savelar@163.com>
2026-06-08 11:44:12 +0800

    feat(ops): ops as an standalone app

---
+++ i2f-tools/i2f-tools-ops/src/main/java/i2f/tools/ToolOpsApplication.java
@@ -0,0 +1,17 @@
+package i2f.tools;
+
+import i2f.tools.application.WarBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ice2Faith
+ * @date 2026/6/8 9:26
+ * @desc
+ */
+@SpringBootApplication
+public class ToolOpsApplication extends WarBootApplication {
+    public static void main(String[] args){
+        startup(ToolOpsApplication.class,args);
+    }
+}
