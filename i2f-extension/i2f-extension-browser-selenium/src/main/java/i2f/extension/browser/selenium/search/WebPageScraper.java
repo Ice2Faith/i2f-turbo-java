bd058fec4b534c7171106129c12f425853822961
Ice2Faith <Ugex_Savelar@163.com>
2026-06-30 15:26:46 +0800

    feat(selenium): html remove script/style/link

--- i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/WebPageScraper.java
+++ i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/WebPageScraper.java
@@ -17,9 +17,9 @@ import java.time.Duration;
  * @desc
  */
 public class WebPageScraper {
-    public static SearchResult scraper(String url, String driverPath) {
+    public static SearchResult scraper(String url, boolean webUi, String driverPath) {
         // 打开目标网页
-        WebDriver driver = BrowserSelenium.getWebDriver(null, true, driverPath);
+        WebDriver driver = BrowserSelenium.getWebDriver(null, webUi, driverPath);
         driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
         try {
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
