ba218f34a83e33d029ca55a3b17a8f7e52fd9a92
Ice2Faith <Ugex_Savelar@163.com>
2026-06-30 14:10:33 +0800

    feat(selenium): html remove script/style/link

--- i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/TouTiaoSearch.java
+++ i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/TouTiaoSearch.java
@@ -5,6 +5,7 @@ import i2f.extension.browser.selenium.search.data.SearchContext;
 import i2f.extension.browser.selenium.search.data.SearchResult;
 import i2f.extension.browser.selenium.search.enums.SearchType;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -182,6 +183,12 @@ public class TouTiaoSearch {
 
 
                     if (context != null) {
+                        try {
+                            JavascriptExecutor executor = (JavascriptExecutor) driver;
+                            executor.executeScript("document.querySelectorAll('style, script','link').forEach(function(el) { el.remove(); });");
+                        } catch (Throwable e) {
+
+                        }
                         SearchResult result = entry.getKey();
                         result.setTitle(driver.getTitle());
                         result.setHtml(driver.getPageSource());
@@ -205,6 +212,12 @@ public class TouTiaoSearch {
                     System.out.println("www-article:\n" + text);
 
                     if (context != null) {
+                        try {
+                            JavascriptExecutor executor = (JavascriptExecutor) driver;
+                            executor.executeScript("document.querySelectorAll('style, script','link').forEach(function(el) { el.remove(); });");
+                        } catch (Throwable e) {
+
+                        }
                         SearchResult result = entry.getKey();
                         result.setTitle(driver.getTitle());
                         result.setHtml(driver.getPageSource());
--- i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/WebPageScraper.java
+++ i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/WebPageScraper.java
@@ -3,6 +3,7 @@ package i2f.extension.browser.selenium.search;
 import i2f.extension.browser.selenium.BrowserSelenium;
 import i2f.extension.browser.selenium.search.data.SearchResult;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,37 +20,51 @@ public class WebPageScraper {
     public static SearchResult scraper(String url, String driverPath) {
         // 打开目标网页
         WebDriver driver = BrowserSelenium.getWebDriver(null, true, driverPath);
-        driver.get(url);
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         try {
-            wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector("body"), 0));
+            driver.get(url);
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+            try {
+                wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector("body"), 0));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+
+            WebElement body = driver.findElement(By.tagName("body"));
+            String text = body.getText();
+            if (text == null || text.trim().isEmpty()) {
+                // 无文本，在等几秒
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            }
+
+            body = driver.findElement(By.tagName("body"));
+            text = body.getText();
+
+            try {
+                JavascriptExecutor executor = (JavascriptExecutor) driver;
+                executor.executeScript("document.querySelectorAll('style, script','link').forEach(function(el) { el.remove(); });");
+            } catch (Throwable e) {
+
+            }
+            SearchResult result = new SearchResult();
+            result.setUrl(url);
+            result.setDescription(null);
+
+            result.setTitle(driver.getTitle());
+            result.setHtml(driver.getPageSource());
+            if (body != null) {
+                result.setText(body.getText());
+            }
+
+            return result;
         } catch (Exception e) {
             e.printStackTrace();
+            throw e;
+        } finally {
+            // 关闭浏览器
+            driver.quit();
         }
-
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-
-        WebElement body = driver.findElement(By.tagName("body"));
-        String text = body.getText();
-        if (text == null || text.trim().isEmpty()) {
-            // 无文本，在等几秒
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        }
-
-        body = driver.findElement(By.tagName("body"));
-        text = body.getText();
-
-        SearchResult result = new SearchResult();
-        result.setUrl(url);
-        result.setDescription(null);
-
-        result.setTitle(driver.getTitle());
-        result.setHtml(driver.getPageSource());
-        if (body != null) {
-            result.setText(body.getText());
-        }
-
-        return result;
     }
 }
