b6840b1e3700428dbb517f99df972a7907d58468
Ice2Faith <Ugex_Savelar@163.com>
2026-06-30 11:25:03 +0800

    feat(ops): ops ai

---
+++ i2f-extension/i2f-extension-browser-selenium/src/main/java/i2f/extension/browser/selenium/search/WebPageScraper.java
@@ -0,0 +1,55 @@
+package i2f.extension.browser.selenium.search;
+
+import i2f.extension.browser.selenium.BrowserSelenium;
+import i2f.extension.browser.selenium.search.data.SearchResult;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+/**
+ * @author Ice2Faith
+ * @date 2026/6/30 11:17
+ * @desc
+ */
+public class WebPageScraper {
+    public static SearchResult scraper(String url, String driverPath) {
+        // 打开目标网页
+        WebDriver driver = BrowserSelenium.getWebDriver(null, true, driverPath);
+        driver.get(url);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        try {
+            wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector("body"), 0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+
+        WebElement body = driver.findElement(By.tagName("body"));
+        String text = body.getText();
+        if (text == null || text.trim().isEmpty()) {
+            // 无文本，在等几秒
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
+
+        body = driver.findElement(By.tagName("body"));
+        text = body.getText();
+
+        SearchResult result = new SearchResult();
+        result.setUrl(url);
+        result.setDescription(null);
+
+        result.setTitle(driver.getTitle());
+        result.setHtml(driver.getPageSource());
+        if (body != null) {
+            result.setText(body.getText());
+        }
+
+        return result;
+    }
+}
