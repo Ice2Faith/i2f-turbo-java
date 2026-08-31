1f6597cfe931879ab7b738f01379da1969b1cc2a
Ice2Faith <Ugex_Savelar@163.com>
2026-08-31 10:50:36 +0800

    feat(ops): ops openai

--- i2f-springboot/i2f-springboot-ops-starter/src/main/java/i2f/springboot/ops/openai/tool/impl/GroovyTools.java
+++ i2f-springboot/i2f-springboot-ops-starter/src/main/java/i2f/springboot/ops/openai/tool/impl/GroovyTools.java
@@ -6,10 +6,18 @@ import i2f.ai.std.tool.annotations.Tool;
 import i2f.ai.std.tool.annotations.ToolParam;
 import i2f.ai.std.tool.annotations.Tools;
 import i2f.extension.groovy.GroovyScript;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,23 +31,45 @@ import java.util.Map;
         GroovyShell.class,
         GroovyScript.class
 })
+@Data
+@NoArgsConstructor
 @Component
 @Tools(tags = {
         "groovy"
 })
-public class GroovyTools {
+public class GroovyTools implements ApplicationContextAware, EnvironmentAware {
+    private ApplicationContext applicationContext;
+    private Environment environment;
+
     @Tool(
             tags = {
                     AiTags.EXECUTABLE_VALUE,
                     AiTags.HUMAN_VALUE,
                     AiTags.SCRIPT_VALUE
             },
-            description = "run groovy script"
+            description = "run groovy script, returns the result of the last statement, and the result must be JSON-serializable. \n" +
+                    "Note: \n" +
+                    "   - Standard output is not captured. Please use the logger variable (`java.io.PrintWriter logger`) for logging.\n" +
+                    "   - embed variables: \n" +
+                    "       - `org.springframework.context.ApplicationContext applicationContext` \n" +
+                    "       - `org.springframework.core.env.Environment environment` \n"
     )
-    public Object groovy_run_script(@ToolParam(value = "script", description = "the script, groovy script")
-                                    String script) {
+    public Map<String, Object> groovy_run_script(@ToolParam(value = "script", description = "the script, groovy script")
+                                                 String script) {
+        Map<String, Object> ret = new HashMap<>();
+
+        StringWriter writer = new StringWriter();
+        PrintWriter logger = new PrintWriter(writer);
+
         Map<String, Object> params = new HashMap<>();
+        params.put("logger", logger);
+        params.put("applicationContext", applicationContext);
+        params.put("environment", environment);
+
         Object obj = GroovyScript.evalScript(script, params);
-        return obj;
+
+        ret.put("returns", obj);
+        ret.put("logs", writer.toString());
+        return ret;
     }
 }
