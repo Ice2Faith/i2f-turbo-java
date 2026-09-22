c69e6e680757a59bc6f6d5244943bb40528c3763
Ice2Faith <Ugex_Savelar@163.com>
2026-09-22 16:13:18 +0800

    docs(wiki): wiki docs

---
+++ i2f-springboot/i2f-springboot-redisson-starter/src/main/java/i2f/springboot/redisson/annotation/RedisLimiter.java
@@ -0,0 +1,22 @@
+package i2f.springboot.redisson.annotation;
+
+import org.redisson.api.RateIntervalUnit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RedisLimiter {
+    /**
+     * redisson limiter 的 rate
+     */
+    long value();
+
+    long rateInterval() default 1;
+
+    RateIntervalUnit unit() default RateIntervalUnit.SECONDS;
+}
